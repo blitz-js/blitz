18d38d79e7073fa99252465541a60abf5a7ca68b
Jim Cummins <jim.for.cy@gmail.com>
2020-03-02 08:50:56 -0600

    fix: noop @prisma/client in browser, closes #13 (#19)
    Co-authored-by: Brandon Bayer <b@bayer.ws>

--- examples/first-demo/next.config.js
+++ examples/first-demo/next.config.js
@@ -1,3 +1,13 @@
+const path = require('path')
 // NOTE: This is ONLY needed in this Blitz monorepo so that package hot reloading works properly
 const withTM = require('next-transpile-modules')(['@blitzjs/core'])
-module.exports = withTM({})
+module.exports = withTM({
+  webpack: (config, {buildId, dev, isServer, defaultLoaders, webpack}) => {
+    if (!isServer) {
+      // Noop resolution of @prisma/client in the browser
+      // since setting an alias to false does not work
+      config.resolve.alias['@prisma/client'] = path.join(__dirname, 'noop.js')
+    }
+    return config
+  },
+})
