207b973d3e1c79d3dde223c09270dfb59c075631
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2020-03-03 10:31:12 -0500

    uses project package manager (npm/yarn) to run test script

--- packages/cli/src/commands/test.ts
+++ packages/cli/src/commands/test.ts
@@ -1,11 +1,14 @@
 import {spawn} from 'child_process'
 import {Command} from '@oclif/command'
+import hasYarn from 'has-yarn'
 
 export default class Test extends Command {
   static description = 'Run project tests'
   static aliases = ['t']
 
   async run() {
-    spawn('yarn', ['test'], {stdio: 'inherit'})
+    const packageManager = hasYarn() ? 'yarn' : 'npm'
+
+    spawn(packageManager, ['test'], {stdio: 'inherit'})
   }
 }
--- packages/cli/test/commands/test.test.ts
+++ packages/cli/test/commands/test.test.ts
@@ -1,12 +1,28 @@
 import childProcess from 'child_process'
+import hasYarn from 'has-yarn'
 import TestCmd from '../../src/commands/test'
 
 jest.mock('child_process')
+jest.mock('has-yarn')
 
 describe('Test command', () => {
+  beforeEach(() => {
+    jest.resetAllMocks()
+  })
+
   it('runs yarn test script', async () => {
+    jest.spyOn(hasYarn, 'default').mockReturnValue(true)
+
     await TestCmd.prototype.run()
 
     expect(childProcess.spawn).toBeCalledWith('yarn', ['test'], {stdio: 'inherit'})
+  })
+
+  it('runs npm test script', async () => {
+    jest.spyOn(hasYarn, 'default').mockReturnValue(false)
+
+    await TestCmd.prototype.run()
+
+    expect(childProcess.spawn).toBeCalledWith('npm', ['test'], {stdio: 'inherit'})
   })
 })
