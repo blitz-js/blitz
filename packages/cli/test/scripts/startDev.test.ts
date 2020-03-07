d4d702330c64a39eb8049f26da0b59c6cc02bf5f
Rudi Yardley <contact@rudiyardley.com>
2020-03-07 19:16:00 +0700

    Neaten up code

--- packages/cli/test/scripts/startDev.test.ts
+++ packages/cli/test/scripts/startDev.test.ts
@@ -1,10 +1,20 @@
-const copy = jest.fn()
-const remove = jest.fn()
-const spawn = jest.fn().mockReturnValue({on: () => {}})
+const childProcessMock = {
+  spawn: jest.fn().mockReturnValue({on: () => {}}),
+}
 
-jest.doMock('child_process', () => ({spawn}))
-jest.mock('fs-extra')
-jest.mock('../../src/scripts/reporter', () => ({reporter: {copy, remove}}))
+const fsExtraMock = {
+  copy: jest.fn(),
+  unlist: jest.fn(),
+  ensureDir: jest.fn(),
+}
+
+const reporterMock = {
+  reporter: {copy: jest.fn(), remove: jest.fn()},
+}
+
+jest.doMock('child_process', () => childProcessMock)
+jest.doMock('fs-extra', () => fsExtraMock)
+jest.doMock('../../src/scripts/reporter', () => reporterMock)
 
 import {startDev} from '../../src/scripts/startDev'
 import {resolve} from 'path'
@@ -16,9 +26,9 @@ describe('Start command', () => {
     await startDev({root, persistent: true})
   })
 
-  it('copies each file to the .blitz folder', async () => {
+  it('copies each file to the .blitz folder', () => {
     const root = resolve(__dirname, './startDev')
-    expect(copy.mock.calls).toEqual([
+    expect(reporterMock.reporter.copy.mock.calls).toEqual([
       // .now should be ignored
       [root, resolve(root, 'one'), resolve(root, '.blitz/one')],
       [root, resolve(root, 'two'), resolve(root, '.blitz/two')],
@@ -26,7 +36,7 @@ describe('Start command', () => {
   })
 
   it('calls spawn with the next cli bin', () => {
-    expect(spawn).toBeCalledWith('../node_modules/.bin/next', ['dev'], {
+    expect(childProcessMock.spawn).toBeCalledWith('../node_modules/.bin/next', ['dev'], {
       cwd: resolve(__dirname, './startDev/.blitz'),
       stdio: 'inherit',
     })
