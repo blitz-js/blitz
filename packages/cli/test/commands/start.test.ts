5ea5da532fd9401c5a64da3c89602afa2679fc10
Rudi Yardley <contact@rudiyardley.com>
2020-03-08 16:42:25 +0700

    Tidy up typescript config and add test

---
+++ packages/cli/test/commands/start.test.ts
@@ -0,0 +1,29 @@
+const dev = jest.fn(() => {})
+const prod = jest.fn(() => {})
+jest.mock('../../src/start/dev', () => ({dev}))
+jest.mock('../../src/start/prod', () => ({prod}))
+
+import StartCmd from '../../src/commands/start'
+import {resolve} from 'path'
+
+describe('Start command', () => {
+  beforeEach(() => {
+    jest.clearAllMocks()
+  })
+
+  const options = {
+    buildFolder: '.blitz/caches/build',
+    devFolder: '.blitz/caches/dev',
+    rootFolder: resolve(__dirname, '../../'),
+  }
+
+  it('runs the dev script', async () => {
+    await StartCmd.run([])
+    expect(dev).toBeCalledWith(options)
+  })
+
+  it('runs the dev script', async () => {
+    await StartCmd.run(['--production'])
+    expect(prod).toBeCalledWith(options)
+  })
+})
