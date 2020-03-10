dc09d5184dbc196813fce45d17e523c6bfedae80
tsawan <tahir.awan@gmail.com>
2020-03-10 11:09:40 +0300

    fixed test

--- packages/cli/src/commands/db.ts
+++ packages/cli/src/commands/db.ts
@@ -1,5 +1,5 @@
-import { spawn } from 'child_process'
-import { Command } from '@oclif/command'
+import {spawn} from 'child_process'
+import {Command} from '@oclif/command'
 
 export default class Db extends Command {
   static description = 'Run project tests'
@@ -8,22 +8,22 @@ export default class Db extends Command {
     {
       name: 'command',
       description: 'Run specific db command',
-      required: true
-    }
+      required: true,
+    },
   ]
 
   async run() {
-    const { args } = this.parse(Db)
-    const command = args['command'];
-    if ((command === 'migrate' || command === 'm')) {
-      const cp = spawn('prisma2', ['migrate', 'save', '--experimental'], { stdio: 'inherit' })
+    const {args} = this.parse(Db)
+    const command = args['command']
+    if (command === 'migrate' || command === 'm') {
+      const cp = spawn('prisma2', ['migrate', 'save', '--experimental'], {stdio: 'inherit'})
       cp.on('exit', (code: number) => {
-        if (code == 0)
-          spawn('prisma2', ['migrate', 'up', '--experimental'], { stdio: 'inherit' })
+        if (code == 0) spawn('prisma2', ['migrate', 'up', '--experimental'], {stdio: 'inherit'})
       })
-    }
-    else ((command === 'init' || command === 'i')) {
-      spawn('prisma2', ['init'], { stdio: 'inherit' })
+    } else if (command === 'init' || command === 'i') {
+      spawn('prisma2', ['init'], {stdio: 'inherit'})
+    } else {
+      this.log('Missing command')
     }
   }
 }
