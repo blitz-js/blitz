2d5c508c4ffdf7c30821dbc6155ba4c62984124e
Rudi Yardley <contact@rudiyardley.com>
2020-03-11 17:21:46 +0700

    Add comments about what is waiting

--- packages/server/register/launch-editor.js
+++ packages/server/register/launch-editor.js
@@ -1,4 +1,5 @@
-// NOTE: This is here temporarily to support deep linking into this package
+// NOTE: This is here to support deep linking into this package
+//       It would be nice to be able to write this code in typescript
 // TODO: Incorporate this deep linked file into our TS build
 
 module.exports = {
@@ -7,7 +8,7 @@ module.exports = {
       // TODO: Implement the following:
       //       1. Load the compilation transform map from JSON
       //       2. Run a reverse lookup of the compiled file to the original URL
-      // NOTE: Currently the following is hard coded as POC until we implement transformation rules
+      // HACK: Currently the following is hard coded as POC until we have a build manifest to lookup
       const tmpReplacementPoc = file.replace(/\/\.blitz\/caches\/dev/, '')
       return launchEditor(tmpReplacementPoc, specifiedEditor, onErrorCallback)
     }
--- packages/server/src/next-utils.ts
+++ packages/server/src/next-utils.ts
@@ -1,14 +1,19 @@
 import {spawn} from 'child_process'
+import pty from 'node-pty'
 
 export async function nextStartDev(nextBin: string, cwd: string) {
-  return Promise.resolve(
-    spawn(nextBin, ['dev'], {
+  const cp = pty
+    .spawn(nextBin, ['dev'], {
+      name: 'xterm-color',
       cwd,
-      stdio: 'inherit',
-    }).on('error', err => {
-      console.error(err)
-    }),
-  )
+    })
+    .on('data', data => {
+      // HACK: The following is temporary until we have a build
+      //       manifest to lookup on a per file basis
+      process.stdout.write(data.toString().replace('.blitz/caches/dev/', ''))
+    })
+
+  return Promise.resolve(cp)
 }
 
 export async function nextBuild(nextBin: string, cwd: string) {
