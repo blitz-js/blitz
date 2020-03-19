7af767da53f7727aaf37cc97b11116a505d02117
Rudi Yardley <contact@rudiyardley.com>
2020-03-19 18:28:52 +0700

    Include catch for missing unlink

--- packages/server/src/synchronizer/unlink.ts
+++ packages/server/src/synchronizer/unlink.ts
@@ -1,4 +1,4 @@
-import {unlink as unlinkFile} from 'fs-extra'
+import {unlink as unlinkFile, pathExists} from 'fs-extra'
 import through from 'through2'
 import * as File from 'vinyl'
 import {relative, resolve} from 'path'
@@ -12,7 +12,7 @@ function getDestPath(folder: string, file: File) {
 export function unlink(folder: string) {
   return through.obj(async (file: File, _encoding, done) => {
     if (file.event === 'unlink' || file.event === 'unlinkDir') {
-      await unlinkFile(getDestPath(folder, file))
+      if (await pathExists(getDestPath(folder, file))) await unlinkFile(getDestPath(folder, file))
     }
 
     done(null, file)
