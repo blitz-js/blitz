53eab985fd65e994ab8b43d0d6546131c3ee98cc
Brandon Bayer <b@bayer.ws>
2020-04-11 10:25:33 +0700

    chore: remove all prototype code in prep for new architecture code (#90)
    * remove all prototype code and clean up deps
    
    * fixes

--- examples/first-demo/pages/api/comments/index.js
+++
@@ -1,3 +0,0 @@
-// This file is needed because of https://github.com/zeit/next.js/issues/10488
-// You can vote or comment there if you don't want these either
-export {default} from './[...id]'
--- examples/first-demo/tests/plugins/index.js
+++
@@ -1,9 +0,0 @@
-const {addComment, addPost, clear} = require('./tasks/db')
-
-module.exports = (on) => {
-  on('task', {
-    'db:clear': clear,
-    'db:posts:add': addPost,
-    'db:comments:add': addComment,
-  })
-}
