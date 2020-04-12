574b27478921891c79f9de27fc16c835a0158d5f
Rudi Yardley <contact@rudiyardley.com>
2020-04-12 14:49:52 +1000

    Add file addition and subtraction to transform stream

---
+++ packages/server/src/synchronizer/transform.test.ts
@@ -0,0 +1,85 @@
+import File from 'vinyl'
+import {Readable, Transform} from 'readable-stream'
+import {transform} from './transform'
+
+function getStream() {
+  const stream = new Readable({objectMode: true})
+  stream._read = () => {}
+  return stream
+}
+
+function getCollector(output: any[]) {
+  const stream = new Transform({objectMode: true})
+
+  return stream.on('data', (data: File) => {
+    output.push(data)
+  })
+}
+
+it('transforms the file', () => {
+  const stream = getStream()
+  const output: File[] = []
+  const collector = getCollector(output)
+
+  stream
+    .pipe(
+      transform((file) => {
+        return [file, new File({path: file.path + '_rpc', contents: file.contents})]
+      }),
+    )
+    .pipe(collector)
+
+  stream.push(
+    new File({
+      path: '/folder/file1',
+      contents: Buffer.from('File 1'),
+    }),
+  )
+  stream.push(
+    new File({
+      path: '/folder/file2',
+      contents: Buffer.from('File 2'),
+    }),
+  )
+  stream.push(
+    new File({
+      path: '/folder/file3',
+      contents: Buffer.from('File 3'),
+    }),
+  )
+  // stream.read()
+  // stream.read()
+  // stream.read()
+
+  expect(
+    output.map(({path, contents}) => ({
+      path,
+      contents: contents?.toString(),
+    })),
+  ).toMatchObject([
+    {
+      path: '/folder/file1',
+      contents: 'File 1',
+    },
+    {
+      path: '/folder/file1_rpc',
+      contents: 'File 1',
+    },
+    {
+      path: '/folder/file2',
+      contents: 'File 2',
+    },
+    {
+      path: '/folder/file2_rpc',
+      contents: 'File 2',
+    },
+    {
+      path: '/folder/file3',
+      contents: 'File 3',
+    },
+    {
+      path: '/folder/file3_rpc',
+      contents: 'File 3',
+    },
+  ])
+})
--- packages/server/src/synchronizer/transform.ts
+++ packages/server/src/synchronizer/transform.ts
@@ -1,8 +1,16 @@
 import through from 'through2'
 import File from 'vinyl'
 
-export function transform(fn: (f: File, e: string) => File) {
-  return through.obj((file: File, encoding, done) => {
-    done(null, fn(file, encoding))
+// Accept a function to transform the stream
+// If the function returns an array of files add all subsequent files to the stream
+// If you return an empty array the file will be deleted from the stream
+export function transform(fn: (f: File, e: string) => File | File[]) {
+  return through.obj(function (file: File, encoding, done) {
+    const ret = fn(file, encoding)
+    const files = Array.isArray(ret) ? ret : [ret]
+    for (file of files) {
+      this.push(file)
+    }
+    done()
   })
 }
