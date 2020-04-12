fafa46c3f1edee64a4320296186bf1d366017474
Brandon Bayer <b@bayer.ws>
2020-04-12 09:54:41 +0700

    typo

--- examples/store/app/products/queries/getProducts.ts
+++ examples/store/app/products/queries/getProducts.ts
@@ -1,7 +1,7 @@
 import db, {FindManyProductArgs} from 'prisma'
 
 export default async function getProducts(args?: FindManyProductArgs) {
-  const product = await db.product.findMany(args)
+  const products = await db.product.findMany(args)
 
-  return product
+  return products
 }
