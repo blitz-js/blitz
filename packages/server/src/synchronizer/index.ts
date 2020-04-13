3c209b060ca90a2ab8bf5e143b4b69e715b62e42
merelinguist <merelinguist@users.noreply.github.com>
2020-04-13 12:35:09 +0100

    First pass at warning about duplicate routes

--- packages/server/src/synchronizer/index.ts
+++ packages/server/src/synchronizer/index.ts
@@ -13,6 +13,7 @@ import {remove, pathExists, ensureDir} from 'fs-extra'
 import {ciLog} from '../ciLog'
 import {runRule} from './rules-runner'
 import through from 'through2'
+import chalk from 'chalk'
 
 type SynchronizeFilesInput = {
   src: string
@@ -57,6 +58,28 @@ export async function synchronizeFiles({
   const {stream, watcher} = watch(includePaths, options)
 
   await clean(destPath)
+
+  const allRoutes = entries.filter((route) => route.includes('routes'))
+  const cleanRoutes = allRoutes.map((route) => route.split('routes/')[1])
+  const duplicateRoutes = cleanRoutes.filter((e, i, a) => a.indexOf(e) !== i)
+  const duplicateRoutesIndexes = duplicateRoutes.map((route) =>
+    cleanRoutes.flatMap((cleanRoute, i) => (cleanRoute === route ? i : [])),
+  )
+  const duplicateRoutesPaths = duplicateRoutesIndexes.map((indexes) =>
+    indexes.map((index) => allRoutes[index]),
+  )
+
+  if (duplicateRoutesPaths.length > 0) {
+    console.log(chalk.yellow('Warning: You have created conflicting routes: \n'))
+
+    duplicateRoutesPaths.forEach((route) => {
+      route.forEach((path) =>
+        console.log(`- ${path.split('routes/')[0]}routes/${chalk.bold.redBright(path.split('routes/')[1])}`),
+      )
+    })
+
+    process.exit(0)
+  }
 
   return await new Promise((resolve, reject) => {
     pipeline(
