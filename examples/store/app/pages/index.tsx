256387cbbd91a9f188e5660ecea63b73cbbde003
Brandon Bayer <b@bayer.ws>
2020-04-14 18:42:24 +0700

    chore(examples): Make store work in prod (switch to Postgres) (#117)
    * wip
    
    * fix(example): changes for deploying store to Zeit. Switch to postgres

--- examples/store/app/pages/index.tsx
+++ examples/store/app/pages/index.tsx
@@ -1,44 +1,27 @@
 import Head from 'next/head'
+import {Link} from '@blitzjs/core'
 
 const Home = () => (
   <div className="container">
     <Head>
-      <title>Create Next App</title>
+      <title>Blitz Example Store</title>
       <link rel="icon" href="/favicon.ico" />
     </Head>
 
     <main>
-      <h1 className="title">
-        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      <h1 className="title" style={{marginBottom: 24}}>
+        Blitz Store Example
       </h1>
-
-      <p className="description">
-        Get started by editing <code>pages/index.js</code>
+      <p>
+        <Link href="/products">
+          <a>View Static Public Product Listings</a>
+        </Link>
       </p>
-
-      <div className="grid">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Learn &rarr;</h3>
-          <p>Learn about Next.js in an interactive course with quizzes!</p>
-        </a>
-
-        <a href="https://github.com/zeit/next.js/tree/master/examples" className="card">
-          <h3>Examples &rarr;</h3>
-          <p>Discover and deploy boilerplate example Next.js projects.</p>
-        </a>
-
-        <a
-          href="https://zeit.co/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          className="card">
-          <h3>Deploy &rarr;</h3>
-          <p>Instantly deploy your Next.js site to a public URL with ZEIT Now.</p>
-        </a>
-      </div>
+      <p>
+        <Link href="/admin">
+          <a>View Dynamic Admin Section</a>
+        </Link>
+      </p>
     </main>
 
     <footer>
@@ -89,11 +72,6 @@ const Home = () => (
       }
 
       a {
-        color: inherit;
-        text-decoration: none;
-      }
-
-      .title a {
         color: #0070f3;
         text-decoration: none;
       }
