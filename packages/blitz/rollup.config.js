c3a2fcf2b2432831f63171156979cb77f4a08a09
Michael Edelman <michael@fabulas.io>
2020-04-15 18:13:52 -0400

    chore: make 'blitz' work

---
+++ packages/blitz/rollup.config.js
@@ -0,0 +1,54 @@
+import pkg from './package.json'
+import typescript from '@wessberg/rollup-plugin-ts'
+import commonjs from 'rollup-plugin-commonjs'
+import external from 'rollup-plugin-peer-deps-external'
+import resolve from 'rollup-plugin-node-resolve'
+import json from 'rollup-plugin-json'
+
+const common = {
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+    'react',
+    'react-dom',
+    // '@prisma/client',
+    'next',
+  ],
+  plugins: [
+    json(),
+    external(),
+    resolve(),
+    typescript({
+      transformers: [],
+    }),
+    commonjs({
+      include: /node_modules/,
+      namedExports: {},
+    }),
+  ],
+}
+
+const lib = {
+  input: './src/index.ts',
+  output: {
+    file: pkg['main'],
+    exports: 'named',
+    sourcemap: 'true',
+  },
+  ...common,
+}
+
+const cli = {
+  input: './src/bin/index.ts',
+  output: {
+    file: './dist/cli.js',
+    exports: 'named',
+    sourcemap: 'true',
+    format: 'cjs',
+  },
+  ...common,
+}
+
+const builds = [lib, cli]
+
+export default builds
