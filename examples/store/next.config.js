11646be5875402a3ec67ec8fa5a4bb4c3057d431
Daniel Kempner <dkempner@lyft.com>
2020-04-17 19:01:33 -0700

    skeleton of webpack

--- examples/store/next.config.js
+++ examples/store/next.config.js
@@ -1,3 +1,13 @@
 const {withBlitz} = require('@blitzjs/server')
 
-module.exports = withBlitz({})
+module.exports = withBlitz({
+  webpack: (config) => {
+    const firstRule = config.module.rules[0]
+    const origExclude = firstRule.exclude
+    firstRule.exclude = (path) => {
+      console.log(path)
+      if (path.includes('db/.generated-prisma-client')) return true
+      return origExclude(path)
+    }
+  },
+})
