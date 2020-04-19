f3640ae9070efd2cb0dac4be0fdd335531b57994
merelinguist <merelinguist@users.noreply.github.com>
2020-04-19 17:14:04 +0100

    Automatically run `blitz db migrate` on every `blitz start`

--- packages/cli/src/commands/start.ts
+++ packages/cli/src/commands/start.ts
@@ -1,6 +1,8 @@
 import {Command, flags} from '@oclif/command'
 import {dev, prod} from '@blitzjs/server'
 
+import {runMigrate, schemaArg} from './db'
+
 export default class Start extends Command {
   static description = 'Start a development server'
   static aliases = ['s']
@@ -12,8 +14,11 @@ export default class Start extends Command {
     }),
   }
 
+  static args = [{name: 'noMigrate', description: 'Disable automatic database migration'}]
+
   async run() {
     const {flags} = this.parse(Start)
+    const {args} = this.parse(Start)
 
     const config = {
       rootFolder: process.cwd(),
@@ -22,6 +27,9 @@ export default class Start extends Command {
     if (flags.production) {
       await prod(config)
     } else {
+      if (!args.noMigrate) {
+        runMigrate(schemaArg)
+      }
       await dev(config)
     }
   }
