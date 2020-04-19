5d3afb0230e0c2fe59e0d7ca17076d76c8befde2
Brandon Bayer <b@bayer.ws>
2020-04-19 12:47:16 +0700

    Fix server for deployment to Zeit (#143)
    * fix production, but tests still failing
    
    * Fix missing await and avoid changing source folder
    
    Co-authored-by: Rudi Yardley <contact@rudiyardley.com>

--- packages/server/test/rules.test.ts
+++ packages/server/test/rules.test.ts
@@ -2,6 +2,7 @@
 
 const nextUtilsMock = {
   nextStartDev: jest.fn().mockReturnValue(Promise.resolve()),
+  nextBuild: jest.fn().mockReturnValue(Promise.resolve()),
 }
 // Quieten reporter
 jest.doMock('../src/reporter', () => ({
@@ -13,9 +14,10 @@ jest.doMock('../src/next-utils', () => nextUtilsMock)
 
 // Import with mocks applied
 import {dev} from '../src/dev'
+import {build} from '../src/build'
 import {resolve} from 'path'
 import {FSWatcher} from 'chokidar'
-import {remove, pathExists} from 'fs-extra'
+import {remove, pathExists, writeFile} from 'fs-extra'
 import directoryTree from 'directory-tree'
 
 describe('Dev command', () => {
@@ -34,7 +36,7 @@ describe('Dev command', () => {
     if (await pathExists(devFolder)) {
       await remove(devFolder)
     }
-    watcher.close()
+    watcher?.close()
   })
 
   it('should copy the correct files to the dev folder', async () => {
@@ -81,6 +83,86 @@ describe('Dev command', () => {
         },
       ],
       size: 270,
+      type: 'directory',
+    })
+  })
+})
+
+describe('Build command ZEIT', () => {
+  const rootFolder = resolve(__dirname, './fixtures/rules')
+  const buildFolder = resolve(rootFolder, '.blitz')
+  const devFolder = resolve(rootFolder, '.blitz-rules')
+
+  beforeEach(async () => {
+    process.env.NOW_BUILDER = '1'
+    jest.clearAllMocks()
+
+    const defaultZeitConfig = "module.exports = { target: 'experimental-serverless-trace' };"
+    await writeFile(resolve(rootFolder, 'next.config.js'), Buffer.from(defaultZeitConfig))
+
+    await build({rootFolder, buildFolder, devFolder, writeManifestFile: false})
+  })
+
+  afterEach(async () => {
+    delete process.env.NOW_BUILDER
+    if (await pathExists(buildFolder)) {
+      await remove(buildFolder)
+    }
+
+    await remove(resolve(rootFolder, 'next.config.js'))
+  })
+
+  it('should copy the correct files to the build folder', async () => {
+    const tree = directoryTree(buildFolder)
+    expect(tree).toEqual({
+      path: `${buildFolder}`,
+      name: '.blitz',
+      children: [
+        {
+          extension: '.js',
+          name: 'blitz.config.js',
+          path: `${buildFolder}/blitz.config.js`,
+          size: 20,
+          type: 'file',
+        },
+        {
+          extension: '.js',
+          name: 'next-zeit.config.js',
+          path: `${buildFolder}/next-zeit.config.js`,
+          size: 61,
+          type: 'file',
+        },
+        {
+          extension: '.js',
+          name: 'next.config.js',
+          path: `${buildFolder}/next.config.js`,
+          size: 61,
+          type: 'file',
+        },
+        {
+          path: `${buildFolder}/pages`,
+          name: 'pages',
+          children: [
+            {
+              path: `${buildFolder}/pages/bar.tsx`,
+              name: 'bar.tsx',
+              size: 60,
+              extension: '.tsx',
+              type: 'file',
+            },
+            {
+              path: `${buildFolder}/pages/foo.tsx`,
+              name: 'foo.tsx',
+              size: 60,
+              extension: '.tsx',
+              type: 'file',
+            },
+          ],
+          size: 120,
+          type: 'directory',
+        },
+      ],
+      size: 262,
       type: 'directory',
     })
   })
