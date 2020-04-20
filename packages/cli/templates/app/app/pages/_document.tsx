bf4b90943bc4e70c1e36dd877e23da3e77162670
Brandon Bayer <b@bayer.ws>
2020-04-20 17:12:54 +0700

    Many updates to blitz new template, including prettier, eslint,… (#150)
    * update template app
    
    * fix `blitz -v`

--- packages/cli/templates/app/app/pages/_document.tsx
+++ packages/cli/templates/app/app/pages/_document.tsx
@@ -1,10 +1,11 @@
-import {Document, Html, DocumentHead, Main, NextScript, DocumentContext} from '@blitzjs/core'
+import { Document, Html, DocumentHead, Main, NextScript /*DocumentContext*/ } from "@blitzjs/core"
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return {...initialProps}
-  }
+  // Only uncomment if you need to customize this behaviour
+  // static async getInitialProps(ctx: DocumentContext) {
+  //   const initialProps = await Document.getInitialProps(ctx)
+  //   return {...initialProps}
+  // }
 
   render() {
     return (
--- packages/cli/templates/app/db/index.ts
+++ packages/cli/templates/app/db/index.ts
@@ -1,5 +1,5 @@
-import {PrismaClient} from '@prisma/client'
-export * from '@prisma/client'
+import { PrismaClient } from "@prisma/client"
+export * from "@prisma/client"
 
 const prisma = new PrismaClient()
 
