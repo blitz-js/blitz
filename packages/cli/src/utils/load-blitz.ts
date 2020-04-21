1aae1ede1702d576af00bcbb7182b16d115cafcf
Brandon Bayer <b@bayer.ws>
2020-04-21 21:06:35 +0700

    wip

--- packages/cli/src/utils/load-blitz.ts
+++ packages/cli/src/utils/load-blitz.ts
@@ -1,7 +1,14 @@
 import {forceRequire} from './module'
+import {join} from 'path'
 
-export const BLITZ_MODULE_PATHS = ['@prisma/client']
+export const BLITZ_MODULE_PATHS = ['db']
 
 export const loadBlitz = () => {
-  return Object.assign({}, ...BLITZ_MODULE_PATHS.map(forceRequire))
+  return Object.assign(
+    {},
+    {db: forceRequire(join(process.cwd(), 'db')).default},
+    // ...BLITZ_MODULE_PATHS.map((path) => ({
+    //   [path]: forceRequire(join(process.cwd(), 'db')).default,
+    // })),
+  )
 }
