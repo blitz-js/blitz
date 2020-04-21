3521486e7c943760f1d78176b6c3f87d872ca42a
Fran Zekan <zekan.fran369@gmail.com>
2020-04-21 03:54:31 +0200

    Fix not adding HEAD warmups on build

--- packages/cli/src/commands/build.ts
+++ packages/cli/src/commands/build.ts
@@ -1,13 +1,15 @@
 import {Command} from '@oclif/command'
 import {build} from '@blitzjs/server'
+import {ServerConfig} from '@blitzjs/server/src/config'
 
 export default class Build extends Command {
   static description = 'Create a production build'
   static aliases = ['b']
 
   async run() {
-    const config = {
+    const config: ServerConfig = {
       rootFolder: process.cwd(),
+      serverless: true,
     }
 
     await build(config)
