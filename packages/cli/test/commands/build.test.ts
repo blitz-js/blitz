d194b44b5543759617bc1fa0dbc5c6d7e7011ec7
Fran Zekan <zekan.fran369@gmail.com>
2020-04-21 03:57:09 +0200

    Fix test

--- packages/cli/test/commands/build.test.ts
+++ packages/cli/test/commands/build.test.ts
@@ -3,14 +3,16 @@ jest.mock('@blitzjs/server', () => ({build}))
 
 import BuildCmd from '../../src/commands/build'
 import {resolve} from 'path'
+import {ServerConfig} from '@blitzjs/server/src/config'
 
 describe('Build command', () => {
   beforeEach(() => {
     jest.clearAllMocks()
   })
 
-  const options = {
+  const options: ServerConfig = {
     rootFolder: resolve(__dirname, '../../'),
+    serverless: true,
   }
 
   it('runs the build script', async () => {
