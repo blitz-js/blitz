10d50f450f8420d5b1e269447a63f453ce546da2
merelinguist <merelinguist@users.noreply.github.com>
2020-04-22 07:53:59 +0100

    Clean up fetching

--- packages/cli/src/utils/get-latest-version.ts
+++ packages/cli/src/utils/get-latest-version.ts
@@ -1,20 +1,24 @@
 import fetch from 'node-fetch'
 
-export const getLatestDistVersion = async (dependency: string) => {
+const fetchAllVersions = async (dependency: string) => {
+  const res = await fetch(`https://registry.npmjs.org/${dependency}`)
+  const json = await res.json()
+  return json.versions as string[]
+}
+
+const fetchLatestDistVersion = async (dependency: string) => {
   const res = await fetch(`https://registry.npmjs.org/-/package/${dependency}/dist-tags`)
   const json = await res.json()
   return json.latest as string
 }
 
 export const getLatestVersion = async (dependency: string, major: string) => {
-  const res = await fetch(`https://registry.npmjs.org/${dependency}`)
-  const json = await res.json()
-  const versions = Object.keys(json.versions)
-    .filter((version) => version.startsWith(major))
-    .sort((a, b) => a.localeCompare(b, undefined, {numeric: true}))
+  const allVersions = await fetchAllVersions(dependency)
+  const latestDistVersion = await fetchLatestDistVersion(dependency)
 
-  const latestVersion = versions[0]
-  const latestDistVersion = await getLatestDistVersion(dependency)
+  const latestVersion = Object.keys(allVersions)
+    .filter((version) => version.startsWith(major))
+    .sort((a, b) => a.localeCompare(b, undefined, {numeric: true}))[0]
 
   // If the latest tagged version matches our pinned major, use that, otherwise use the
   // latest untagged which does
