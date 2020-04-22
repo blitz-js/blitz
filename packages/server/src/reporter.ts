0029ef493455636defa991db48847e14bc054988
Oscar Sirvent <oscar.sirvent@gmail.com>
2020-04-22 10:12:43 +0200

    update logging cli

--- packages/server/src/reporter.ts
+++ packages/server/src/reporter.ts
@@ -1,12 +1,13 @@
 import {resolve} from 'path'
+import {progress} from './log'
 
 // TODO: Eventually use a proper reporting mechanism
 export const reporter = {
   copy(fileRoot: string, srcPath: string, destPath: string) {
-    console.log(`${resolve(fileRoot, srcPath)} => ${resolve(srcPath, destPath)}`)
+    progress(`${resolve(fileRoot, srcPath)} => ${resolve(srcPath, destPath)}`)
   },
 
   remove(fileRoot: string, filePath: string) {
-    console.log(`DELETE: ${resolve(fileRoot, filePath)}`)
+    progress(`DELETE: ${resolve(fileRoot, filePath)}`)
   },
 }
