4f45e8d6bd6851faab633527f0d786877d4559c4
Rudi Yardley <contact@rudiyardley.com>
2020-04-22 16:26:57 +1000

    Alter types

--- packages/server/src/synchronizer/types.ts
+++ packages/server/src/synchronizer/types.ts
@@ -1,41 +1,9 @@
-import {Manifest} from './pipeline/rules/manifest'
-import {through} from './streams'
 import {Readable, Writable} from 'stream'
 import {FileCache} from './pipeline/helpers/file-cache'
 
-export type PhaseNames = keyof RuleTransformObject
-
-type RuleTransformObject = {
-  input: ReturnType<typeof through>
-  analyze: ReturnType<typeof through>
-  process: ReturnType<typeof through>
-  write: ReturnType<typeof through>
-  complete: ReturnType<typeof through>
-}
-
-export type ReadyObj = {
-  manifest: Manifest
-}
-
-export type RuleArgs = {
-  config: RuleConfig
-  input: Writable
-  reporter: Writable
-  errors: Writable
-  getInputCache: () => FileCache
-}
-
-export type ReadyFn = (obj: ReadyObj) => void
-export type ReadyArg = ReadyObj | ReadyFn
-
-export type Rule = (
-  a: RuleArgs,
-) => {
-  stream: Readable
-} & Record<any, any>
-
-export type RuleInitializer = any
-
+/**
+ * Configuration for Rules
+ */
 export type RuleConfig = {
   src: string
   dest: string
@@ -45,3 +13,23 @@ export type RuleConfig = {
     write: boolean
   }
 }
+
+/**
+ * Arguments object for Rules
+ */
+export type RuleArgs = {
+  config: RuleConfig
+  input: Writable
+  reporter: Writable
+  errors: Writable
+  getInputCache: () => FileCache
+}
+
+/**
+ * Basic template for Business rules modules
+ */
+export type Rule = (
+  a: RuleArgs,
+) => {
+  stream: Readable
+} & Record<any, any>
