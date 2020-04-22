61ef34f16911b35cc7a42f5d18652f7eb5107bb5
Justin Hall <justin.r.hall@gmail.com>
2020-04-22 12:58:56 -0600

    fix(server): make synchronizer pages rule cross-platform

--- packages/server/src/synchronizer/pipeline/rules/pages/pages.test.ts
+++ packages/server/src/synchronizer/pipeline/rules/pages/pages.test.ts
@@ -1,31 +1,32 @@
+import {normalize} from 'path'
 import {pagesPathTransformer} from '.'
 
 describe('createPagesPathTransformer', () => {
   const tests = [
     {
       name: 'extracts paths folder to the root in a basic transformation',
-      input: 'app/users/pages/one/two/three.tsx',
-      expected: 'pages/one/two/three.tsx',
+      input: normalize('app/users/pages/one/two/three.tsx'),
+      expected: normalize('pages/one/two/three.tsx'),
     },
     {
       name: 'handles leading /',
-      input: '/app/users/pages/one/two/three.tsx',
-      expected: 'pages/one/two/three.tsx',
+      input: normalize('/app/users/pages/one/two/three.tsx'),
+      expected: normalize('pages/one/two/three.tsx'),
     },
     {
       name: 'handles nested pages folders',
-      input: 'app/users/pages/one/two/pages/three.tsx',
-      expected: 'pages/one/two/pages/three.tsx',
+      input: normalize('app/users/pages/one/two/pages/three.tsx'),
+      expected: normalize('pages/one/two/pages/three.tsx'),
     },
     {
       name: 'ignores files outside of app',
-      input: 'thing/users/pages/one/two/pages/three.tsx',
-      expected: 'thing/users/pages/one/two/pages/three.tsx',
+      input: normalize('thing/users/pages/one/two/pages/three.tsx'),
+      expected: normalize('thing/users/pages/one/two/pages/three.tsx'),
     },
     {
       name: 'extracts paths folder to the root in a basic transformation',
-      input: '/User/foo/bar/app/users/pages/one/two/three.tsx',
-      expected: 'pages/one/two/three.tsx',
+      input: normalize('/User/foo/bar/app/users/pages/one/two/three.tsx'),
+      expected: normalize('pages/one/two/three.tsx'),
     },
   ]
 
