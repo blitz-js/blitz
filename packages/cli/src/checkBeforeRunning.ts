60f0de59ffd930d8cde05ca6602dbf3d42b890a1
Fran Zekan <zekan.fran369@gmail.com>
2020-04-23 17:40:41 +0200

    Add init hook to check if running inside of blitz root

---
+++ packages/cli/src/checkBeforeRunning.ts
@@ -0,0 +1,16 @@
+import {Hook} from '@oclif/config'
+import {readFile} from 'fs-extra'
+
+const whitelistGlobal = ['new']
+
+const hook: Hook<'init'> = async function (options) {
+  if (options.id && whitelistGlobal.includes(options.id)) return
+
+  try {
+    await readFile('blitz.config.js')
+  } catch (err) {
+    this.error(`It appears that you're not running in a blitz directory`)
+  }
+}
+
+export default hook
