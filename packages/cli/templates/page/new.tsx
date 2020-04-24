08921373708693f83c2ac621743d5dada685beef
Brandon Bayer <b@bayer.ws>
2020-04-24 16:36:56 +0700

    🎉 Add full working CRUD actions to pages generator! + fix RPC b… (#214)
    minor

--- packages/cli/templates/page/new.tsx
+++ packages/cli/templates/page/new.tsx
@@ -1,16 +1,41 @@
-import {Head} from '@blitzjs/core'
+import {Head, Link, useRouter} from 'blitz'
+import create<%= ModelName %> from 'app/<%= modelNames %>/mutations/create<%= ModelName %>'
 
-const New<%= ModelName %>Page = () => (
-  <div className="container">
-    <Head>
-      <title>New <%= ModelName %></title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
+const New<%= ModelName %>Page = () => {
+  const router = useRouter()
+  return (
+    <div className="container">
+      <Head>
+        <title>New <%= ModelName %></title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-    <main>
-      <h1>Place the form to create a new <%= ModelName %> here</h1>
-    </main>
-  </div>
-)
+      <main>
+        <h1>Create New <%= ModelName %> </h1>
+
+        <form onSubmit={async (event) => {
+          event.preventDefault()
+          try {
+            const <%= modelName %> = await create<%= ModelName %>({data: {name: 'MyName'}})
+            alert('Success!' + JSON.stringify(<%= modelName %>))
+            router.push('/<%= modelNames %>/[id]', `/<%= modelNames %>/${<%= modelName %>.id}`)
+          } catch (error) {
+            alert('Error creating <%= modelName %> ' + JSON.stringify(error, null, 2))
+          }
+        }}>
+          <div>Put your form fields here</div>
+          <button>Submit</button>
+        </form>
+
+        <p>
+          <Link href="/<%= modelNames %>">
+            <a><%= ModelNames %></a>
+          </Link>
+        </p>
+      </main>
+    </div>
+  )
+}
 
 export default New<%= ModelName %>Page
+
