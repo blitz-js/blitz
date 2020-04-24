1935ba79f1744e832f984c114bc20001d917ff79
Fran Zekan <zekan.fran369@gmail.com>
2020-04-24 12:00:37 +0200

    Fix eslint config

--- examples/store/.eslintrc.js
+++ examples/store/.eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   rules: {
     'import/no-webpack-loader-syntax': 'off',
     'react/react-in-jsx-scope': 'off', // React is always in scope with Blitz
-    'jsx-a11y/anchor-is-valid': 'off',
   },
   env: {
     'cypress/globals': true,
