c6f07e72fe424420febdfdfd761cbb1ef460b25f
Fran Zekan <zekan.fran369@gmail.com>
2020-04-24 10:00:31 +0200

    Add cypress and eslint config for cypress

--- examples/store/.eslintrc.js
+++ examples/store/.eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
   extends: ['react-app', 'plugin:jsx-a11y/recommended'],
-  plugins: ['jsx-a11y'],
+  plugins: ['jsx-a11y', 'cypress'],
   rules: {
     'import/no-webpack-loader-syntax': 'off',
     'react/react-in-jsx-scope': 'off', // React is always in scope with Blitz
+  },
+  env: {
+    'cypress/globals': true,
   },
 }
