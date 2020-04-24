2dfa86556d866bbc976a8a0a09273137ba9f5d25
Dylan Brookes <24858006+merelinguist@users.noreply.github.com>
2020-04-24 06:36:10 +0100

    Beautify query/mutation logging and Refactor logger (#199)
    * Refactor logger and beautify rpc logging
    
    * Resolve conflicts and make requested changes
    
    * move rpcHandler into server package where it belongs
    
    * improve logging even more
    
    * fix dependency issue
    (patch)
    Co-authored-by: merelinguist <merelinguist@users.noreply.github.com>
    Co-authored-by: Brandon Bayer <b@bayer.ws>

--- packages/core/src/rpc.ts
+++ packages/core/src/rpc.ts
@@ -1,5 +1,4 @@
-import {BlitzApiRequest, BlitzApiResponse} from '.'
-import {serializeError, deserializeError} from 'serialize-error'
+import {deserializeError} from 'serialize-error'
 
 export async function rpc(url: string, params: any) {
   if (typeof window === 'undefined') return
@@ -39,57 +38,5 @@ export function isomorphicRpc(resolver: any, cacheKey: string) {
     return rpcFn
   } else {
     return resolver
-  }
-}
-
-export function rpcHandler(
-  type: string,
-  name: string,
-  resolver: (...args: any) => Promise<any>,
-  connectDb?: () => any,
-) {
-  return async function (req: BlitzApiRequest, res: BlitzApiResponse) {
-    const logPrefix = `[${type}:${name}]`
-    console.log(`${logPrefix} ${req.method} ${JSON.stringify(req.body)} `)
-
-    if (req.method === 'HEAD') {
-      // Warm the lamda and connect to DB
-      if (typeof connectDb === 'function') {
-        connectDb()
-      }
-      console.log(`${logPrefix} SUCCESS 200`)
-      return res.status(200).end()
-    } else if (req.method === 'POST') {
-      // Handle RPC call
-
-      if (typeof req.body.params === 'undefined') {
-        const error = {message: "Request body is missing the 'params' key"}
-        console.log(`${logPrefix} ERROR: ${JSON.stringify(error)}`)
-        return res.status(400).json({
-          result: null,
-          error,
-        })
-      }
-
-      try {
-        const result = await resolver(req.body.params)
-
-        console.log(`${logPrefix} SUCCESS ${JSON.stringify(result)}`)
-        return res.json({
-          result,
-          error: null,
-        })
-      } catch (error) {
-        console.log(`${logPrefix} ERROR: ${error}`)
-        return res.json({
-          result: null,
-          error: serializeError(error),
-        })
-      }
-    } else {
-      // Everything else is error
-      console.log(`${logPrefix} ERROR: 404`)
-      return res.status(404).end()
-    }
   }
 }
