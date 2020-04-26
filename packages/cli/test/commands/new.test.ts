e03bc3bbe81565481ec62504fd3047e1b476e5dc
Simon Knott <info@simonknott.de>
2020-04-26 10:48:11 +0200

    Setup integration test for `blitz new`
    Mocking out fs is not compatible with oclif (see https://github.com/oclif/oclif/issues/182).
    That's why I've decided to use a temporary directory instead. AS these are deleted on startup (see https://www.2daygeek.com/automatically-delete-clean-up-tmp-directory-folder-contents-in-linux/), this should not be a big problem.
    What is a big problem though, is the duration of these tests: Since it *really runs blitz new*, it takes a long time to run (which is obvious, since it needs to download the entirety of the internet for dependencies ;D).
    There should be a way to differentiate between integration/e2e tests and unit tests so that the long-running ones can be omitted during dev. But that's out of scope for this PR.
    The next.js repository can serve as a good inspiration on how to achieve that separation.

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -1,3 +1,41 @@
-jest.mock('fs')
+import NewCmd from "../../src/commands/new";
+import * as fs from "fs";
+import * as path from "path";
+import * as os from "os";
 
-test('Mock inquirer.js and test the command', () => {})
+function makeTempDir() {
+  const tmpDirPath = path.join(
+    os.tmpdir(),
+    "blitzjs-test-"
+  );
+
+  return fs.mkdtempSync(tmpDirPath);
+}
+
+describe("`new` command", () => {
+  describe("when scaffolding new project", () => {
+
+    jest.setTimeout(60 * 1000);
+
+    let tempDir: string;
+    beforeAll(async () => {
+      tempDir = makeTempDir();
+      await NewCmd.run([tempDir]);
+    });
+
+    afterAll(() => {
+      fs.rmdirSync(tempDir);
+    });
+
+    it("pins Blitz to the current version", () => {
+      const packageJsonFile = fs.readFileSync(
+        path.join(tempDir, "package.json"),
+        { encoding: "utf8", flag: "r" }
+      );
+      const packageJson = JSON.parse(packageJsonFile);
+      const { dependencies: { blitz: blitzVersion } } = packageJson;
+      expect(blitzVersion).toBe("canary");
+    });
+
+  })
+})
