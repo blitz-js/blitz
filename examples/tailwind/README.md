052300f09db619d14d0093e23e819e981d7b4d50
Brandon Bayer <b@bayer.ws>
2020-04-26 11:54:59 +0700

    Add Tailwind CSS example app

---
+++ examples/tailwind/README.md
@@ -0,0 +1,11 @@
+# Blitz Tailwind Example
+
+## Getting Started
+
+1. Start the dev server
+
+```
+blitz start
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
---
+++ examples/tailwind/blitz.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    return config
+  },
+  webpackDevMiddleware: (config) => {
+    // Perform customizations to webpack dev middleware config
+    // Important: return the modified config
+    return config
+  },
+}
