650504df87b7ff72d24d1ecc6de267db38eeb161
Simon Knott <info@simonknott.de>
2020-04-27 08:25:16 +0200

    Use `got` in npm-fetch to add retry characteristics

--- packages/cli/src/utils/npm-fetch.ts
+++ packages/cli/src/utils/npm-fetch.ts
@@ -1,26 +1,35 @@
-import fetch from 'node-fetch'
+import got from 'got'
+
+type PackageInformation = any
+type NpmDepResponse = {versions: Record<string, PackageInformation>}
 
 export const fetchAllVersions = async (dependency: string) => {
-  const res = await fetch(`https://registry.npmjs.org/${dependency}`)
-  if (res.ok) {
-    const json = await res.json()
-    return json.versions as string[]
-  } else {
-    return
+  try {
+    const res = await got(`https://registry.npmjs.org/${dependency}`, {
+      retry: {limit: 2},
+      responseType: 'json',
+    }).json<NpmDepResponse>()
+    return Object.keys(res.versions)
+  } catch (error) {
+    return undefined
   }
 }
 
+type NpmDistTagsResponse = {latest: string; canary: string}
+
 export const fetchDistTags = async (dependency: string) => {
-  const res = await fetch(`https://registry.npmjs.org/-/package/${dependency}/dist-tags`)
-  if (res.ok) {
-    const json = await res.json()
-    return json;
-  } else {
-    return
+  try {
+    const res = await got(`https://registry.npmjs.org/-/package/${dependency}/dist-tags`, {
+      retry: {limit: 2},
+      responseType: 'json',
+    }).json<NpmDistTagsResponse>()
+    return res
+  } catch (errror) {
+    return undefined
   }
 }
 
 export const fetchLatestDistVersion = async (dependency: string) => {
-  const res = await fetchDistTags(dependency);
-  return res?.latest;
+  const res = await fetchDistTags(dependency)
+  return res?.latest
 }
