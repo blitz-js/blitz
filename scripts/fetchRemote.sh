7db98c363613cbebc0a890f0daadf7d12584ad9c
Lori Karikari <lori.karikari@gmail.com>
2020-04-27 12:22:45 +0200

    changed btliz to blitz in fork script (#285)
    (ignore)

--- scripts/fetchRemote.sh
+++ scripts/fetchRemote.sh
@@ -4,7 +4,7 @@ set -e
 
 if [[ $(git remote -v | grep 'upstream' | wc -l) -eq 0 ]]; then
   echo "Upstream remote not found, adding one"
-  git remote add upstream https://github.com/btliz-js/blitz.git
+  git remote add upstream https://github.com/blitz-js/blitz.git
 fi
 
 git fetch upstream
