2f8a3bcac6f334d4a792e924af01d009ea0ea93f
Simon Knott <info@simonknott.de>
2020-04-27 09:04:21 +0200

    Add test for missing internet connection

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -3,6 +3,14 @@ import * as fs from "fs";
 import * as path from "path";
 import * as os from "os";
 import fetch from "node-fetch";
+import nock from "nock";
+
+nock("https://registry.npmjs.org")
+  .persist()
+  .get(() => true)
+  .reply(408);
+
+nock.restore();
 
 async function getLatestBlitzVersion() {
   const response = await fetch("https://registry.npmjs.org/-/package/blitz/dist-tags");
@@ -22,27 +30,40 @@ function makeTempDir() {
 describe("`new` command", () => {
   describe("when scaffolding new project", () => {
 
+    
+
     jest.setTimeout(60 * 1000);
 
-    let tempDir: string;
-    beforeAll(async () => {
-      tempDir = makeTempDir();
+    async function withNewApp(test: (dirName: string, packageJson: any) => Promise<void> | void) {
+      const tempDir = makeTempDir();
       await NewCmd.run([tempDir]);
-    });
 
-    afterAll(() => {
-      fs.rmdirSync(tempDir);
-    });
-
-    it("pins Blitz to the current version", async () => {
       const packageJsonFile = fs.readFileSync(
         path.join(tempDir, "package.json"),
         { encoding: "utf8", flag: "r" }
       );
       const packageJson = JSON.parse(packageJsonFile);
+
+      await test(tempDir, packageJson);
+
+      fs.rmdirSync(tempDir);
+    }
+
+    it("pins Blitz to the current version", () => withNewApp(async (_, packageJson) => {
       const { dependencies: { blitz: blitzVersion } } = packageJson;
 
       expect(blitzVersion).toEqual(await getLatestBlitzVersion());
+    }));
+
+    describe("with network trouble", () => {
+      it("uses template versions", async () => {
+        nock.activate();
+        await withNewApp((_, packageJson) => {
+          const { dependencies } = packageJson;
+          expect(dependencies.blitz).toBe("latest");
+        })
+        nock.restore();
+      })
     });
 
   })
