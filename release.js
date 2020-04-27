c2b85a747ee3bd35489331e12aaa4457bb36ce29
Brandon Bayer <b@bayer.ws>
2020-04-27 20:36:43 +0700

    more work on release.js

--- release.js
+++ release.js
@@ -2,5 +2,6 @@ module.exports = async (markdown, metaData) => {
   // Use the available data to create a custom release
   console.log(markdown)
   console.log(JSON.stringify(metaData))
+  throw new Error()
   return markdown
 }
