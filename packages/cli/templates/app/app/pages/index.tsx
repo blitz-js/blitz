4ff9aa44e1d3df3ee52aa2da127ca74eec6c71b5
merelinguist <merelinguist@users.noreply.github.com>
2020-04-28 17:08:25 +0100

    Clean generate snippets

--- packages/cli/templates/app/app/pages/index.tsx
+++ packages/cli/templates/app/app/pages/index.tsx
@@ -1,5 +1,13 @@
 import {Head, Link} from 'blitz'
 
+const modelSnippet = `model Project {
+  id      Int      @default(autoincrement()) @id
+  name    String
+}`
+
+const migrateSnippet = `$ blitz db migrate
+$ blitz generate all project`
+
 const Home = () => (
   <div className="container">
     <Head>
@@ -16,17 +24,13 @@ const Home = () => (
       </p>
       <pre>
         <code>
-          {`model Project {
-  id      Int      @default(autoincrement()) @id
-  name    String
-}`}
+          {modelSnippet}
         </code>
       </pre>
       <p>2. Run these commands in your terminal:</p>
       <pre>
         <code>
-          {`$ blitz db migrate
-$ blitz generate all project`}
+          {migrateSnippet}
         </code>
       </pre>
 
