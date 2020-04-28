59dfcddf497b6d467ecb4ed76484da97b9046000
Lori <lori.karikari@gmail.com>
2020-04-28 09:32:37 +0200

    fixed prettier

--- packages/cli/src/commands/db.ts
+++ packages/cli/src/commands/db.ts
@@ -1,17 +1,17 @@
-import { spawn } from 'cross-spawn'
-import { flags } from '@oclif/command'
-import { Command } from '@oclif/command'
+import {spawn} from 'cross-spawn'
+import {flags} from '@oclif/command'
+import {Command} from '@oclif/command'
 import chalk from 'chalk'
 import * as path from 'path'
-import { resolveBinAsync } from '@blitzjs/server'
-import { Client } from "pg"
+import {resolveBinAsync} from '@blitzjs/server'
+import {Client} from 'pg'
 import * as mysql from 'mysql2/promise'
-import * as fs from "fs"
-import { prompt } from "enquirer"
-import { promisify } from 'util'
+import * as fs from 'fs'
+import {prompt} from 'enquirer'
+import {promisify} from 'util'
 
 const envPath = path.join(process.cwd(), '.env')
-require('dotenv').config({ path: envPath })
+require('dotenv').config({path: envPath})
 
 const schemaPath = path.join(process.cwd(), 'db', 'schema.prisma')
 const schemaArg = `--schema=${schemaPath}`
@@ -20,187 +20,189 @@ const dbUrl = process.env.DATABASE_URL
 
 // Prisma client generation will fail if no model is defined in the schema.
 // So the silent option is here to ignore that failure
-export const runPrismaGeneration = async ({ silent = false } = {}) => {
-	const prismaBin = await getPrismaBin()
+export const runPrismaGeneration = async ({silent = false} = {}) => {
+  const prismaBin = await getPrismaBin()
 
-	return new Promise((resolve) => {
-		spawn(prismaBin, ['generate', schemaArg], { stdio: silent ? 'ignore' : 'inherit' }).on(
-			'exit',
-			(code: number) => {
-				if (code === 0) {
-					resolve()
-				} else if (silent) {
-					resolve()
-				} else {
-					process.exit(1)
-				}
-			},
-		)
-	})
+  return new Promise((resolve) => {
+    spawn(prismaBin, ['generate', schemaArg], {stdio: silent ? 'ignore' : 'inherit'}).on(
+      'exit',
+      (code: number) => {
+        if (code === 0) {
+          resolve()
+        } else if (silent) {
+          resolve()
+        } else {
+          process.exit(1)
+        }
+      },
+    )
+  })
 }
 
 export const runMigrate = async () => {
-	const prismaBin = await getPrismaBin()
+  const prismaBin = await getPrismaBin()
 
-	return new Promise((resolve) => {
-		const cp = spawn(prismaBin, ['migrate', 'save', schemaArg, '--create-db', '--experimental'], {
-			stdio: 'inherit',
-		})
-		cp.on('exit', (code: number) => {
-			if (code == 0) {
-				const cp = spawn(prismaBin, ['migrate', 'up', schemaArg, '--create-db', '--experimental'], {
-					stdio: 'inherit',
-				})
-				cp.on('exit', async (code: number) => {
-					if (code == 0) {
-						await runPrismaGeneration()
-						resolve()
-					} else {
-						process.exit(1)
-					}
-				})
-			} else {
-				process.exit(1)
-			}
-		})
-	})
+  return new Promise((resolve) => {
+    const cp = spawn(prismaBin, ['migrate', 'save', schemaArg, '--create-db', '--experimental'], {
+      stdio: 'inherit',
+    })
+    cp.on('exit', (code: number) => {
+      if (code == 0) {
+        const cp = spawn(prismaBin, ['migrate', 'up', schemaArg, '--create-db', '--experimental'], {
+          stdio: 'inherit',
+        })
+        cp.on('exit', async (code: number) => {
+          if (code == 0) {
+            await runPrismaGeneration()
+            resolve()
+          } else {
+            process.exit(1)
+          }
+        })
+      } else {
+        process.exit(1)
+      }
+    })
+  })
 }
 
 export async function resetPostgres(): Promise<void> {
-	const dbUrlParts: string[] = dbUrl!.split('/')
-	const dbName: string = dbUrlParts[dbUrlParts.length - 1]
-	const client: Client = new Client({
-		connectionString: dbUrl
-	})
-	await client.connect()
-	try {
-		// close all other connections
-		await client.query(`select pg_terminate_backend(pid) from pg_stat_activity where pid <> pg_backend_pid() and datname='${dbName}'`)
-		// currently assuming the public schema is being used
-		await client.query("drop schema public cascade; create schema public; grant all on schema public to postgres; grant all on schema public to public")
-		await runMigrate()
-	} catch (err) {
-		process.exit(1)
-	} finally {
-		await client.end()
-	}
+  const dbUrlParts: string[] = dbUrl!.split('/')
+  const dbName: string = dbUrlParts[dbUrlParts.length - 1]
+  const client: Client = new Client({
+    connectionString: dbUrl,
+  })
+  await client.connect()
+  try {
+    // close all other connections
+    await client.query(
+      `select pg_terminate_backend(pid) from pg_stat_activity where pid <> pg_backend_pid() and datname='${dbName}'`,
+    )
+    // currently assuming the public schema is being used
+    await client.query(
+      'drop schema public cascade; create schema public; grant all on schema public to postgres; grant all on schema public to public',
+    )
+    await runMigrate()
+  } catch (err) {
+    process.exit(1)
+  } finally {
+    await client.end()
+  }
 }
 
 export async function resetMysql(): Promise<void> {
-	const dbUrlParts: string[] = dbUrl!.split('/')
-	const dbName: string = dbUrlParts[dbUrlParts.length - 1]
-	const client = await mysql.createConnection(dbUrl)
-	try {
-		await client.query(`DROP DATABASE \`${dbName}\``)
-		await runMigrate()
-	} catch (err) {
-		process.exit(1)
-	} finally {
-		await client.end()
-	}
+  const dbUrlParts: string[] = dbUrl!.split('/')
+  const dbName: string = dbUrlParts[dbUrlParts.length - 1]
+  const client = await mysql.createConnection(dbUrl)
+  try {
+    await client.query(`DROP DATABASE \`${dbName}\``)
+    await runMigrate()
+  } catch (err) {
+    process.exit(1)
+  } finally {
+    await client.end()
+  }
 }
 
 export async function resetSqlite(): Promise<void> {
-	// currently assuming the database is located at the db folder
-	const dbPath: string = `db${dbUrl?.split('file:.').pop()}`
-	const unlink = promisify(fs.unlink)
-	try {
-		await unlink(dbPath)
-		await runMigrate()
-	} catch (err) {
-		process.exit(1)
-	}
+  // currently assuming the database is located at the db folder
+  const dbPath: string = `db${dbUrl?.split('file:.').pop()}`
+  const unlink = promisify(fs.unlink)
+  try {
+    await unlink(dbPath)
+    await runMigrate()
+  } catch (err) {
+    process.exit(1)
+  }
 }
 
 export default class Db extends Command {
-	static description = `Run database commands
+  static description = `Run database commands
 
 ${chalk.bold('migrate')}   Run any needed migrations via Prisma 2 and generate Prisma Client.
 
 ${chalk.bold(
-		'introspect',
-	)}   Will introspect the database defined in db/schema.prisma and automatically generate a complete schema.prisma file for you. Lastly, it\'ll generate Prisma Client.
+  'introspect',
+)}   Will introspect the database defined in db/schema.prisma and automatically generate a complete schema.prisma file for you. Lastly, it\'ll generate Prisma Client.
 
 ${chalk.bold(
-		'studio',
-	)}   Open the Prisma Studio UI at http://localhost:5555 so you can easily see and change data in your database.
+  'studio',
+)}   Open the Prisma Studio UI at http://localhost:5555 so you can easily see and change data in your database.
 
-${chalk.bold(
-		'reset',
-	)}   Reset the database and run a fresh migration via Prisma 2.
+${chalk.bold('reset')}   Reset the database and run a fresh migration via Prisma 2.
 `
 
-	static args = [
-		{
-			name: 'command',
-			description: 'Run specific db command',
-			required: true,
-		},
-	]
+  static args = [
+    {
+      name: 'command',
+      description: 'Run specific db command',
+      required: true,
+    },
+  ]
 
-	static flags = {
-		help: flags.help({ char: 'h' }),
-	}
+  static flags = {
+    help: flags.help({char: 'h'}),
+  }
 
-	async run() {
-		const { args } = this.parse(Db)
-		const command = args['command']
+  async run() {
+    const {args} = this.parse(Db)
+    const command = args['command']
 
-		const prismaBin = await getPrismaBin()
+    const prismaBin = await getPrismaBin()
 
-		if (command === 'migrate' || command === 'm') {
-			await runMigrate()
-		} else if (command === 'introspect') {
-			const cp = spawn(prismaBin, ['introspect', schemaArg], {
-				stdio: 'inherit',
-			})
-			cp.on('exit', (code: number) => {
-				if (code == 0) {
-					spawn(prismaBin, ['generate', schemaArg], { stdio: 'inherit' }).on('exit', (code: number) => {
-						if (code !== 0) {
-							process.exit(1)
-						}
-					})
-				} else {
-					process.exit(1)
-				}
-			})
-		} else if (command === 'studio') {
-			const cp = spawn(prismaBin, ['studio', schemaArg, '--experimental'], {
-				stdio: 'inherit',
-			})
-			cp.on('exit', (code: number) => {
-				if (code == 0) {
-				} else {
-					process.exit(1)
-				}
-			})
-		} else if (command === 'reset') {
-			if (typeof dbUrl !== "undefined") {
-				await prompt<{ confirm: string }>({
-					type: 'confirm',
-					name: 'confirm',
-					message: 'Are you sure you want to reset your database?'
-				}).then(res => {
-					if (res.confirm) {
-						if (dbUrl.includes('postgresql')) {
-							resetPostgres()
-						} else if (dbUrl.includes('mysql')) {
-							resetMysql()
-						} else if (dbUrl.includes('file:')) {
-							resetSqlite()
-						} else {
-							this.log("The database url is not valid.")
-						}
-					}
-				})
-			} else {
-				this.log("Add a database url to your .env file.")
-			}
-		} else {
-			this.log("\nUh oh, we don't support that command.")
-			this.log('You can try running a prisma command directly with:')
-			this.log('\n  `npm run prisma COMMAND` or `yarn prisma COMMAND`\n')
-		}
-	}
+    if (command === 'migrate' || command === 'm') {
+      await runMigrate()
+    } else if (command === 'introspect') {
+      const cp = spawn(prismaBin, ['introspect', schemaArg], {
+        stdio: 'inherit',
+      })
+      cp.on('exit', (code: number) => {
+        if (code == 0) {
+          spawn(prismaBin, ['generate', schemaArg], {stdio: 'inherit'}).on('exit', (code: number) => {
+            if (code !== 0) {
+              process.exit(1)
+            }
+          })
+        } else {
+          process.exit(1)
+        }
+      })
+    } else if (command === 'studio') {
+      const cp = spawn(prismaBin, ['studio', schemaArg, '--experimental'], {
+        stdio: 'inherit',
+      })
+      cp.on('exit', (code: number) => {
+        if (code == 0) {
+        } else {
+          process.exit(1)
+        }
+      })
+    } else if (command === 'reset') {
+      if (typeof dbUrl !== 'undefined') {
+        await prompt<{confirm: string}>({
+          type: 'confirm',
+          name: 'confirm',
+          message: 'Are you sure you want to reset your database?',
+        }).then((res) => {
+          if (res.confirm) {
+            if (dbUrl.includes('postgresql')) {
+              resetPostgres()
+            } else if (dbUrl.includes('mysql')) {
+              resetMysql()
+            } else if (dbUrl.includes('file:')) {
+              resetSqlite()
+            } else {
+              this.log('The database url is not valid.')
+            }
+          }
+        })
+      } else {
+        this.log('Add a database url to your .env file.')
+      }
+    } else {
+      this.log("\nUh oh, we don't support that command.")
+      this.log('You can try running a prisma command directly with:')
+      this.log('\n  `npm run prisma COMMAND` or `yarn prisma COMMAND`\n')
+    }
+  }
 }
