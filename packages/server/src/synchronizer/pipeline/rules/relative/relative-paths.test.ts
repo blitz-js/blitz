82e9bcb371c16d3eaa298b8df5013b0c62df2f7f
Justin Hall <justin.r.hall@gmail.com>
2020-05-02 01:15:27 -0600

    test(server): reduce noise in relative-paths tests

--- packages/server/src/synchronizer/pipeline/rules/relative/relative-paths.test.ts
+++ packages/server/src/synchronizer/pipeline/rules/relative/relative-paths.test.ts
@@ -1,21 +1,21 @@
 import {relativeToAbsolute, replaceRelativeImports} from './index'
 
+// I don't like this one bit, but Node's path module behaves so differently
+// based on the runtime platform that it's just easier to test paths as they
+// would be on the platform currently executing the test suite. Tests should
+// be run on both platforms, so both test paths will be executed - just not
+// during the same run.
+const platformSensitiveAbsolutePath = (p: string) =>
+  process.platform === 'win32' ? 'C:' + p.split('/').join('\\') : p
+
 describe('relativeToAbsolute', () => {
-  // I don't like this one bit, but Node's path module behaves so differently
-  // based on the runtime platform that it's just easier to test paths as they
-  // would be on the platform currently executing the test suite. Tests should
-  // be run on both platforms, so both test paths will be executed - just not
-  // during the same run.
-  const isWindows = process.platform === 'win32'
   const tests = [
     {
       name: 'Provides an absolute path within app',
       input: {
         relativeImport: '../components/three',
-        filename: isWindows
-          ? 'C:\\projects\\blitz\\blitz\\app\\users\\pages.ts'
-          : '/projects/blitz/blitz/app/users/pages.ts',
-        cwd: isWindows ? 'C:\\projects\\blitz\\blitz' : '/projects/blitz/blitz',
+        filename: platformSensitiveAbsolutePath('/projects/blitz/blitz/app/users/pages.ts'),
+        cwd: platformSensitiveAbsolutePath('/projects/blitz/blitz'),
       },
       expected: 'app/components/three',
     },
@@ -23,10 +23,8 @@ describe('relativeToAbsolute', () => {
       name: 'Works outside app',
       input: {
         relativeImport: '../../extras/foo',
-        filename: isWindows
-          ? 'C:\\projects\\blitz\\blitz\\app\\users\\pages.ts'
-          : '/projects/blitz/blitz/app/users/pages.ts',
-        cwd: isWindows ? 'C:\\projects\\blitz\\blitz' : '/projects/blitz/blitz',
+        filename: platformSensitiveAbsolutePath('/projects/blitz/blitz/app/users/pages.ts'),
+        cwd: platformSensitiveAbsolutePath('/projects/blitz/blitz'),
       },
       expected: 'extras/foo',
     },
@@ -34,10 +32,8 @@ describe('relativeToAbsolute', () => {
       name: 'Leaves absolute paths alone',
       input: {
         relativeImport: 'app/one/two',
-        filename: isWindows
-          ? 'C:\\projects\\blitz\\blitz\\app\\users\\pages.ts'
-          : '/projects/blitz/blitz/app/users/pages.ts',
-        cwd: isWindows ? 'C:\\projects\\blitz\\blitz' : '/projects/blitz/blitz',
+        filename: platformSensitiveAbsolutePath('/projects/blitz/blitz/app/users/pages.ts'),
+        cwd: platformSensitiveAbsolutePath('/projects/blitz/blitz'),
       },
       expected: 'app/one/two',
     },
