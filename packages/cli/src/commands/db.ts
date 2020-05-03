c198ce6e71763dd34e46d4a44aa6cbaff61550ec
Nabiullah elham <nabi009@yahoo.com>
2020-05-03 17:09:05 +0430

    update db.ts for #383
    I have changed in javascript, plz check typescript if I am not wrong so 
    
    https://github.com/blitz-js/blitz/issues/383
    
    exports.default = Db;
    Db.description =`Run database
    
    ${chalk_1.default.bold('👉 migrate')}   Run any needed migrations via Prisma 2 and generate Prisma Client.
    
    ${chalk_1.default.bold('👉 introspect')}   Will introspect the database defined in db/schema.prisma and automatically \ngenerate a complete schema.prisma file for you. Lastly, it\'ll generate Prisma Client.
    
    ${chalk_1.default.bold('👉 studio')}   Open the Prisma Studio UI at http://localhost:5555 so you can easily see and\n change data in your database.
    `;
    
    Db.args = [
    
        {
            name: 'Commands',
            description: Db.description,
            required: true,
        },
       
    ];

--- packages/cli/src/commands/db.ts
+++ packages/cli/src/commands/db.ts
@@ -124,25 +124,25 @@ export function getDbName(connectionString: string): string {
 }
 
 export default class Db extends Command {
-  static description = `Run database commands
+  static description = `Run database
 
-${chalk.bold('migrate')}   Run any needed migrations via Prisma 2 and generate Prisma Client.
-
-${chalk.bold(
-  'introspect',
-)}   Will introspect the database defined in db/schema.prisma and automatically generate a complete schema.prisma file for you. Lastly, it\'ll generate Prisma Client.
+${chalk.bold('👉 migrate')}   Run any needed migrations via Prisma 2 and generate Prisma Client.
 
 ${chalk.bold(
-  'studio',
-)}   Open the Prisma Studio UI at http://localhost:5555 so you can easily see and change data in your database.
+  '👉 introspect',
+)}   Will introspect the database defined in db/schema.prisma and automatically \ngenerate a complete schema.prisma file for you. Lastly, it\'ll generate Prisma Client.
 
-${chalk.bold('reset')}   Reset the database and run a fresh migration via Prisma 2.
+${chalk.bold(
+  '👉 studio',
+)}    Open the Prisma Studio UI at http://localhost:5555 so you can easily see and\n change data in your database.
+
+${chalk.bold('👉 reset')}   Reset the database and run a fresh migration via Prisma 2.
 `
 
   static args = [
     {
-      name: 'command',
-      description: 'Run specific db command',
+      name: 'Commands',
+      description: this.description,
       required: true,
     },
   ]
