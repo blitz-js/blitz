f450692e65a350bd7fc07a1b1279688911d76fd7
Oscar Sirvent <oscar.sirvent@gmail.com>
2020-05-04 12:48:32 +0200

    Fix postcss purge routes on Tailwind example (#381)
    (example)

--- examples/tailwind/tailwind.config.js
+++ examples/tailwind/tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  purge: [
+    "./**/{pages,components}/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
