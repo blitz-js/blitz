2b1d4897f8779c7f5570debb45269fa1006a61f8
Simon Knott <info@simonknott.de>
2020-05-06 09:56:09 +0200

    Extract fromEntries into its own function

--- packages/generator/src/utils/fetch-latest-version-for.ts
+++ packages/generator/src/utils/fetch-latest-version-for.ts
@@ -1,6 +1,12 @@
 import {getLatestVersion} from './get-latest-version'
 import {Fallbackable} from './fallbackable'
 
+function fromEntries<K extends number | string, V extends any>(entries: [K, V][]): Record<K, V> {
+  return entries.reduce<Record<K, V>>((result, [key, value]) => {
+    return Object.assign({}, result, {[key]: value})
+  }, {} as Record<K, V>)
+}
+
 export const fetchLatestVersionsFor = async <T extends Record<string, string>>(
   dependencies: T,
 ): Promise<Fallbackable<T>> => {
@@ -9,25 +15,25 @@ export const fetchLatestVersionsFor = async <T extends Record<string, string>>(
   let fallbackUsed = false
 
   const updated = await Promise.all(
-    entries.map(async ([dep, version]) => {
-      if (version.match(/\d.x/)) {
-        const {value: latestVersion, isFallback} = await getLatestVersion(dep, version)
+    entries.map(
+      async ([dep, version]): Promise<[string, string]> => {
+        if (version.match(/\d.x/)) {
+          const {value: latestVersion, isFallback} = await getLatestVersion(dep, version)
 
-        if (isFallback) {
-          fallbackUsed = true
+          if (isFallback) {
+            fallbackUsed = true
+          }
+
+          return [dep, latestVersion]
+        } else {
+          return [dep, version]
         }
-
-        return [dep, latestVersion]
-      } else {
-        return [dep, version]
-      }
-    }),
+      },
+    ),
   )
 
   return {
     isFallback: fallbackUsed,
-    value: updated.reduce((result, [key, value]) => {
-      return Object.assign({}, result, {[key]: value})
-    }, {} as T),
+    value: fromEntries(updated) as T,
   }
 }
