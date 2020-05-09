694d12c596967573884cf760789fde881e53f427
Simon Knott <info@simonknott.de>
2020-05-09 10:14:07 +0200

    Add self-rolled history for Node v10 (Closes #458)

--- packages/cli/src/commands/console.ts
+++ packages/cli/src/commands/console.ts
@@ -7,6 +7,7 @@ import {watch} from 'chokidar'
 import pkgDir from 'pkg-dir'
 import {log} from '@blitzjs/server'
 import chalk from 'chalk'
+import os from 'os'
 
 // import {loadDependencies} from '../utils/load-dependencies'
 import {setupTsnode, getBlitzModulePaths, loadBlitz} from '../utils/load-blitz'
@@ -65,18 +66,43 @@ export class Console extends Command {
       .on('exit', () => watchers.forEach((watcher) => watcher.close()))
   }
 
+  private static setupSelfRolledHistory(repl: any, path: string) {
+    function init() {
+      try {
+        const history = fs.readFileSync(path, {encoding: 'utf8'})
+        const nonEmptyLines = history.split(os.EOL).filter((line) => line.trim())
+        repl.history.push(...nonEmptyLines.reverse())
+      } catch (err) {
+        if (err.code !== 'ENOENT') {
+          throw err
+        }
+      }
+    }
+
+    function onExit() {
+      const addedHistory = repl.lines.join(os.EOL)
+      fs.appendFileSync(path, addedHistory)
+    }
+
+    init()
+    repl.on('exit', onExit)
+  }
+
+  private static setupHistory(repl: any) {
+    const blitzConsoleHistoryPath = path.join(projectRoot, '.blitz-console-history')
+    if (repl.setupHistory) {
+      repl.setupHistory(blitzConsoleHistoryPath, () => {})
+    } else {
+      this.setupSelfRolledHistory(repl, blitzConsoleHistoryPath)
+    }
+  }
+
   private static initializeRepl() {
     const repl = REPL.start(Console.replOptions)
     Console.defineCommands(repl, Console.commands)
     Console.loadModules(repl)
 
-    function setupHistoryIfAvailable(repl: any) {
-      if (repl.setupHistory) {
-        repl.setupHistory(path.join(projectRoot, '.blitz-console-history'), () => {})
-      }
-    }
-
-    setupHistoryIfAvailable(repl)
+    this.setupHistory(repl)
 
     return repl
   }
