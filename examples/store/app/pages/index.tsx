3f5b9f20eb4361fd3e33142f043b5e2cc821675a
Fran Zekan <zekan.fran369@gmail.com>
2020-05-12 17:26:50 +0200

    Test experiemental react

--- examples/store/app/pages/index.tsx
+++ examples/store/app/pages/index.tsx
@@ -11,6 +11,7 @@ const Home = () => (
       <h1 className="title" style={{marginBottom: 24}}>
         Blitz Store Example
       </h1>
+      <button onClick={() => console.log('Hello world')}>Button</button>
       <ul>
         <li>
           <Link href="/products">
