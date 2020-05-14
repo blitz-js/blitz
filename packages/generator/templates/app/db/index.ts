7c192362dd2eae83c6a74d2774759fa7c37e2cf5
Abu Uzayr <abu.uzayr@builtforfifty.com>
2020-05-15 00:33:34 +0800

    Fixes #485

--- packages/generator/templates/app/db/index.ts
+++ packages/generator/templates/app/db/index.ts
@@ -1,6 +1,15 @@
 import { PrismaClient } from "@prisma/client"
 export * from "@prisma/client"
 
-const prisma = new PrismaClient()
+let prisma
+
+if (process.env.NODE_ENV === "production") {
+    prisma = new PrismaClient()
+} else {
+    // Ensure the prisma instance is re-used during hot-reloading
+    // Otherwise, we will spawn a new client on every reload
+    global.prisma = global.prisma || new PrismaClient()
+    prisma = global.prisma
+}
 
 export default prisma
