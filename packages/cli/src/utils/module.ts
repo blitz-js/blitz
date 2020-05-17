133589dc19e5e8c1e5d4a28e4e80914d663e6160
Rafael Nunes <rafaelngds@gmail.com>
2020-05-17 17:09:32 +1000

    Makes console command call b@blitz/console

--- packages/cli/src/utils/module.ts
+++
@@ -1,8 +0,0 @@
-const invalidateCache = (module: string) => {
-  delete require.cache[require.resolve(module)]
-}
-
-export const forceRequire = (module: string) => {
-  invalidateCache(module)
-  return require(module)
-}
