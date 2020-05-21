2d8dd8da5d6470b2771b1739afd2e58ea8fb61c8
Brandon Bayer <b@bayer.ws>
2020-05-21 21:56:26 +0700

    Fix: remove `include` from "findMany" query template

--- packages/generator/templates/query/get__ModelNames__.ts
+++ packages/generator/templates/query/get__ModelNames__.ts
@@ -2,34 +2,31 @@ import db, {FindMany__ModelName__Args} from 'db'
 
 type Get__ModelNames__Input = {
   where: FindMany__ModelName__Args['where']
-  include?: FindMany__ModelName__Args['include']
   orderBy?: FindMany__ModelName__Args['orderBy']
   skip?: FindMany__ModelName__Args['skip']
-  after?: FindMany__ModelName__Args['after']
-  before?: FindMany__ModelName__Args['before']
   first?: FindMany__ModelName__Args['first']
   last?: FindMany__ModelName__Args['last']
+  after?: FindMany__ModelName__Args['after']
+  before?: FindMany__ModelName__Args['before']
 }
 
 export default async function get__ModelNames__({
   where,
-  include,
   orderBy,
   skip,
-  after,
-  before,
   first,
   last,
+  after,
+  before,
 }: Get__ModelNames__Input) {
   const __modelNames__ = await db.__modelName__.findMany({
     where,
-    include,
     orderBy,
     skip,
-    after,
-    before,
     first,
     last,
+    after,
+    before,
   })
 
   return __modelNames__
