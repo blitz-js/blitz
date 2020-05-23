edda912a2a53605c2b8e3dda1f43a5eb679446b4
Jeremy Liberman <mrleebo@msn.com>
2020-05-22 23:30:23 -0500

    Fix useQuery unit tests

--- packages/core/test/use-query.test.tsx
+++ packages/core/test/use-query.test.tsx
@@ -1,42 +1,59 @@
 import React from 'react'
-import {act, render, screen} from '@testing-library/react'
+import {act, render, waitForElementToBeRemoved, screen} from '@testing-library/react'
 import {useQuery} from '../src/use-query'
 
 describe('useQuery', () => {
-  const mockQuery = async (args: string): Promise<string> => {
-    return args.toUpperCase()
-  }
-
-  const setupHook = (params: any, queryFn = mockQuery): {data?: any} => {
+  const setupHook = (params: any, queryFn: (...args: any) => Promise<any>): [{data?: any}, Function] => {
     let res = {}
     function TestHarness() {
       useQuery(async (num: number) => num, 1)
       const [data] = useQuery(queryFn, params)
       Object.assign(res, {data})
-      return data ? <>Ready</> : <>Loading...</>
+      return <div id="harness">{data ? 'Ready' : 'Missing Dependency'}</div>
     }
-    render(
+
+    const ui = () => (
       <React.Suspense fallback="Loading...">
         <TestHarness />
-      </React.Suspense>,
+      </React.Suspense>
     )
-    return res
+
+    const {rerender} = render(ui())
+    return [res, () => rerender(ui())]
   }
 
-  it('should work', async () => {
-    const res = setupHook('test')
-    await act(async () => {
-      await screen.findByText('Ready')
-      expect(res.data).toBe('TEST')
+  describe('a "query" that converts the string parameter to uppercase', () => {
+    const upcase = async (args: string): Promise<string> => {
+      return args.toUpperCase()
+    }
+    it('should work', async () => {
+      const [res] = setupHook('test', upcase)
+      await waitForElementToBeRemoved(() => screen.getByText('Loading...'))
+      await act(async () => {
+        await screen.findByText('Ready')
+        expect(res.data).toBe('TEST')
+      })
     })
-  })
 
-  it('should work on dependent query', async () => {
-    // TODO: make this actually dependent when I get the fundamentals working
-    const res = setupHook(() => 'test')
-    await act(async () => {
-      await screen.findByText('Ready')
-      expect(res.data).toBe('TEST')
+    it('should work on dependent query', async () => {
+      // dependent queries are canceled if the params function throws
+      let params: Function = () => {
+        throw new Error('not ready yet')
+      }
+
+      const [res, rerender] = setupHook(() => params(), upcase)
+      await screen.findByText('Missing Dependency')
+
+      await act(async () => {
+        // simulates the dependency becoming available
+        params = () => 'test'
+        rerender()
+      })
+
+      await act(async () => {
+        await screen.findByText('Ready')
+        expect(res.data).toBe('TEST')
+      })
     })
   })
 })
