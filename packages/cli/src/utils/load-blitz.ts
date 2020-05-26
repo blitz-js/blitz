ee91ef2972ce457d361b129dc2e1a5aaadc561ca
Pierre Grimaud <grimaud.pierre@gmail.com>
2020-05-26 13:51:26 +0200

    Fix typos (#551)

--- packages/cli/src/utils/load-blitz.ts
+++ packages/cli/src/utils/load-blitz.ts
@@ -8,7 +8,7 @@ const projectRoot = pkgDir.sync() || process.cwd()
 
 export const setupTsnode = () => {
   if (!process[REGISTER_INSTANCE]) {
-    // During blitz interal dev, oclif automaticaly sets up ts-node so we have to check
+    // During blitz interal dev, oclif automatically sets up ts-node so we have to check
     require('ts-node').register({compilerOptions: {module: 'commonjs'}})
   }
   require('tsconfig-paths/register')
