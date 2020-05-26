d314248aadf14305feeb7997f7446053cfb1f98e
Enzo Ferey <enzo.ferey@chance.co>
2020-05-26 23:24:57 +0200

    Replace require with esmRequire.

--- packages/cli/src/utils/module.ts
+++ packages/cli/src/utils/module.ts
@@ -1,8 +1,10 @@
+const esmRequire = require('esm')(module)
+
 const invalidateCache = (module: string) => {
   delete require.cache[require.resolve(module)]
 }
 
 export const forceRequire = (module: string) => {
   invalidateCache(module)
-  return require(module)
+  return esmRequire(module)
 }
