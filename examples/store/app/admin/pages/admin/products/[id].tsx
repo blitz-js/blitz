3aa919e825017556e13ef847f6bff72e56b9a329
Ante Primorac <anteprimorac@users.noreply.github.com>
2020-05-30 06:17:30 +0200

    Fix `examples/store` code style issues (#580)
    (meta)

--- examples/store/app/admin/pages/admin/products/[id].tsx
+++ examples/store/app/admin/pages/admin/products/[id].tsx
@@ -1,14 +1,14 @@
-import {Suspense} from 'react'
-import {Link, useRouter, useQuery} from 'blitz'
-import getProduct from 'app/products/queries/getProduct'
-import ProductForm from 'app/products/components/ProductForm'
+import { Suspense } from "react"
+import { Link, useRouter, useQuery } from "blitz"
+import getProduct from "app/products/queries/getProduct"
+import ProductForm from "app/products/components/ProductForm"
 
 function Product() {
   const router = useRouter()
   const id = parseInt(router?.query.id as string)
-  const [product] = useQuery(getProduct, {where: {id}})
+  const [product] = useQuery(getProduct, { where: { id } })
 
-  return <ProductForm product={product} onSuccess={() => router.push('/admin/products')} />
+  return <ProductForm product={product} onSuccess={() => router.push("/admin/products")} />
 }
 
 function EditProductPage() {
