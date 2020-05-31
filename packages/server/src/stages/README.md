5bd9bba22ce90b3957d3067a759f319ad9377fba
Rudi Yardley <contact@rudiyardley.com>
2020-05-31 23:29:44 +1000

    Add Documentation

---
+++ packages/server/src/stages/README.md
@@ -0,0 +1,7 @@
+# Pipeline Stages for Blitz
+
+These are the business rule transformations that are run on sourcefiles while in transit during a Blitz Start or a Blitz Build.
+
+Each folder represents a business rule and should be separately applicable and not depend on each other.
+
+For more information see [@blitzjs/file-pipeline](../../../file-pipeline/README.md)
--- packages/server/src/stages/index.ts
+++ packages/server/src/stages/index.ts
@@ -4,6 +4,8 @@ import {createStageRpc} from './rpc'
 import {createStageConfig} from './config'
 import {createStageManifest} from './manifest'
 
+// These create pipeline stages that are run as the business rules for Blitz
+// Read this folders README for more information
 export const configureStages = (config: {writeManifestFile: boolean}) => [
   // Order is important
   createStageRelative,
