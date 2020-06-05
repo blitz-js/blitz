3b316448a7b682751594958b5ccb1a61e1269de2
Rudi Yardley <contact@rudiyardley.com>
2020-06-05 19:15:22 +1000

    Fix ready event bug (#616)
    * Apply transform to file-pipeline
    
    * Rewrite writer to correctly report that file has been deleted
    
    * Debug windows issues
    
    * Fix up action changes to run all tests
    
    * Fix bad merge
    
    * Fire ready event as string down pipeline
    
    * Fire ready event always
    
    * Add comment
    
    * Close stream in test
    
    * Debugging: Try skip live watching test
    
    * Don't create the watcher unless we are in watch mode
    
    * Update agnostic-source.test.ts
    
    * Wait for close promise to end before starting other test
    
    * Debug watcher limit issue
    
    * Try increasing watchers
    
    * Try increasing watchers after dependency installation
    
    * Only increase watchers in linux
    
    * Only increase watchers in linux
    
    * Fix bad os selection
    
    Co-authored-by: Brandon Bayer <b@bayer.ws>

---
+++ packages/file-pipeline/src/helpers/idle-handler/idle-handler.test.ts
@@ -0,0 +1,39 @@
+import {createIdleHandler} from '.'
+import {pipeline, through} from '../../streams'
+import {testStreamItems} from '../../test-utils'
+import {IDLE, READY} from '../../events'
+
+const sleep = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms))
+
+describe('idlehander', () => {
+  it('should fire the idle event', async () => {
+    // Setup an input stream
+    const input = through({objectMode: true}, (ev, __, next) => {
+      next(null, ev)
+    })
+
+    const bus = through({objectMode: true}, (ev, __, next) => {
+      next(null, ev)
+    })
+
+    // setup the test pipeline
+    const idleHandler = createIdleHandler(bus, 100)
+    pipeline(input, idleHandler.stream)
+
+    const arr = [1, 2, 3, 4]
+    for (const item of arr) {
+      input.write(item)
+    }
+    await sleep(150)
+    for (const item of arr) {
+      input.write(item)
+    }
+    input.write('ready')
+    await sleep(150)
+    for (const item of arr) {
+      input.write(item)
+    }
+
+    await testStreamItems(bus, [{type: IDLE}, {type: READY}, {type: IDLE}], (a) => a)
+  })
+})
