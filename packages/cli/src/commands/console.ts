3215b32b32309dafac999bba301a4a74ac6ff2be
Brandon Bayer <b@bayer.ws>
2020-06-06 16:07:07 +0700

    tiny improvement to `blitz console` help text

--- packages/cli/src/commands/console.ts
+++ packages/cli/src/commands/console.ts
@@ -25,7 +25,9 @@ export class Console extends Command {
     log.branded('You have entered the Blitz console')
     console.log(chalk.yellow('Tips: - Exit by typing .exit or pressing Ctrl-D'))
     console.log(chalk.yellow('      - Use your db like this: db.project.findMany().then(console.log)'))
-    console.log(chalk.yellow('      - Use your queries/mutations like this: getProjects().then(console.log)'))
+    console.log(
+      chalk.yellow('      - Use your queries/mutations like this: getProjects({}).then(console.log)'),
+    )
     console.log(
       chalk.yellow('      - Top level `await` support coming: https://github.com/blitz-js/blitz/issues/230'),
     )
