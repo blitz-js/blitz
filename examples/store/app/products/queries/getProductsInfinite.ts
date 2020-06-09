95b4b67703d7de8522b27e18b8e5f4a371012670
Oscar Sirvent <oscar.sirvent@gmail.com>
2020-06-09 16:11:54 +0200

    new Prisma 2.0.0 pagination on store example

--- examples/store/app/products/queries/getProductsInfinite.ts
+++ examples/store/app/products/queries/getProductsInfinite.ts
@@ -4,25 +4,23 @@ type GetProductsInput = {
   where?: FindManyProductArgs["where"]
   orderBy?: FindManyProductArgs["orderBy"]
   skip?: FindManyProductArgs["skip"]
-  first?: FindManyProductArgs["first"]
-  last?: FindManyProductArgs["last"]
-  after?: FindManyProductArgs["after"]
-  before?: FindManyProductArgs["before"]
+  cursor?: FindManyProductArgs["cursor"]
+  take?: FindManyProductArgs["take"]
   // Only available if a model relationship exists
   // include?: FindManyProductArgs['include']
 }
 
-export default async function getProducts({ where, orderBy, first, skip }: GetProductsInput) {
+export default async function getProducts({ where, orderBy, take, skip }: GetProductsInput) {
   const products = await db.product.findMany({
     where,
     orderBy,
-    first,
+    take,
     skip,
   })
 
   const count = await db.product.count()
-  const hasMore = skip + first < count
-  const nextPage = hasMore ? { first, skip: skip + first } : null
+  const hasMore = skip + take < count
+  const nextPage = hasMore ? { take, skip: skip + take } : null
 
   return {
     products,
