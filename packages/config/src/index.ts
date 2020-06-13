04dd60623888d520bfc8c459f290371d31765f5f
Brandon Bayer <b@bayer.ws>
2020-06-13 15:00:18 +0700

    few tweaks

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -2,26 +2,26 @@ import pkgDir from 'pkg-dir'
 import {join} from 'path'
 import {existsSync} from 'fs'
 
-const configFiles = ['blitz.config.js', 'next.config.js']
+const configFiles = ['next.config.js']
 /**
  * @param {boolean | undefined} reload - reimport config files to reset global cache
  */
 export const getConfig = async (reload?: boolean): Promise<Record<string, unknown>> => {
-  if (Object.keys(global.config).length > 0 && !reload) {
-    return global.config
+  if (Object.keys(global.blitzConfig).length > 0 && !reload) {
+    return global.blitzConfig
   }
 
-  let config = {}
+  let blitzConfig = {}
   const projectRoot = (await pkgDir()) || process.cwd()
 
   for (const configFile of configFiles) {
     if (existsSync(join(projectRoot, configFile))) {
       const file = require(join(projectRoot, configFile))
-      config = {...config, ...file}
+      blitzConfig = {...blitzConfig, ...file}
     }
   }
 
-  global.config = config
+  global.blitzConfig = blitzConfig
 
-  return config
+  return blitzConfig
 }
