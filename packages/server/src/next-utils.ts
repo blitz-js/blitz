4b45c05044ebe4f4ec7b6e7f759179635af441c4
mac <mac@macs-MacBook-Pro.local>
2020-06-13 12:16:37 +0530

    resolves the change request

--- packages/server/src/next-utils.ts
+++ packages/server/src/next-utils.ts
@@ -29,6 +29,21 @@ function createOutputTransformer(manifest: Manifest, devFolder: string) {
   return {stream}
 }
 
+async function createCommandAndPort(config: ServerConfig, command: string) {
+  let spawnCommand: string[] = [command]
+  let availablePort: number | undefined
+
+  if (config.port) {
+    availablePort = await detect({port: config.port})
+    spawnCommand = spawnCommand.concat(["-p", `${config.port}`])
+  }
+  if (config.hostname) {
+    spawnCommand = spawnCommand.concat(["-H", `${config.hostname}`])
+  }
+
+  return {spawnCommand, availablePort}
+}
+
 export async function nextStartDev(
   nextBin: string,
   cwd: string,
@@ -37,15 +52,7 @@ export async function nextStartDev(
   config: ServerConfig,
 ) {
   const transform = createOutputTransformer(manifest, devFolder).stream
-  let spawnCommand: string[] = ["dev"]
-  let availablePort: number
-  if (config.port) {
-    availablePort = await detect({port: config.port})
-    spawnCommand = spawnCommand.concat(["-p", `${config.port}`])
-  }
-  if (config.hostname) {
-    spawnCommand = spawnCommand.concat(["-H", `${config.hostname}`])
-  }
+  const {spawnCommand, availablePort} = await createCommandAndPort(config, "dev")
 
   return new Promise((res, rej) => {
     if (availablePort && availablePort !== config.port) {
@@ -77,15 +84,7 @@ export function nextBuild(nextBin: string, cwd: string) {
 }
 
 export async function nextStart(nextBin: string, cwd: string, config: ServerConfig) {
-  let spawnCommand: string[] = ["start"]
-  let availablePort: number
-  if (config.port) {
-    availablePort = await detect({port: config.port})
-    spawnCommand = spawnCommand.concat(["-p", `${config.port}`])
-  }
-  if (config.hostname) {
-    spawnCommand = spawnCommand.concat(["-H", `${config.hostname}`])
-  }
+  const {spawnCommand, availablePort} = await createCommandAndPort(config, "start")
 
   return new Promise((res, rej) => {
     if (availablePort && availablePort !== config.port) {
@@ -96,7 +95,7 @@ export async function nextStart(nextBin: string, cwd: string, config: ServerConf
         stdio: "inherit",
       })
         .on("exit", (code: number) => {
-          code === 0 ? res() : rej(`'next build' failed with status code: ${code}`)
+          code === 0 ? res() : rej(`'next start' failed with status code: ${code}`)
         })
         .on("error", (err) => {
           console.error(err)
