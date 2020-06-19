09fc2e10d61905b7f2732c1e38a744c496ef2a6e
Rafael Nunes <rafaelngds@gmail.com>
2020-06-19 12:11:46 +1000

    Fix page generator with custom context (#669)
    Co-authored-by: Rafael Nunes <rafaelgnds@gmail.com> (patch)

--- packages/cli/test/commands/generate.test.ts
+++ packages/cli/test/commands/generate.test.ts
@@ -2,24 +2,50 @@ import {Generate} from '../../src/commands/generate'
 import * as path from 'path'
 
 describe('`generate` command', () => {
-  it('properly extracts context from arguments', () => {
-    const getModelNameAndContext = Generate.prototype.getModelNameAndContext
-    expect(getModelNameAndContext('tasks', 'admin')).toEqual({
-      model: 'tasks',
-      context: 'admin',
+  describe('#getModelNameAndContext', () => {
+    it('properly extracts context from arguments', () => {
+      const getModelNameAndContext = Generate.prototype.getModelNameAndContext
+      expect(getModelNameAndContext('admin/tasks')).toEqual({
+        model: 'tasks',
+        context: 'admin',
+      })
+
+      expect(getModelNameAndContext('admin/projects/tasks')).toEqual({
+        model: 'tasks',
+        context: path.join('admin', 'projects'),
+      })
+
+      // this should fail on windows if generic filesystem-specific code makes it in
+      expect(getModelNameAndContext('admin\\projects\\tasks')).toEqual({
+        model: 'tasks',
+        context: path.join('admin', 'projects'),
+      })
     })
-    expect(getModelNameAndContext('admin/tasks')).toEqual({
-      model: 'tasks',
-      context: 'admin',
-    })
-    expect(getModelNameAndContext('admin/projects/tasks')).toEqual({
-      model: 'tasks',
-      context: path.join('admin', 'projects'),
-    })
-    // this should fail on windows if generic filesystem-specific code makes it in
-    expect(getModelNameAndContext('admin\\projects\\tasks')).toEqual({
-      model: 'tasks',
-      context: path.join('admin', 'projects'),
+
+    describe('when passing context', () => {
+      it('returns both model and context in their own field', () => {
+        const getModelNameAndContext = Generate.prototype.getModelNameAndContext
+        expect(getModelNameAndContext('tasks', 'admin')).toEqual({
+          model: 'tasks',
+          context: 'admin',
+        })
+      })
+
+      it('returns context undefined if not set', () => {
+        const getModelNameAndContext = Generate.prototype.getModelNameAndContext
+        expect(getModelNameAndContext('tasks')).toEqual({
+          model: 'tasks',
+          context: undefined,
+        })
+      })
+
+      it('returns context undefined if empty string', () => {
+        const getModelNameAndContext = Generate.prototype.getModelNameAndContext
+        expect(getModelNameAndContext('tasks', '')).toEqual({
+          model: 'tasks',
+          context: undefined,
+        })
+      })
     })
   })
 })
---
+++ packages/generator/test/generators/page-generator.test.ts
@@ -0,0 +1,98 @@
+import {PageGenerator} from '../../src/generators/page-generator'
+
+describe('PageGenerator', () => {
+  const generator = new PageGenerator({
+    ModelName: 'project',
+    ModelNames: 'projects',
+    modelName: 'project',
+    modelNames: 'projects',
+  })
+
+  describe('#getModelNamesPath', () => {
+    it('returns path only with default modelNames', () => {
+      expect(generator.getModelNamesPath()).toEqual('projects')
+    })
+
+    describe('when generator has context option', () => {
+      const generator = new PageGenerator({
+        ModelName: 'project',
+        ModelNames: 'projects',
+        modelName: 'project',
+        modelNames: 'projects',
+        context: 'marketing',
+      })
+
+      it('returns path with context as prefix', () => {
+        expect(generator.getModelNamesPath()).toEqual('marketing/projects')
+      })
+    })
+  })
+
+  describe('#getTemplateValues', () => {
+    it('returns object with correct template values', async () => {
+      const values = await generator.getTemplateValues()
+      expect(values).toEqual({
+        ModelName: 'project',
+        ModelNames: 'projects',
+        ParentModel: undefined,
+        ParentModels: undefined,
+        modelNamesPath: 'projects',
+        modelId: 'projectId',
+        modelIdParam: '[projectId]',
+        modelName: 'project',
+        modelNames: 'projects',
+        parentModel: undefined,
+        parentModelId: '',
+        parentModelParam: '',
+        parentModels: undefined,
+      })
+    })
+
+    describe('when generator has context option', () => {
+      const generator = new PageGenerator({
+        ModelName: 'project',
+        ModelNames: 'projects',
+        modelName: 'project',
+        modelNames: 'projects',
+        context: 'marketing',
+      })
+
+      it('returns path with context as prefix', async () => {
+        const values = await generator.getTemplateValues()
+        expect(values).toEqual(
+          expect.objectContaining({
+            modelNamesPath: 'marketing/projects',
+          }),
+        )
+      })
+    })
+
+    describe('when generator has parent option', () => {
+      const generator = new PageGenerator({
+        ModelName: 'project',
+        ModelNames: 'projects',
+        modelName: 'project',
+        modelNames: 'projects',
+        context: 'marketing',
+        parentModel: 'board',
+        parentModels: 'boards',
+        ParentModel: 'board',
+        ParentModels: 'boards',
+      })
+
+      it('returns path with context as prefix', async () => {
+        const values = await generator.getTemplateValues()
+        expect(values).toEqual(
+          expect.objectContaining({
+            parentModelId: 'boardId',
+            parentModelParam: '[boardId]',
+            parentModel: 'board',
+            parentModels: 'boards',
+            ParentModel: 'board',
+            ParentModels: 'boards',
+          }),
+        )
+      })
+    })
+  })
+})
