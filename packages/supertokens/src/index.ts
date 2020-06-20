daf200a56289f60eeabb046473820038094eb36c
Brandon Bayer <b@bayer.ws>
2020-06-20 22:04:12 +0700

    add middleware and session context

--- packages/supertokens/src/index.ts
+++ packages/supertokens/src/index.ts
@@ -1,23 +1,23 @@
-import invariant from 'tiny-invariant'
-import {v4 as uuidv4} from 'uuid'
-import hasha, {HashaInput} from 'hasha'
-import cookie from 'cookie'
-import {BlitzApiRequest, BlitzApiResponse} from '@blitzjs/core'
-import {addMinutes, isPast, differenceInMinutes} from 'date-fns'
+import invariant from "tiny-invariant"
+import {v4 as uuidv4} from "uuid"
+import hasha, {HashaInput} from "hasha"
+import cookie from "cookie"
+import {BlitzApiRequest, BlitzApiResponse, Middleware} from "@blitzjs/core"
+import {addMinutes, isPast, differenceInMinutes} from "date-fns"
 
-const TOKEN_SEPARATOR = ';'
-const HANDLE_SEPARATOR = ':'
-const SESSION_TYPE_OPAQUE_TOKEN_SIMPLE = 'ots'
-const SESSION_TOKEN_VERSION_0 = 'v0'
+const TOKEN_SEPARATOR = ";"
+const HANDLE_SEPARATOR = ":"
+const SESSION_TYPE_OPAQUE_TOKEN_SIMPLE = "ots"
+const SESSION_TOKEN_VERSION_0 = "v0"
 
-const COOKIE_SESSION_TOKEN = 'sSessionToken'
-const COOKIE_REFRESH_TOKEN = 'sIdRefreshToken'
+const COOKIE_SESSION_TOKEN = "sSessionToken"
+const COOKIE_REFRESH_TOKEN = "sIdRefreshToken"
 
-const HEADER_CSRF = 'anti-csrf'
-const HEADER_PUBLIC_DATA_TOKEN = 'public-data-token'
+const HEADER_CSRF = "anti-csrf"
+const HEADER_PUBLIC_DATA_TOKEN = "public-data-token"
 
 export interface PublicData extends Record<any, unknown> {
-  userId: string | number
+  userId: string | number | null
   role: string
 }
 export type PrivateData = Record<any, unknown>
@@ -35,8 +35,8 @@ type SessionModel = {
 
 const defaultConfig = {
   sessionExpiryMinutes: 100,
-  method: 'essential',
-  apiDomain: 'example.com',
+  method: "essential",
+  apiDomain: "example.com",
   async getSession(handle: string): Promise<SessionModel> {
     await true
     return {} as SessionModel
@@ -49,7 +49,7 @@ const defaultConfig = {
     await true
     return {} as SessionModel
   },
-  async updateSession(session: Partial<SessionModel> & {handle: string}): Promise<SessionModel> {
+  async updateSession(handle: string, session: Partial<SessionModel>): Promise<SessionModel> {
     await true
     return {} as SessionModel
   },
@@ -70,74 +70,85 @@ class AntiCSRFTokenMismatchException extends Error {
   // TODO implement constructor and custom name
 }
 
+export type SessionContext = {
+  /**
+   * null if anonymous
+   */
+  userId: string | null
+  role: string
+  handle: string
+  create: (arg: {publicData: PublicData; privateData?: PrivateData}) => Promise<SessionContext>
+  revoke: () => Promise<boolean>
+  getPrivateData: () => Promise<PrivateData>
+  setPrivateData: (data: PrivateData) => Promise<void>
+  getPublicData: () => Promise<PublicData>
+  // TODO
+  // regenerate: (arg: {publicData: PublicData}) => Promise<SessionContext>
+}
+
 // --------------------------------
-// Session Class
+// Middleware
 // --------------------------------
-class Session {
-  private sessionHandle: string | undefined
-  private publicData: PublicData | undefined
-  public userId: string | undefined
-  public role: string | undefined
+export const sessionMiddleware: Middleware = async (req, res, next) => {
+  try {
+    res.blitzCtx.session = await createSessionContextFromRequest(req, res)
+    return next()
+  } catch (err) {
+    // if (Session.Error.isUnauthorized(err)) {
+    //   if (Session.Error.isAntiCSRFTokenFailed(err)) {
+    //     throw err
+    //   }
+    // } else {
+    //   throw err
+    // }
+  }
+}
 
-  constructor(sessionHandle?: string, publicData?: PublicData) {
-    if (sessionHandle) {
-      this.sessionHandle = sessionHandle
-    }
-    if (publicData) {
-      this.publicData = publicData
-      this.userId = publicData.userId
-      this.role = publicData.role
-    }
+export async function createSessionContextFromRequest(
+  req: BlitzApiRequest,
+  res: BlitzApiResponse,
+): Promise<SessionContext> {
+  let session = await getSession(req, res)
+
+  if (!session) {
+    session = await createAnonymousSession(res)
   }
 
-  create(publicData, privateData) {
-    // TODO:
-  }
+  return createSessionContext(res, session)
+}
 
-  revoke() {
-    if (this.sessionHandle !== undefined) {
-      // TODO:
-    }
-  }
-
-  getPrivateData() {
-    if (this.sessionHandle !== undefined) {
-      // TODO:
-    } else {
-      throw Error('no session exists')
-    }
-  }
-
-  setPrivateData() {
-    if (this.sessionHandle !== undefined) {
-      // TODO:
-    } else {
-      throw Error('no session exists')
-    }
-  }
-
-  getPublicData() {
-    if (this.sessionHandle !== undefined) {
-      // TODO:
-    } else {
-      throw Error('no session exists')
-    }
-  }
-
-  setPublicData() {
-    if (this.sessionHandle !== undefined) {
-      // TODO:
-    } else {
-      throw Error('no session exists')
-    }
+export function createSessionContext(
+  res: BlitzApiResponse,
+  {
+    handle,
+  }: {
+    handle: string
+    publicData: PublicData
+  },
+): SessionContext {
+  return {
+    //TODO - where does userId come from?
+    userId: null,
+    //TODO - where does role come from?
+    role: "public",
+    handle,
+    create: async ({publicData, privateData}) => {
+      return createSessionContext(res, await createNewSession(res, publicData, privateData))
+    },
+    revoke: () => revokeSession(handle),
+    getPrivateData: () => getPrivateData(handle),
+    setPrivateData: (data) => setPrivateData(handle, data),
+    getPublicData: () => getPublicData(handle),
+    // TODO
+    // regenerate: () => {},
   }
 }
 
 // --------------------------------
 // General utils
 // --------------------------------
-const base64 = (input: HashaInput) => hasha(input, {encoding: 'base64'})
-const hash = (input: HashaInput) => hasha(input, {algorithm: 'sha256'})
+const base64 = (input: HashaInput) => hasha(input, {encoding: "base64"})
+const hash = (input: HashaInput) => hasha(input, {algorithm: "sha256"})
 
 // --------------------------------
 // Session utils
@@ -146,19 +157,24 @@ export const generateEssentialSessionHandle = () => {
   return uuidv4() + HANDLE_SEPARATOR + SESSION_TYPE_OPAQUE_TOKEN_SIMPLE
 }
 
-export const createSessionToken = (sessionHandle: string, publicData: PublicData) => {
+export const createSessionToken = (handle: string, publicData: PublicData | string) => {
   // We store the hashed public data in the opaque token so that when we verify,
   // we can detect changes in it and return a new set of tokens if necessary.
+
+  let publicDataString
+  if (typeof publicData === "string") {
+    publicDataString = publicData
+  } else {
+    publicDataString = JSON.stringify(publicData)
+  }
   return base64(
-    [sessionHandle, uuidv4(), hash(JSON.stringify(publicData)), SESSION_TOKEN_VERSION_0].join(
-      TOKEN_SEPARATOR,
-    ),
+    [handle, uuidv4(), hash(publicDataString), SESSION_TOKEN_VERSION_0].join(TOKEN_SEPARATOR),
   )
 }
 export const parseSessionToken = (token: string) => {
-  const [sessionHandle, id, hashedPublicData, version] = token.split(TOKEN_SEPARATOR)
+  const [handle, id, hashedPublicData, version] = token.split(TOKEN_SEPARATOR)
   return {
-    sessionHandle,
+    handle,
     id,
     hashedPublicData,
     version,
@@ -174,15 +190,19 @@ export const createAntiCSRFToken = () => uuidv4()
 export const setSessionCookie = (res: BlitzApiResponse, sessionToken: string, expiresAt: Date) => {
   // TODO - what if another middlware use `Set-Cookie`?
   res.setHeader(
-    'Set-Cookie',
+    "Set-Cookie",
     cookie.serialize(COOKIE_SESSION_TOKEN, sessionToken, {
       //domain - can we omit?
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: process.env.NODE_ENV === "production",
       sameSite: true,
       expires: expiresAt,
     }),
   )
+}
+
+export async function createAnonymousSession(res: BlitzApiResponse) {
+  return await createNewSession(res, {userId: null, role: "public"})
 }
 
 // --------------------------------
@@ -195,14 +215,16 @@ export async function createNewSession(
 ) {
   const config = defaultConfig
 
-  invariant(publicData.userId, 'You must publicData.userId')
-  invariant(publicData.role, 'You must publicData.role')
+  invariant(publicData.userId, "You must publicData.userId")
+  invariant(publicData.role, "You must publicData.role")
 
+  // Ensure userId is a string within the session library
   const userId = publicData.userId.toString()
 
-  if (config.method === 'essential') {
+  if (config.method === "essential") {
+    // TODO handle anonymous
     const {
-      sessionHandle,
+      handle,
       sessionToken,
       antiCSRFToken,
       publicDataToken,
@@ -214,11 +236,11 @@ export async function createNewSession(
     res.setHeader(HEADER_PUBLIC_DATA_TOKEN, publicDataToken)
 
     return {
-      sessionHandle,
+      handle,
       publicData,
     }
-  } else if (config.method === 'essential') {
-    throw new Error('The advanced method is not yet supported')
+  } else if (config.method === "essential") {
+    throw new Error("The advanced method is not yet supported")
   } else {
     throw new Error(
       `Session management method ${config.method} is invalid. Supported methods are "essential" and "advanced"`,
@@ -235,14 +257,14 @@ export async function createNewSessionHelper(
 
   const createdAt = new Date()
   const expiresAt = addMinutes(createdAt, config.sessionExpiryMinutes)
-  const sessionHandle = generateEssentialSessionHandle()
-  const sessionToken = createSessionToken(sessionHandle, publicData)
+  const handle = generateEssentialSessionHandle()
+  const sessionToken = createSessionToken(handle, publicData)
   const publicDataToken = createPublicDataToken(JSON.stringify(publicData), expiresAt)
   const antiCSRFToken = createAntiCSRFToken()
 
   try {
     await config.createSession({
-      handle: sessionHandle,
+      handle,
       userId,
       hashedSessionToken: hash(sessionToken),
       antiCSRFToken,
@@ -256,7 +278,7 @@ export async function createNewSessionHelper(
   }
 
   return {
-    sessionHandle,
+    handle,
     sessionToken,
     antiCSRFToken,
     publicDataToken,
@@ -264,38 +286,49 @@ export async function createNewSessionHelper(
   }
 }
 
-export function getSession(req: BlitzApiRequest, res: BlitzApiResponse, enableCsrfProtection: boolean) {
-  let sessionToken = req.cookies[COOKIE_SESSION_TOKEN] // for essential method
-  let idRefreshToken = req.cookies[COOKIE_REFRESH_TOKEN] // for advanced method
+export async function getSession(req: BlitzApiRequest, res: BlitzApiResponse) {
+  const enableCsrfProtection = req.method !== "GET" && req.method !== "OPTIONS"
+  const sessionToken = req.cookies[COOKIE_SESSION_TOKEN] // for essential method
+  const idRefreshToken = req.cookies[COOKIE_REFRESH_TOKEN] // for advanced method
 
   if (sessionToken === undefined && idRefreshToken === undefined) {
-    throw new AuthenticationError('Missing session tokens. Please login again')
+    // No session exists
+    return null
   }
 
   if (sessionToken) {
     let antiCSRFToken = req.headers[HEADER_CSRF] as string
-    let {sessionHandle, publicData, newSessionToken, newPublicDataToken, newExpiresAt} = getSessionHelper(
+    let {
+      handle,
+      publicData,
+      newSessionToken,
+      newPublicDataToken,
+      newExpiresAt,
+    } = await getSessionHelper(
       sessionToken,
       antiCSRFToken,
       enableCsrfProtection,
       req.method as string,
     )
+
     if (newSessionToken && newExpiresAt) {
       setSessionCookie(res, sessionToken, newExpiresAt)
     }
     if (newPublicDataToken) {
       res.setHeader(HEADER_PUBLIC_DATA_TOKEN, newPublicDataToken)
     }
+
     return {
-      sessionHandle,
+      handle,
       publicData,
     }
   } else {
     // TODO: advanced method
+    return null
   }
 }
 
-export function getSessionHelper(
+export async function getSessionHelper(
   sessionToken: string,
   inputAntiCSRFToken: string | undefined,
   enableCsrfProtection: boolean,
@@ -303,13 +336,13 @@ export function getSessionHelper(
 ) {
   const config = defaultConfig
 
-  const {sessionHandle, version, hashedPublicData} = parseSessionToken(sessionToken)
+  const {handle, version, hashedPublicData} = parseSessionToken(sessionToken)
 
   if (version !== SESSION_TOKEN_VERSION_0) {
-    throw new AuthenticationError('Session token is not ' + SESSION_TOKEN_VERSION_0)
+    throw new AuthenticationError("Session token is not " + SESSION_TOKEN_VERSION_0)
   }
 
-  let session = config.getSession(sessionHandle)
+  let session = await config.getSession(handle)
 
   if (!session) {
     throw new AuthenticationError("Input session ID doesn't exist")
@@ -321,7 +354,7 @@ export function getSessionHelper(
     throw new AuthenticationError("Input session ID doesn't exist")
   }
   if (isPast(session.expiresAt)) {
-    revokeSession(sessionHandle)
+    await revokeSession(handle)
     throw new AuthenticationError("Input session ID doesn't exist")
   }
 
@@ -333,7 +366,7 @@ export function getSessionHelper(
   // Only renew with non-GET requests
   // We can't reliably update the frontend expiry information in GET requests since they could
   // be from a browser level navigation (for example: user opening the site after a long time).
-  if (httpMethod !== 'GET') {
+  if (httpMethod !== "GET") {
     // Check if > 1/4th of the expiry time has passed
     // (since we are doing a rolling expiry window).
     quarterExpiryTimePassed =
@@ -345,13 +378,14 @@ export function getSessionHelper(
   if (publicDataChanged || quarterExpiryTimePassed) {
     newExpiresAt = addMinutes(new Date(), config.sessionExpiryMinutes)
     newPublicDataToken = createPublicDataToken(session.publicData, newExpiresAt)
-    newSessionToken = createSessionToken(sessionHandle, session.publicData)
+    newSessionToken = createSessionToken(handle, session.publicData)
 
     // should not wait for this to happen
-    updateSessionExpiryInDb(sessionHandle, newExpiresAt)
+    // eslint-disable-next-line
+    updateSessionExpiryInDb(handle, newExpiresAt)
   }
   return {
-    sessionHandle,
+    handle,
     publicData: JSON.parse(session.publicData),
     newSessionToken,
     newPublicDataToken,
@@ -363,9 +397,9 @@ export function refreshSession(req: BlitzApiRequest, res: BlitzApiResponse) {
   // TODO: advanced method
 }
 
-export async function updateSessionExpiryInDb(sessionHandle: string, expiresAt: Date) {
+export async function updateSessionExpiryInDb(handle: string, expiresAt: Date) {
   const config = defaultConfig
-  return await config.updateSession({handle: sessionHandle, expiresAt})
+  return await config.updateSession(handle, {expiresAt})
 }
 
 export async function getAllSessionHandlesForUser(userId: string) {
@@ -373,10 +407,10 @@ export async function getAllSessionHandlesForUser(userId: string) {
   return (await config.getSessions(userId)).map((session) => session.handle)
 }
 
-export async function revokeSession(sessionHandle: string): Promise<boolean> {
+export async function revokeSession(handle: string): Promise<boolean> {
   const config = defaultConfig
 
-  return await config.deleteSession(sessionHandle)
+  return await config.deleteSession(handle)
 }
 
 export function revokeMultipleSessions(sessionHandles: string[]) {
@@ -395,51 +429,53 @@ export async function revokeAllSessionsForUser(userId: string) {
   return revokeMultipleSessions(sessionHandles)
 }
 
-export async function getPublicData(sessionHandle: string): Promise<PublicData> {
+export async function getPublicData(handle: string): Promise<PublicData> {
   const config = defaultConfig
-  const session = await config.getSession(sessionHandle)
+  const session = await config.getSession(handle)
   if (!session) {
-    throw new AuthenticationError("sessionHandle doesn't exist")
+    throw new AuthenticationError("handle doesn't exist")
   }
   return JSON.parse(session.publicData) as PublicData
 }
 
-export async function getPrivateData(sessionHandle: string): Promise<PrivateData> {
+export async function getPrivateData(handle: string): Promise<PrivateData> {
   const config = defaultConfig
-  const session = await config.getSession(sessionHandle)
+  const session = await config.getSession(handle)
   if (!session) {
-    throw new AuthenticationError("sessionHandle doesn't exist")
+    throw new AuthenticationError("handle doesn't exist")
   }
   return JSON.parse(session.privateData) as PrivateData
 }
 
-export function setPrivateData(sessionHandle: string, data: PrivateData) {
-  const newData: PrivateData = {
-    ...getPrivateData(sessionHandle),
-    ...data,
-  }
-  // update <table> set sessionData = newData where session_handle = sessionHandle;
-  // TODO
-  // let numberOfRowsUpdated = updateInDb(sessionHandle, JSON.stringify(newData))
-  const numberOfRowsUpdated = 1
-  if (numberOfRowsUpdated === 0) {
-    throw new AuthenticationError("sessionHandle doesn't exist")
+export async function setPrivateData(handle: string, data: PrivateData) {
+  const config = defaultConfig
+
+  try {
+    const privateData = JSON.stringify({
+      ...(await getPrivateData(handle)),
+      ...data,
+    })
+    await config.updateSession(handle, {privateData})
+  } catch (error) {
+    // TODO handle error
+    throw error
   }
 }
 
-export function setPublicData(sessionHandle: string, data: Omit<PublicData, 'useId'>) {
+export async function setPublicData(handle: string, data: Omit<PublicData, "userId">) {
   // Don't allow updating userId
   delete data.userId
 
-  const newData: PrivateData = {
-    ...getPrivateData(sessionHandle),
-    ...data,
-  }
-  // update <table> set sessionData = newData where session_handle = sessionHandle;
-  // TODO
-  // let numberOfRowsUpdated = updateInDb(sessionHandle, JSON.stringify(newData))
-  const numberOfRowsUpdated = 1
-  if (numberOfRowsUpdated === 0) {
-    throw new AuthenticationError("sessionHandle doesn't exist")
+  const config = defaultConfig
+
+  try {
+    const publicData = JSON.stringify({
+      ...(await getPublicData(handle)),
+      ...data,
+    })
+    await config.updateSession(handle, {publicData})
+  } catch (error) {
+    // TODO handle error
+    throw error
   }
 }
