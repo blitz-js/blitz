0bfa501ad084823b9ee409e964af67041cb34489
Brandon Bayer <b@bayer.ws>
2020-06-20 14:36:46 +0700

    try again to disable screenshot

--- examples/store/cypress/support/index.js
+++ examples/store/cypress/support/index.js
@@ -12,7 +12,6 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
-import * as Cypress from "cypress"
 
 // Import commands.js using ES2015 syntax:
 import "./commands"
