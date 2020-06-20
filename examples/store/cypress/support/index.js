f79ae76a98b5bd533904625ce5a10b2c081993de
Brandon Bayer <b@bayer.ws>
2020-06-20 14:27:29 +0700

    disable cypress screenshots because it causes random test failures

--- examples/store/cypress/support/index.js
+++ examples/store/cypress/support/index.js
@@ -12,9 +12,14 @@
 // You can read more here:
 // https://on.cypress.io/configuration
 // ***********************************************************
+import * as Cypress from "cypress"
 
 // Import commands.js using ES2015 syntax:
 import "./commands"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+Cypress.Screenshot.defaults({
+  screenshotOnRunFailure: false,
+})
