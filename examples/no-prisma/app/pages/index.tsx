956743d797abea488749de42103ccd885cc77694
Simon Knott <info@simonknott.de>
2020-07-23 11:53:01 +0200

    Add some beautiful CSS

--- examples/no-prisma/app/pages/index.tsx
+++ examples/no-prisma/app/pages/index.tsx
@@ -1,9 +1,9 @@
-import { Suspense } from "react"
+import { Suspense, useState } from "react"
 import { useQuery } from "blitz"
 import getGIFs from "app/queries/getGIFs"
 import addRating from "app/mutations/addRating"
 import addGIF from "app/mutations/addGIF"
-import { useState } from "react"
+import styles from "./index.module.css"
 
 function GIFRater() {
   const [gifs, gifsQuery] = useQuery(getGIFs, {})
@@ -11,7 +11,7 @@ function GIFRater() {
   const [gifTitle, setGifTitle] = useState<string>()
 
   return (
-    <main>
+    <main className={styles.main}>
       <h1>The best GIFs of all time:</h1>
 
       <form>
@@ -27,35 +27,42 @@ function GIFRater() {
             await addGIF(gifTitle)
             await gifsQuery.refetch()
           }}
-        ></button>
+        >
+          Submit
+        </button>
       </form>
 
-      <ul>
+      <ul className={styles.gif_list}>
         {gifs.map((gif) => (
           <li key={gif.url}>
             <img src={gif.url} alt="GIF" />
-            Rating: {gif.rating}
-            <select
-              placeholder="Change"
-              defaultValue="title"
-              onBlur={async (evt) => {
-                await addRating({
-                  gif_url: gif.url,
-                  rating: +evt.target.value as 1 | 2 | 3 | 4 | 5,
-                })
+            <span>
+              Rating: {gif.rating.toFixed(1)}
+              <select
+                placeholder="Change"
+                defaultValue="title"
+                onBlur={async (evt) => {
+                  const rating = +evt.target.value as 1 | 2 | 3 | 4 | 5
+                  evt.target.value = "title"
 
-                await gifsQuery.refetch()
-              }}
-            >
-              <option disabled value="title">
-                Rate
-              </option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
+                  await addRating({
+                    gif_url: gif.url,
+                    rating,
+                  })
+
+                  await gifsQuery.refetch()
+                }}
+              >
+                <option disabled value="title">
+                  Add Rating
+                </option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </span>
           </li>
         ))}
       </ul>
