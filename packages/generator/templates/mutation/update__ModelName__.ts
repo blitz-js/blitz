aca4ec10e944e2009580d38eaec535517dcaa88e
Simon Edelmann <admin@simonedelmann.de>
2020-07-23 15:08:19 +0200

    Update update__ModelName__.ts

--- packages/generator/templates/mutation/update__ModelName__.ts
+++ packages/generator/templates/mutation/update__ModelName__.ts
@@ -17,8 +17,6 @@ export default async function update__ModelName__(
   {where, data}: Update__ModelName__Input,
   ctx: Record<any, any> = {},
 ) {
-  // Don't allow updating
-  delete data.id
   if (process.env.parentModel) {
     delete (data as any).__parentModel__
   }
