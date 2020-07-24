c0ac5d93fab86b86926c3b0e0159571b155c28a3
Brandon Bayer <b@bayer.ws>
2020-07-24 22:30:42 +0700

    abstract adapter

--- examples/auth/app/api/auth/[...auth].ts
+++ examples/auth/app/api/auth/[...auth].ts
@@ -6,103 +6,120 @@ import {
   Middleware,
   SessionContext,
 } from "blitz"
-import passport from "passport"
+import passport, {Strategy} from "passport"
 import {Strategy as TwitterStrategy} from "passport-twitter"
-import invariant from "tiny-invariant"
 import cookieSession from "cookie-session"
 import db from "db"
 
-export default async function authHandler(req: BlitzApiRequest, res: BlitzApiResponse) {
-  const middleware: Middleware[] = [
-    connectMiddleware(
-      cookieSession({
-        secret: process.env.SESSION_SECRET_KEY || "default-dev-secret",
-        secure: process.env.NODE_ENV === "production",
-      }),
+function assert(condition: any, message: string): asserts condition {
+  if (!condition) throw new Error(message)
+}
+
+// These aren't required, but this is a good practice to ensure you have the env vars you need
+assert(process.env.TWITTER_CONSUMER_KEY, "You must provide the TWITTER_CONSUMER_KEY env variable")
+assert(
+  process.env.TWITTER_CONSUMER_SECRET,
+  "You must provide the TWITTER_CONSUMER_SECRET env variable",
+)
+
+export default passportAuth({
+  successRedirectUrl: "/",
+  strategies: [
+    new TwitterStrategy(
+      {
+        consumerKey: process.env.TWITTER_CONSUMER_KEY,
+        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+        callbackURL:
+          process.env.NODE_ENV === "production"
+            ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/twitter/callback"
+            : "http://localhost:3000/api/auth/twitter/callback",
+        includeEmail: true,
+      },
+      async function (_token, _tokenSecret, profile, done) {
+        const email = profile.emails && profile.emails[0]?.value
+
+        if (!email) {
+          // This can happen if you haven't enabled email access in your twitter app permissions
+          return done(new Error("Twitter OAuth response doesn't have email."))
+        }
+
+        const user = await db.user.upsert({
+          where: {email},
+          create: {
+            email,
+            name: profile.displayName,
+          },
+          update: {email},
+        })
+
+        const publicData = {userId: user.id, roles: [user.role], source: "twitter"}
+        done(null, publicData)
+      },
     ),
-    connectMiddleware(passport.initialize()),
-    connectMiddleware(passport.session()),
-  ]
+  ],
+})
 
-  const protocol = process.env.NODE_ENV === "production" ? "https://" : "http://"
-  const origin = protocol + req.headers.host
-  console.log("Origin:", origin)
+type BlitzPassportConfig = {
+  // TODO - support function that receives publicData
+  successRedirectUrl?: string
+  errorRedirectUrl?: string
+  strategies: Required<Strategy>[]
+}
 
-  if (req.url?.includes("twitter")) {
-    invariant(
-      process.env.TWITTER_CONSUMER_KEY,
-      "You must provide the TWITTER_CONSUMER_KEY env variable",
-    )
-    invariant(
-      process.env.TWITTER_CONSUMER_SECRET,
-      "You must provide the TWITTER_CONSUMER_SECRET env variable",
-    )
-
-    console.log("QUERY", req.query)
-
-    const callbackURL = origin + "/api/auth/twitter/callback"
-
-    passport.use(
-      new TwitterStrategy(
-        {
-          consumerKey: process.env.TWITTER_CONSUMER_KEY,
-          consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-          callbackURL,
-          includeEmail: true,
-        },
-        async function (token, tokenSecret, profile, done) {
-          // console.log("\nCALLBACK", profile)
-
-          const email = profile.emails && profile.emails[0]?.value
-          if (!email) {
-            return done(
-              new Error(
-                "The Twitter OAuth response doesn't have an email. Make sure you have enabled email access in your app permissions inside the Twitter developer dashboard",
-              ),
-            )
-          }
-
-          const user = await db.user.upsert({
-            where: {email},
-            create: {
-              email,
-              name: profile.displayName,
-            },
-            update: {email},
-          })
-
-          const publicData = {userId: user.id, roles: [user.role], source: "twitter"}
-          done(null, publicData)
-        },
+function passportAuth(config: BlitzPassportConfig) {
+  return async function authHandler(req: BlitzApiRequest, res: BlitzApiResponse) {
+    const middleware: Middleware[] = [
+      connectMiddleware(
+        cookieSession({
+          secret: process.env.SESSION_SECRET_KEY || "default-dev-secret",
+          secure: process.env.NODE_ENV === "production",
+        }),
       ),
+      connectMiddleware(passport.initialize()),
+      connectMiddleware(passport.session()),
+    ]
+
+    if (!req.query.auth.length) {
+      return res.status(404).end()
+    }
+
+    assert(
+      config.strategies.length,
+      "No Passport strategies found! Please add at least one strategy.",
     )
-    if (req.query.auth.length === 1 && req.query.auth[0] === "twitter") {
-      console.log("Authenticating via twitter...")
-      middleware.push(connectMiddleware(passport.authenticate("twitter")))
-    } else if (req.query.auth[0] === "twitter" && req.query.auth[1] === "callback") {
-      console.log("Twitter auth callback...")
+
+    const strategy = config.strategies.find((strategy) => strategy.name === req.query.auth[0])
+    assert(strategy, `A passport strategy was not found for: ${req.query.auth[0]}`)
+
+    passport.use(strategy)
+
+    if (req.query.auth.length === 1) {
+      console.log(`Starting authentication via ${strategy.name}...`)
+      middleware.push(connectMiddleware(passport.authenticate(strategy.name)))
+    } else if (req.query.auth[1] === "callback") {
+      console.log(`Processing callback for ${strategy.name}`)
       middleware.push(
         connectMiddleware((req, res, next) => {
-          const session = res.blitzCtx.session as SessionContext
-          if (!session) throw new Error("Missing Blitz sessionMiddleware!")
+          // TODO fix type
+          const session = (res as any).blitzCtx.session as SessionContext
+          assert(session, "Missing Blitz sessionMiddleware!")
 
-          passport.authenticate("twitter", async (err, user, info) => {
+          passport.authenticate(strategy.name, async (err, user) => {
             if (err) {
               // TODO - redirect back to origin with error
               throw err
             }
-            if (!user) throw new Error("Twitter OAuth: No user found")
-
+            assert(user, `No user found in callback from ${strategy.name}!`)
             await session.create(user)
-            res.setHeader("Location", `/`)
-            res.status(302).end()
+
+            res.setHeader("Location", config.successRedirectUrl || "/")
+            // TODO fix type
+            ;(res as any).status(302).end()
           })(req, res, next)
         }),
       )
     }
-  } else {
-    return res.status(404).end()
-  }
 
-  await handleRequestWithMiddleware(req, res, middleware)
+    await handleRequestWithMiddleware(req, res, middleware)
+  }
 }
