5463a310ff55ddf0ada1bc977f98ddc2d689f509
Brandon Bayer <b@bayer.ws>
2020-07-27 21:36:34 +0700

    add message

--- packages/core/src/errors.ts
+++ packages/core/src/errors.ts
@@ -1,19 +1,24 @@
 export class CSRFTokenMismatchError extends Error {
   name = "CSRFTokenMismatchError"
-  statusCode = 401 // Unauthorized
+  statusCode = 401
+  message = "Unauthorized"
 }
 
 export class AuthenticationError extends Error {
   name = "AuthenticationError"
-  statusCode = 401 // Unauthorized
+  statusCode = 401
+  message = "Unauthorized"
 }
 
 export class AuthorizationError extends Error {
   name = "AuthorizationError"
-  statusCode = 403 // Forbidden
+  statusCode = 403
+  message = "Forbidden"
 }
 
 export class NotFoundError extends Error {
   name = "NotFoundError"
   statusCode = 404
+  message = "Not Found"
+  // TODO - allow custom message
 }
