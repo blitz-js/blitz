0125306e978e3d9c8e02d4916e577281ff8a6550
Brandon Bayer <b@bayer.ws>
2020-07-28 09:22:09 +0700

    fix test

--- packages/installer/test/utils/paths.test.ts
+++ packages/installer/test/utils/paths.test.ts
@@ -1,22 +1,20 @@
 import {paths} from "@blitzjs/installer"
 import * as fs from "fs-extra"
 
+jest.mock("fs-extra")
+
 describe("path utils", () => {
-  it("returns proper file paths", () => {
-    // ---------------
-    // IN A TS PROJECT
-    // ---------------
-    jest.spyOn(fs, "existsSync").mockReturnValue(true)
+  it("returns proper file paths in a TS project", () => {
+    fs.existsSync.mockReturnValue(true)
     expect(paths.document()).toBe("app/pages/_document.tsx")
     expect(paths.app()).toBe("app/pages/_app.tsx")
     expect(paths.entry()).toBe("app/pages/index.tsx")
     // blitz config is always JS, we shouldn't transform this extension
     expect(paths.blitzConfig()).toBe("blitz.config.js")
+  })
 
-    // ---------------
-    // IN A JS PROJECT
-    // ---------------
-    jest.spyOn(fs, "existsSync").mockReturnValue(false)
+  it("returns proper file paths in a JS project", () => {
+    fs.existsSync.mockReturnValue(false)
     expect(paths.document()).toBe("app/pages/_document.js")
     expect(paths.app()).toBe("app/pages/_app.js")
     expect(paths.entry()).toBe("app/pages/index.js")
