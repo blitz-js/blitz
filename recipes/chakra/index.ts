7509aca08ddc817fa4b1ed586af190b6c45353dc
Fran Zekan <zekan.fran369@gmail.com>
2020-08-04 23:38:11 +0200

    Add Blitz recipe for Chakra (#830)
    Co-authored-by: Brandon Bayer <b@bayer.ws> (minor)

---
+++ recipes/chakra/index.ts
@@ -0,0 +1,84 @@
+import {RecipeBuilder, paths, addImport} from "@blitzjs/installer"
+import {builders} from "ast-types/gen/builders"
+import {ASTNode} from "ast-types/lib/types"
+import {NamedTypes} from "ast-types/gen/namedTypes"
+import {visit} from "ast-types"
+
+// Copied from https://github.com/blitz-js/blitz/pull/805, let's add this to the @blitzjs/installer
+function wrapComponentWithThemeProvider(ast: ASTNode, b: builders, t: NamedTypes) {
+  if (!t.File.check(ast)) return
+
+  visit(ast, {
+    visitExportDefaultDeclaration(path) {
+      return this.traverse(path)
+    },
+    visitJSXElement(path) {
+      const {node} = path
+      if (
+        t.JSXIdentifier.check(node.openingElement.name) &&
+        // TODO: need a better way to detect the Component
+        node.openingElement.name.name === "Component"
+      ) {
+        path.replace(
+          b.jsxElement(
+            b.jsxOpeningElement(b.jsxIdentifier("ThemeProvider")),
+            b.jsxClosingElement(b.jsxIdentifier("ThemeProvider")),
+            [
+              b.literal("\n  \t  "),
+              b.jsxElement(b.jsxOpeningElement(b.jsxIdentifier("CSSReset"), [], true)),
+              b.literal("\n  \t  "),
+              node,
+              b.literal("\n    "),
+            ],
+          ),
+        )
+        return false
+      }
+      return this.traverse(path)
+    },
+  })
+
+  return ast
+}
+
+export default RecipeBuilder()
+  .setName("Chakra UI")
+  .setDescription(
+    `Configure your Blitz app's styling with Chakra UI. This recipe will install all necessary dependencies and configure Chakra UI for immediate use.`,
+  )
+  .setOwner("zekan.fran369@gmail.com")
+  .setRepoLink("https://github.com/blitz-js/blitz")
+  .addAddDependenciesStep({
+    stepId: "addDeps",
+    stepName: "Add npm dependencies",
+    explanation: `Chakra requires some other dependencies like emotion to work`,
+    packages: [
+      {name: "@chakra-ui/core", version: "latest"},
+      {name: "@emotion/core", version: "latest"},
+      {name: "@emotion/styled", version: "latest"},
+      {name: "emotion-theming", version: "latest"},
+    ],
+  })
+  .addTransformFilesStep({
+    stepId: "importProviderAndReset",
+    stepName: "Import ThemeProvider and CSSReset component",
+    explanation: `We can import the chakra provider into _app, so it is accessibly in the whole app`,
+    singleFileSearch: paths.app(),
+    transform(ast: ASTNode, b: builders, t: NamedTypes) {
+      const stylesImport = b.importDeclaration(
+        [
+          b.importSpecifier(b.identifier("CSSReset")),
+          b.importSpecifier(b.identifier("ThemeProvider")),
+        ],
+        b.literal("@chakra-ui/core"),
+      )
+
+      if (t.File.check(ast)) {
+        addImport(ast, b, t, stylesImport)
+        return wrapComponentWithThemeProvider(ast, b, t)!
+      }
+
+      throw new Error("Not given valid source file")
+    },
+  })
+  .build()
