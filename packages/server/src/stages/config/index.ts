1ea31c6d63e2a5af6d8eb572f0ec16d54e79c9a7
Simon Knott <info@simonknott.de>
2020-08-04 09:32:24 +0200

    Remove check for Vercel
    This shouldn't be required since Blitz.js is now supported natively by Vercel, right?

--- packages/server/src/stages/config/index.ts
+++ packages/server/src/stages/config/index.ts
@@ -6,7 +6,6 @@ import {transform} from "@blitzjs/file-pipeline"
 import {Stage} from "@blitzjs/file-pipeline"
 
 const isNextConfigPath = (p: string) => /next\.config\.(js|ts)/.test(p)
-const isNowBuild = () => process.env.NOW_BUILDER || process.env.VERCEL_BUILDER
 /**
  * Returns a Stage that manages converting from blitz.config.js to next.config.js
  */
@@ -15,7 +14,7 @@ export const createStageConfig: Stage = ({config, input}) => {
   const hasNextConfig = pathExistsSync(resolve(config.src, "next.config.js"))
   const hasBlitzConfig = pathExistsSync(resolve(config.src, "blitz.config.js"))
 
-  if (hasNextConfig && !isNowBuild()) {
+  if (hasNextConfig) {
     // TODO: Pause the stream and ask the user if they wish to have their configuration file renamed
     const err = new Error(
       "Blitz does not support next.config.js. Please rename your next.config.js to blitz.config.js",
@@ -35,19 +34,17 @@ export const createStageConfig: Stage = ({config, input}) => {
     )
   }
 
-  if (!hasNextConfig) {
-    input.write(
-      new File({
-        cwd: config.src,
-        path: resolve(config.src, "next.config.js"),
-        contents: Buffer.from(`
+  input.write(
+    new File({
+      cwd: config.src,
+      path: resolve(config.src, "next.config.js"),
+      contents: Buffer.from(`
 const {withBlitz} = require('@blitzjs/server');
 const config = require('./blitz.config.js');
 module.exports = withBlitz(config);
-        `),
-      }),
-    )
-  }
+      `),
+    }),
+  )
 
   // No need to filter yet
   const stream = transform.file((file) => {
