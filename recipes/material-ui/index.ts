f97c1f68682be0e64629024a3edf7859b7fba1bb
Soumyajit Pathak <s.pathak5995@gmail.com>
2020-08-12 03:29:38 +0530

    add custom _document getInitialProps

--- recipes/material-ui/index.ts
+++ recipes/material-ui/index.ts
@@ -29,6 +29,184 @@ export default RecipeBuilder()
     templateValues: {},
   })
   .addTransformFilesStep({
+    stepId: "modifyGetInitialPropsInCustomDocumentApp",
+    stepName: "Add custom getInitialProps logic in Custom Document",
+    explanation: `We need to do this so that styles are correctly rendered on the server side.`,
+    singleFileSearch: paths.document(),
+    transform(ast: ASTNode, b: builders, t: NamedTypes) {
+      // import ServerStyleSheets
+      const serverStyleSheetsImport = b.importDeclaration(
+        [b.importSpecifier(b.identifier("ServerStyleSheets"))],
+        b.literal("@material-ui/core/styles"),
+      )
+
+      let isReactImported = false
+
+      visit(ast, {
+        visitImportDeclaration(path) {
+          // check if React is already imported
+          // if yes then we can skip importing it
+          // since we need it for useEffect
+          const source = path.value.source.value
+          if (source === "react") {
+            isReactImported = true
+
+            // currently, we only check if the default export is there
+            // because we use the hook as React.useEffect
+            // if not then add the default export
+            if (
+              !path.value.specifiers.some(
+                (node: namedTypes.ImportDefaultSpecifier | namedTypes.ImportSpecifier) =>
+                  node.type === "ImportDefaultSpecifier",
+              )
+            ) {
+              path.value.specifiers.splice(0, 0, b.importDefaultSpecifier(b.identifier("React")))
+
+              return false
+            }
+          }
+          // check if DocumentContext is imported from blitz if not add that as well
+          else if (source === "blitz") {
+            if (
+              !path.value.specifiers
+                .filter(
+                  (node: namedTypes.ImportDefaultSpecifier | namedTypes.ImportSpecifier) =>
+                    node.type === "ImportSpecifier",
+                )
+                .some(
+                  (node: namedTypes.ImportSpecifier) => node?.imported?.name === "DocumentContext",
+                )
+            ) {
+              path.value.specifiers.splice(0, 0, b.importSpecifier(b.identifier("DocumentContext")))
+            }
+          }
+
+          return this.traverse(path)
+        },
+        visitClassBody(path) {
+          const {node} = path
+
+          const ctxParam = b.identifier("ctx")
+          ctxParam.typeAnnotation = b.tsTypeAnnotation(
+            b.tsTypeReference(b.identifier("DocumentContext")),
+          )
+
+          const getInitialPropsBody = b.blockStatement([
+            b.variableDeclaration("const", [
+              b.variableDeclarator(
+                b.identifier("sheets"),
+                b.newExpression(b.identifier("ServerStyleSheets"), []),
+              ),
+            ]),
+            b.variableDeclaration("const", [
+              b.variableDeclarator(
+                b.identifier("originalRenderPage"),
+                b.memberExpression(b.identifier("ctx"), b.identifier("renderPage")),
+              ),
+            ]),
+            b.expressionStatement(
+              b.assignmentExpression(
+                "=",
+                b.memberExpression(b.identifier("ctx"), b.identifier("renderPage")),
+                b.arrowFunctionExpression(
+                  [],
+                  b.callExpression(b.identifier("originalRenderPage"), [
+                    b.objectExpression([
+                      b.objectProperty(
+                        b.identifier("enhanceApp"),
+                        b.arrowFunctionExpression(
+                          [b.identifier("App")],
+                          b.arrowFunctionExpression(
+                            [b.identifier("props")],
+                            b.callExpression(
+                              b.memberExpression(b.identifier("sheets"), b.identifier("collect")),
+                              [
+                                b.jsxElement(
+                                  b.jsxOpeningElement(
+                                    b.jsxIdentifier("App"),
+                                    [b.jsxSpreadAttribute(b.identifier("props"))],
+                                    true,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ]),
+                  ]),
+                ),
+              ),
+            ),
+            b.variableDeclaration("const", [
+              b.variableDeclarator(
+                b.identifier("initialProps"),
+                b.awaitExpression(
+                  b.callExpression(
+                    b.memberExpression(b.identifier("Document"), b.identifier("getInitialProps")),
+                    [b.identifier("ctx")],
+                  ),
+                ),
+              ),
+            ]),
+            b.returnStatement(
+              b.objectExpression([
+                b.spreadElement(b.identifier("initialProps")),
+                b.objectProperty(
+                  b.identifier("styles"),
+                  b.arrayExpression([
+                    b.spreadElement(
+                      b.callExpression(
+                        b.memberExpression(
+                          b.memberExpression(b.identifier("React"), b.identifier("Children")),
+                          b.identifier("toArray"),
+                        ),
+                        [b.memberExpression(b.identifier("initialProps"), b.identifier("styles"))],
+                      ),
+                    ),
+                    b.callExpression(
+                      b.memberExpression(b.identifier("sheets"), b.identifier("getStyleElement")),
+                      [],
+                    ),
+                  ]),
+                ),
+              ]),
+            ),
+          ])
+
+          const getInitialPropsMethod = b.classMethod(
+            "method",
+            b.identifier("getInitialProps"),
+            [ctxParam],
+            getInitialPropsBody,
+            false,
+            true,
+          )
+          getInitialPropsMethod.async = true
+
+          // TODO: better way will be to check if the method already exists and modify it or else add it
+          // currently it gets added assuming it did not exist before
+          node.body.splice(0, 0, getInitialPropsMethod)
+
+          return false
+        },
+      })
+
+      // import React if it wasn't already imported
+      if (!isReactImported) {
+        const reactImport = b.importDeclaration(
+          [b.importDefaultSpecifier(b.identifier("React"))],
+          b.literal("react"),
+        )
+        addImport(ast, b, t, reactImport)
+      }
+
+      addImport(ast, b, t, serverStyleSheetsImport)
+
+      return ast
+    },
+  })
+  .addTransformFilesStep({
     stepId: "importThemeProviderInCustomApp",
     stepName:
       "Customize App and import ThemeProvider with the base theme and CssBaseline component",
@@ -209,7 +387,7 @@ export default RecipeBuilder()
             const {value} = path
 
             // TODO: there may be a better way to do this
-            // currently only handle adding the experimental reactMode legacy
+            // currently only handle adding { experimental: { reactMode: 'legacy' } } directly
             // doesn't check if any part of the tree already exists
             if (
               value.type === "AssignmentExpression" &&
