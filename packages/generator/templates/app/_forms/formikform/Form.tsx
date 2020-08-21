71cb60539b4128d7e562abc5bc7a0d4931833d08
Brady Pascoe <18705892+bpas247@users.noreply.github.com>
2020-08-20 22:45:20 -0700

    feat(app-generator): add formik as option for new apps
    Adds Formik as an option for users looking to create their new
    app with Formik example code.
    
    Fixes #918

---
+++ packages/generator/templates/app/_forms/formikform/Form.tsx
@@ -0,0 +1,84 @@
+import React, { useState, ReactNode, PropsWithoutRef } from "react";
+import { Formik, FormikProps, FormikErrors } from "formik";
+import * as z from "zod";
+
+type FormProps<FormValues> = {
+  /** All your form fields */
+  children: ReactNode;
+  /** Text to display in the submit button */
+  submitText: string;
+  onSubmit: (values: FormValues) => Promise<void | OnSubmitResult>;
+  initialValues?: FormikProps<FormValues>["initialValues"];
+  schema?: z.ZodType<any, any>;
+} & Omit<PropsWithoutRef<JSX.IntrinsicElements["form"]>, "onSubmit">;
+
+type OnSubmitResult = {
+  FORM_ERROR?: string
+  [prop: string]: any
+}
+
+export const FORM_ERROR = "FORM_ERROR"
+
+export function Form<FormValues extends Record<string, unknown>>({
+  children,
+  submitText,
+  schema,
+  initialValues,
+  onSubmit,
+  ...props
+}: FormProps<FormValues>) {
+  const [formError, setFormError] = useState<string | null>(null);
+  return (
+    <Formik<FormValues>
+      initialValues={initialValues || {} as FormValues}
+      validate={(values) => {
+        if (!schema) return;
+        try {
+          schema.parse(values);
+        } catch (error) {
+          return error.formErrors.fieldErrors;
+        }
+      }}
+      onSubmit={async (values, {setErrors}) => {
+        const {FORM_ERROR, ...otherErrors} = (await onSubmit(values as FormValues)) || {}
+
+        if(FORM_ERROR) {
+          setFormError(FORM_ERROR);
+        } 
+
+        if(Object.keys(otherErrors).length > 0) {
+          setErrors(otherErrors as FormikErrors<FormValues>)
+        }         
+      }}
+    >
+      {({ handleSubmit, isSubmitting, values, setErrors }) => (
+        <form
+          onSubmit={handleSubmit}
+          className="form"
+          {...props}
+        >
+          {/* Form fields supplied as children are rendered here */}
+          {children}
+
+          {formError && (
+            <div role="alert" style={{ color: "red" }}>
+              {formError}
+            </div>
+          )}
+
+          <button type="submit" disabled={isSubmitting}>
+            {submitText}
+          </button>
+
+          <style global jsx>{`
+            .form > * + * {
+              margin-top: 1rem;
+            }
+          `}</style>
+        </form>
+      )}
+    </Formik>
+  );
+}
+
+export default Form;
