b77cf3b1d6dd1525fc5513c11ffa3ae7170dd406
George Karagkiaouris <gkaragkiaouris2@gmail.com>
2020-08-23 11:22:26 -0400

    Move code below comment

--- packages/generator/templates/app/app/pages/index.test.tsx
+++ packages/generator/templates/app/app/pages/index.test.tsx
@@ -8,6 +8,10 @@ jest.mock("app/hooks/useCurrentUser")
 const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>;
 
 test.skip("renders blitz documentation link", () => {
+  // This is an example of how to ensure a specific item is in the document
+  // But it's disabled by default (by test.skip) so the test doesn't fail
+  // when you remove the the default content from the page
+
   // This is an example on how to mock api hooks when testing
   mockUseCurrentUser.mockReturnValue({
     id: 1,
@@ -16,9 +20,6 @@ test.skip("renders blitz documentation link", () => {
     role: 'user'
   });
 
-  // This is an example of how to ensure a specific item is in the document
-  // But it's disabled by default (by test.skip) so the test doesn't fail
-  // when you remove the the default content from the page
   const { getByText } = render(<Home />)
   const linkElement = getByText(/Documentation/i)
   expect(linkElement).toBeInTheDocument()
