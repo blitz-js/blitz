0fa73b557065edda52f15707ca039712666cdf93
Brandon Bayer <b@bayer.ws>
2020-09-01 15:06:07 -0400

    update

--- examples/auth/app/hooks/useCurrentUser.ts
+++ examples/auth/app/hooks/useCurrentUser.ts
@@ -4,7 +4,6 @@ import getCurrentUser from "app/users/queries/getCurrentUser"
 export const useCurrentUser = () => {
   // We wouldn't have to useSession() here, but doing so improves perf on initial
   // load since we can skip the getCurrentUser() request.
-  // It also ensures that everything re-renders once the session expires
   const session = useSession()
   const [user] = useQuery(getCurrentUser, null, {enabled: !!session.userId})
   return session.userId ? user : null
