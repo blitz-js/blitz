9e888f904b693247f0d4b6cd0d0b1028297a080b
Brandon Bayer <b@bayer.ws>
2020-09-10 10:45:01 -0400

    add to new app template

---
+++ packages/generator/templates/app/types.ts
@@ -0,0 +1,8 @@
+import { DefaultAuthTypes } from "blitz"
+import { User } from "db"
+
+declare module "blitz" {
+  export interface AuthTypes extends DefaultAuthTypes {
+    userId: User["id"]
+  }
+}
