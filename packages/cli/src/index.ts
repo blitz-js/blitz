ab3021a3713b3bac990cd2ffce19898f768faa72
Brandon Bayer <b@bayer.ws>
2020-09-14 20:15:50 -0400

    Significantly improve CLI performance: reduce execute time from 1700ms  -> 900ms (#1057)
    (patch)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,3 +1,6 @@
+require("v8-compile-cache")
+const lazyLoad = require("@salesforce/lazy-require").default.create(".blitz-cli-cache")
+lazyLoad.start()
 import {run as oclifRun} from "@oclif/command"
 
 // Load the .env environment variable so it's available for all commands
