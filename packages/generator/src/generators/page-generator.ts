d42166c7ccee1ef93ed901bb2f0aefc6c248069e
Patrick Engelkes <patrick.engelkes@finstreet.de>
2020-09-15 15:55:03 +0200

    1022: Add utility function for camelCaseToKebabCase and use this function to change generate pages directory to use kebabCase.

--- packages/generator/src/generators/page-generator.ts
+++ packages/generator/src/generators/page-generator.ts
@@ -1,5 +1,6 @@
 import {Generator, GeneratorOptions} from "../generator"
 import {join} from "path"
+import {camelCaseToKebabCase} from "../utils/kebab-case"
 
 export interface PageGeneratorOptions extends GeneratorOptions {
   ModelName: string
@@ -51,9 +52,10 @@ export class PageGenerator extends Generator<PageGeneratorOptions> {
   }
 
   getTargetDirectory() {
+    const kebabCaseModelName = camelCaseToKebabCase(this.options.modelNames)
     const parent = this.options.parentModels
       ? `${this.options.parentModels}/__parentModelParam__/`
       : ""
-    return `app/${this.getModelNamesPath()}/pages/${parent}${this.options.modelNames}`
+    return `app/${this.getModelNamesPath()}/pages/${parent}${kebabCaseModelName}`
   }
 }
