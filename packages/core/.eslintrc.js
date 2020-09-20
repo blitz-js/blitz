d9383dfd7b5e39ee0666943bf7750bac610c1e82
Avinash <avinash@avinash.com.np>
2020-09-20 12:10:06 +0545

    add rule to prevent Object.fromEntries in core package

--- packages/core/.eslintrc.js
+++ packages/core/.eslintrc.js
@@ -1,8 +1,9 @@
 module.exports = {
   extends: ["../../.eslintrc.js"],
-  plugins: ["es5"],
+  plugins: ["es5", "es"],
   rules: {
     "es5/no-for-of": "error",
+    "es/no-object-fromentries": "error",
     "es5/no-generators": "error",
     "es5/no-typeof-symbol": "error",
     "es5/no-es6-methods": "error",
