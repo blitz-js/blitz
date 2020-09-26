88da5e0ad3d16d55ab79f8d4b92d344c897b4405
Brandon Bayer <b@bayer.ws>
2020-09-26 15:21:02 -0400

    clean up unused files in auth example

--- examples/auth/app/users/queries/getUser.ts
+++ examples/auth/app/users/queries/getUser.ts
@@ -3,20 +3,18 @@ import {SessionContext, NotFoundError} from "blitz"
 
 type GetUserInput = {
   where: FindOneUserArgs["where"]
-  select?: FindOneUserArgs["select"]
   // Only available if a model relationship exists
   // include?: FindOneUserArgs['include']
 }
 
-export default async function getUser(
-  {where, select}: GetUserInput,
-  ctx: {session?: SessionContext} = {},
-) {
+export default async function getUser({where}: GetUserInput, ctx: {session?: SessionContext} = {}) {
   ctx.session?.authorize(["admin", "user"])
 
-  const user = await db.user.findOne({where, select})
+  const user = await db.user.findOne({where})
 
   if (!user) throw new NotFoundError(`User with id ${where.id} does not exist`)
 
-  return user
+  const {hashedPassword, ...rest} = user
+
+  return rest
 }
