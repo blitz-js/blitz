06af490294f0d2ec0d0d58088b4afac4ee878b31
Brandon Bayer <b@bayer.ws>
2020-09-28 12:53:37 -0400

    fix test

--- packages/core/test/react-query-utils.test.ts
+++ packages/core/test/react-query-utils.test.ts
@@ -5,6 +5,9 @@ jest.mock("react-query")
 
 describe("getQueryCacheFunctions", () => {
   it("returns a mutate function with working options", async () => {
+    window.requestIdleCallback = jest.fn((fn) => {
+      fn({} as any)
+    })
     const spyRefetchQueries = jest.spyOn(queryCache, "invalidateQueries")
     const {mutate} = getQueryCacheFunctions("testQueryKey")
     expect(mutate).toBeTruthy()
