310079b3bc73a1690bab7673aac1653887ffe0ed
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-05 23:46:06 +0100

    Fix Safari compatibility: shim requestIdleCallback (#1247)
    (patch)

--- packages/core/test/react-query-utils.test.ts
+++ packages/core/test/react-query-utils.test.ts
@@ -4,11 +4,26 @@ import {queryCache} from "react-query"
 jest.mock("react-query")
 
 describe("getQueryCacheFunctions", () => {
+  const spyRefetchQueries = jest.spyOn(queryCache, "invalidateQueries")
+
+  beforeEach(() => spyRefetchQueries.mockReset())
+
   it("returns a mutate function with working options", async () => {
     window.requestIdleCallback = jest.fn((fn) => {
       fn({} as any)
     })
-    const spyRefetchQueries = jest.spyOn(queryCache, "invalidateQueries")
+    const {mutate} = getQueryCacheFunctions("testQueryKey")
+    expect(mutate).toBeTruthy()
+    await mutate({newData: true})
+    expect(spyRefetchQueries).toBeCalledTimes(1)
+    await mutate({newData: true}, {refetch: false})
+    expect(spyRefetchQueries).toBeCalledTimes(1)
+    await mutate({newData: true}, {refetch: true})
+    expect(spyRefetchQueries).toBeCalledTimes(2)
+  })
+
+  it("works even when requestIdleCallback is undefined", async () => {
+    window.requestIdleCallback = undefined as any
     const {mutate} = getQueryCacheFunctions("testQueryKey")
     expect(mutate).toBeTruthy()
     await mutate({newData: true})
