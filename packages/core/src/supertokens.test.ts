4839523d53d6d796a6420ab7958a8df9eb68c967
Cody G <37571416+clgeoio@users.noreply.github.com>
2020-10-11 06:43:16 +1100

    Adds tests for useSession in core super tokens (#1258)
    (meta)

--- packages/core/src/supertokens.test.ts
+++ packages/core/src/supertokens.test.ts
@@ -1,4 +1,7 @@
-import {parsePublicDataToken, TOKEN_SEPARATOR} from "./supertokens"
+import {parsePublicDataToken, TOKEN_SEPARATOR, useSession} from "./supertokens"
+import {renderHook} from "../test/test-utils"
+import {publicDataStore} from "./public-data-store"
+import {act} from "@testing-library/react-hooks"
 
 describe("supertokens", () => {
   describe("parsePublicDataToken", () => {
@@ -25,6 +28,63 @@ describe("supertokens", () => {
       const data = `"foo"${TOKEN_SEPARATOR}123`
       expect(parsePublicDataToken(btoa(data))).toEqual({
         publicData: "foo",
+      })
+    })
+  })
+
+  describe("useSession", () => {
+    it("returns empty at when no value is set", () => {
+      const {result} = renderHook(() => useSession())
+
+      expect(result.current).toEqual({
+        isLoading: false,
+        ...publicDataStore.emptyPublicData,
+      })
+    })
+
+    it("subscribes to the public data store", async () => {
+      const {result} = renderHook(() => useSession())
+
+      await act(() => {
+        publicDataStore.updateState({roles: ["foo"], userId: "bar"})
+      })
+
+      expect(result.current).toEqual({
+        isLoading: false,
+        roles: ["foo"],
+        userId: "bar",
+      })
+
+      await act(() => {
+        publicDataStore.updateState({roles: ["baz"], userId: "boo"})
+      })
+
+      expect(result.current).toEqual({
+        isLoading: false,
+        roles: ["baz"],
+        userId: "boo",
+      })
+    })
+
+    it("un-subscribes from the public data store on unmount", async () => {
+      const {result, unmount} = renderHook(() => useSession())
+
+      await act(() => {
+        publicDataStore.updateState({roles: ["foo"], userId: "bar"})
+      })
+
+      await act(() => {
+        unmount()
+      })
+
+      await act(() => {
+        publicDataStore.updateState({roles: ["baz"], userId: "boo"})
+      })
+
+      expect(result.current).toEqual({
+        isLoading: false,
+        roles: ["foo"],
+        userId: "bar",
       })
     })
   })
