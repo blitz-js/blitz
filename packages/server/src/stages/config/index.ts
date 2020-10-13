fbc9b886310c0fe49452b7c4ce38d39ec9dc3500
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2020-10-13 18:51:55 +0100

    Add transpilation step to the config stage

--- packages/server/src/stages/config/index.ts
+++ packages/server/src/stages/config/index.ts
@@ -1,11 +1,13 @@
 import {pathExistsSync} from "fs-extra"
 import {resolve} from "path"
 import File from "vinyl"
+import {transformFileSync as babelTransform} from "@babel/core"
 
 import {transform} from "@blitzjs/file-pipeline"
 import {Stage} from "@blitzjs/file-pipeline"
 
 const isNextConfigPath = (p: string) => /next\.config\.(js|ts)/.test(p)
+const isBlitzTsConfigPath = (p: string) => /blitz\.config\.ts/.test(p)
 const isNowBuild = () => process.env.NOW_BUILDER || process.env.VERCEL_BUILDER
 /**
  * Returns a Stage that manages converting from blitz.config.js to next.config.js
@@ -52,6 +54,25 @@ module.exports = withBlitz(config);
 
   // No need to filter yet
   const stream = transform.file((file) => {
+    if (isBlitzTsConfigPath(file.path)) {
+      const result = babelTransform(resolve(config.src, "blitz.config.ts"))
+
+      if (!result || !result.code) {
+        throw new Error("Blitz was unable to transpile your blitz.config.ts file")
+      }
+
+      processNewChildFile({
+        parent: file,
+        child: new File({
+          cwd: config.src,
+          path: resolve(config.src, "blitz.config.js"),
+          contents: Buffer.from(result.code),
+        }),
+        stageId: "config",
+        subfileId: "blitz-config-js",
+      })
+    }
+
     if (!isNextConfigPath(file.path)) return file
 
     // File is next.config.js
