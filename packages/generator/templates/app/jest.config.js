e4983c6f2e2a85a3536729287bb39ba21c7e23dd
Peter Shih <peter50216@gmail.com>
2020-10-15 05:00:14 +0800

    (newapp) Fix tests not working out of box (#1323)

--- packages/generator/templates/app/jest.config.js
+++ packages/generator/templates/app/jest.config.js
@@ -15,7 +15,7 @@ module.exports = {
     "^.+\\.(ts|tsx)$": "babel-jest",
   },
   // This makes absolute imports work
-  moduleDirectories: ["node_modules", "."],
+  moduleDirectories: ["node_modules", "<rootDir>"],
   modulePathIgnorePatterns: [".blitz"],
   moduleNameMapper: {
     // This ensures any path aliases in tsconfig also work in jest
