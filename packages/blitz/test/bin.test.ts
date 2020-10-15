1b6a0f0e6e3dc828dcd0ba6d41f16b5f2c4ebd53
Mark Hähnel <hello@markhaehnel.de>
2020-10-15 19:11:24 +0200

    Fix exit code tests

--- packages/blitz/test/bin.test.ts
+++ packages/blitz/test/bin.test.ts
@@ -1,15 +1,18 @@
 import spawn from "cross-spawn"
+import path from "path"
+
+const blitzBin = path.resolve(__dirname, "../bin/blitz")
+const exampleProject = path.resolve(__dirname, "../../../examples/auth")
 
 describe("Binary blitz", () => {
-  it("should return exit code 0 if command is successfull", () => {
-    const result = spawn.sync("blitz", ["--version"])
+  it("should return exit code 0 if command is successful", () => {
+    const result = spawn.sync(blitzBin, ["--version"], {
+      cwd: exampleProject,
+    })
     expect(result.status).toBe(0)
   })
-  it.skip("should return exit code 1 if command is unsuccessfull", () => {
-    // TODO: Commands not available dont return exit code 1.
-    // Need do setup a environment to get it failing.
-
-    const result = spawn.sync("blitz", ["notAvailableCommand"])
+  it("should return exit code 1 if command is unsuccessful", () => {
+    const result = spawn.sync(blitzBin, ["notAvailableCommand"])
     expect(result.status).toBe(1)
   })
 })
