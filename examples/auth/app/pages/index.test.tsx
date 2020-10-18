03f6fd11197c2289dadcc2c574c9e6bb60c5b84b
Brandon Bayer <b@bayer.ws>
2020-10-18 16:08:30 -0500

    fix auth example in ci

--- examples/auth/app/pages/index.test.tsx
+++ examples/auth/app/pages/index.test.tsx
@@ -8,11 +8,6 @@ jest.mock("app/hooks/useCurrentUser")
 const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>
 
 test("renders blitz documentation link", () => {
-  // This is an example of how to ensure a specific item is in the document
-  // But it's disabled by default (by test.skip) so the test doesn't fail
-  // when you remove the the default content from the page
-
-  // This is an example on how to mock api hooks when testing
   mockUseCurrentUser.mockReturnValue({
     id: 1,
     name: "User",
