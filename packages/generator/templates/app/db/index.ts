ffd405390e22692a9a08c3a4140b4b1199c0fc17
Kristóf Poduszló <kripod@protonmail.com>
2020-10-20 00:12:37 +0200

    refactor(db): simpler assignments and const export

--- packages/generator/templates/app/db/index.ts
+++ packages/generator/templates/app/db/index.ts
@@ -1,15 +1,17 @@
 import { PrismaClient } from "@prisma/client"
 export * from "@prisma/client"
 
-let prisma: PrismaClient
-
-if (process.env.NODE_ENV === "production") {
-    prisma = new PrismaClient()
-} else {
-    // Ensure the prisma instance is re-used during hot-reloading
-    // Otherwise, a new client will be created on every reload
-    globalThis["prisma"] = globalThis["prisma"] || new PrismaClient()
-    prisma = globalThis["prisma"]
+declare namespace globalThis {
+  let prisma: PrismaClient
 }
 
-export default prisma
+function getPersistentPrismaClient() {
+  // Ensure the Prisma instance is re-used during hot-reloading
+  // Otherwise, a new client would be created on every reload
+  globalThis.prisma ||= new PrismaClient()
+  return globalThis.prisma
+}
+
+export default process.env.NODE_ENV === "production"
+  ? getPersistentPrismaClient()
+  : new PrismaClient()
