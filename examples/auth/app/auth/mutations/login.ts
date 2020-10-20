fbb8af0b62857fba245bc6c2b737d82249b45d42
Utkarsh Kukreti <utkarshkukreti@gmail.com>
2020-10-21 02:03:16 +0530

    fix types of auth mutations in examples and generator
    By typing them as `LoginInputType`, the user could access fields in the object
    before validation. Now they can only do after validation.

--- examples/auth/app/auth/mutations/login.ts
+++ examples/auth/app/auth/mutations/login.ts
@@ -1,8 +1,8 @@
 import {Ctx} from "blitz"
 import {authenticateUser} from "app/auth/auth-utils"
-import {LoginInput, LoginInputType} from "../validations"
+import {LoginInput} from "../validations"
 
-export default async function login(input: LoginInputType, {session}: Ctx) {
+export default async function login(input: unknown, {session}: Ctx) {
   // This throws an error if input is invalid
   const {email, password} = LoginInput.parse(input)
 
--- examples/auth/app/auth/mutations/signup.ts
+++ examples/auth/app/auth/mutations/signup.ts
@@ -1,9 +1,9 @@
 import {Ctx} from "blitz"
 import db from "db"
 import {hashPassword} from "app/auth/auth-utils"
-import {SignupInput, SignupInputType} from "app/auth/validations"
+import {SignupInput} from "app/auth/validations"
 
-export default async function signup(input: SignupInputType, {session}: Ctx) {
+export default async function signup(input: unknown, {session}: Ctx) {
   // This throws an error if input is invalid
   const {email, password} = SignupInput.parse(input)
 
