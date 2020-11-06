26d4580bf963c7e524ae1b41ca6b2a83917f4a6b
Brandon Bayer <b@bayer.ws>
2020-11-05 20:51:02 -0500

    wip

--- examples/store/app/admin/pages/admin/products/[id].tsx
+++ examples/store/app/admin/pages/admin/products/[id].tsx
@@ -1,11 +1,16 @@
 import {Suspense} from "react"
 import {Link, useRouter, useQuery, useParam} from "blitz"
+import {useRouter as useNextRouter} from "next/router"
 import getProduct from "app/products/queries/getProduct"
 import ProductForm from "app/products/components/ProductForm"
 
 function Product() {
   const router = useRouter()
+  const nextRouter = useNextRouter()
   const id = useParam("id", "number")
+  console.log("id", id)
+  console.log("bRouter", router.params)
+  console.log("nRouter", nextRouter.query)
   const [product] = useQuery(getProduct, {where: {id}})
 
   return (
