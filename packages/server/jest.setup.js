51afb04ab05479600a96e3913efba17bbfdcc084
Brandon Bayer <b@bayer.ws>
2020-11-07 16:45:56 -0500

    another fix

---
+++ packages/server/jest.setup.js
@@ -0,0 +1,11 @@
+// TODO - remove once https://github.com/vercel/next.js/issues/18415 is fixed
+process.env = {
+  ...process.env,
+  __NEXT_IMAGE_OPTS: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [],
+    domains: ["images.example.com"],
+    path: "/_next/image",
+    loader: "default",
+  },
+}
