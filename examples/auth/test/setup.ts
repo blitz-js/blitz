27cabb65470919ec79389bb292f5e72097e2d43f
Brandon Bayer <b@bayer.ws>
2020-11-07 16:31:34 -0500

    fix

--- examples/auth/test/setup.ts
+++ examples/auth/test/setup.ts
@@ -4,3 +4,15 @@
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect"
 require("dotenv-flow").config({silent: true})
+
+// TODO - remove once https://github.com/vercel/next.js/issues/18415 is fixed
+process.env = {
+  ...process.env,
+  __NEXT_IMAGE_OPTS: {
+    deviceSizes: [320, 420, 768, 1024, 1200],
+    imageSizes: [],
+    domains: ["images.example.com"],
+    path: "/_next/image",
+    loader: "default",
+  },
+}
