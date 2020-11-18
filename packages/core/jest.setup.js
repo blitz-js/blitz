fc8fd8d6398c8edc910d1e47c59c075386e12abd
Brandon Bayer <b@bayer.ws>
2020-11-18 17:44:39 -0500

    cleanup

--- packages/core/jest.setup.js
+++ packages/core/jest.setup.js
@@ -1,14 +1,2 @@
 require("@testing-library/jest-dom")
 process.env.BLITZ_TEST_ENVIRONMENT = true
-
-// TODO - remove once https://github.com/vercel/next.js/issues/18415 is fixed
-process.env = {
-  ...process.env,
-  __NEXT_IMAGE_OPTS: {
-    deviceSizes: [320, 420, 768, 1024, 1200],
-    imageSizes: [],
-    domains: ["images.example.com"],
-    path: "/_next/image",
-    loader: "default",
-  },
-}
