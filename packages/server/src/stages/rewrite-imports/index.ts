9b94689bf3a086eb254f327d97bf1835a1434680
Brandon Bayer <b@bayer.ws>
2020-11-17 21:42:41 -0500

    add comments

--- packages/server/src/stages/rewrite-imports/index.ts
+++ packages/server/src/stages/rewrite-imports/index.ts
@@ -30,6 +30,14 @@ export const createStageRewriteImports: Stage = ({config: {cwd}}) => {
 
 export const patternImport = /(import\s*)(?:(\*\s+as\s+\w+)|(?:(\w+\s*)(,\s*)?)?(?:(\{\s*\w+\s*(?:,\s*\w+\s*)*\}))?)(\s+from\s+)?((?:\(\s*)?["'])([\w\.\\\/]+)(["'](?:\s*\))?)/gs
 
+// Whenever we see smth like `import { myFunc } from "app/queries/myQuery"`,
+//  we rewrite that to app/_resolvers/queries/myQuery
+//
+// When we see smth like `import myQuery, { myFunc } from "app/queries/myQuery"`,
+//  we rewrite it to the following two:
+//   import myQuery from "app/queries/myQuery"
+//   import { myFunc } from "app/_resolvers/queries/myQuery"
+
 export function replaceImports(content: string) {
   return content.replace(patternImport, (...args) => {
     const [
@@ -66,12 +74,16 @@ export function replaceImports(content: string) {
 export function rewriteImportOrigin(origin: string, importsNamedExports: boolean): string {
   const parts = origin.split("/")
 
+  // If it's an import from a page, say from app/pages/mypage,
+  // we'll rewrite that import to pages/mypage.
   if (parts.includes("pages")) {
     if (parts[0] === "app") {
       parts.splice(0, 1)
     }
   }
 
+  // If it's an import from an API Route, say from app/users/api/myRoute,
+  // we'll rewrite it to pages/api/myRoute.
   if (parts.includes("api")) {
     if (parts.indexOf("api") === parts.length - 1) {
       parts.push("index")
