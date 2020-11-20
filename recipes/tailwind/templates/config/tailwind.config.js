6fb9cb27f673cea721bd1f7bea20cee6225f7061
Mzaien <s201540840@kfupm.edu.sa>
2020-11-20 13:40:55 +0300

    postcss &tailwind
    Changed postcss & tailwind config to follow [next.js tailwind 2](https://github.com/vercel/next.js/pull/19278/files) Thanks to @pbteja1998

--- recipes/tailwind/templates/config/tailwind.config.js
+++ recipes/tailwind/templates/config/tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  future: "all",
   purge: ["{app,pages}/**/*.{js,jsx,ts,tsx}"],
+  darkMode: false,  // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  darkMode: false,  // or 'media' or 'class'
-  variants: {},
+  variants: {
+    extend: {},
+  },
   plugins: [],
 }
