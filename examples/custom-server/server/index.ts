6311a661156ce557d7ae1ae59c6e4532ceedcbb6
KATT <alexander@n1s.se>
2020-11-22 15:11:04 +0100

    custom-server example working

---
+++ examples/custom-server/server/index.ts
@@ -0,0 +1,27 @@
+import { createServer } from "http";
+import { parse } from "url";
+import { createBlitzApp } from "@blitzjs/server";
+
+const { PORT = "3000", NODE_ENV } = process.env
+
+const dev = NODE_ENV !== "production"
+
+async function main() {
+  const { requestHandler } = await createBlitzApp({ dev })
+
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url!, true)
+    const { pathname } = parsedUrl
+
+    if (pathname === "/hello") {
+      res.writeHead(200).end("world")
+      return
+    }
+
+    requestHandler(req, res, parsedUrl)
+  }).listen(PORT, () => {
+    console.log(`🎉 Ready on http://localhost:${PORT}`)
+  })
+}
+
+main()
