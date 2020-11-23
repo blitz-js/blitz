53dee7ca4867dc74d84629b4234a29c373a53d67
Brandon Bayer <b@bayer.ws>
2020-11-23 18:45:49 -0500

    update templates

--- packages/generator/templates/mutation/delete__ModelName__.ts
+++ packages/generator/templates/mutation/delete__ModelName__.ts
@@ -1,7 +1,7 @@
 import {Ctx} from "blitz"
-import db, {__ModelName__DeleteArgs} from "db"
+import db, {Prisma} from "db"
 
-type Delete__ModelName__Input = Pick<__ModelName__DeleteArgs, "where">
+type Delete__ModelName__Input = Pick<Prisma.__ModelName__DeleteArgs, "where">
 
 export default async function delete__ModelName__({where}: Delete__ModelName__Input, ctx: Ctx) {
   ctx.session.authorize()
