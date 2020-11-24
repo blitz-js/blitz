2bf7daa1096e63d36c75d68fb55f18d72a246e94
Mzaien <s201540840@kfupm.edu.sa>
2020-11-24 09:25:49 +0300

    Revert "postcss &tailwind"
    This reverts commit 6fb9cb27f673cea721bd1f7bea20cee6225f7061.

--- recipes/tailwind/index.ts
+++ recipes/tailwind/index.ts
@@ -15,12 +15,20 @@ export default RecipeBuilder()
     stepName: "Add npm dependencies",
     explanation: `Tailwind CSS requires a couple of dependencies to get up and running.
 We'll install the Tailwind library itself, as well as PostCSS for removing unused styles from our production bundles.`,
-packages: [
-  {name: "tailwindcss", version: "latest", isDevDep: true},
-  {name: "postcss", version: "latest", isDevDep: true},
-  {name: "postcss-flexbugs-fixes", version: "latest", isDevDep: true},
-  {name: "postcss-preset-env", version: "latest", isDevDep: true},
-],
+    packages: [
+      {name: "tailwindcss", version: "2"},
+      {name: "postcss-preset-env", version: "latest", isDevDep: true},
+    ],
+  })
+  .addNewFilesStep({
+    stepId: "addConfig",
+    stepName: "Add Tailwind CSS and PostCSS config files",
+    explanation: `In order to set up Tailwind CSS properly, we need to include a few configuration files. We'll configure Tailwind CSS to know where your app's pages live, and PostCSS for elimination of unused styles.
+
+These config files can be extended for additional customization, but for now we'll just give the minimum required to get started.`,
+    targetDirectory: ".",
+    templatePath: join(__dirname, "templates", "config"),
+    templateValues: {},
   })
   .addNewFilesStep({
     stepId: "addStyles",
@@ -36,7 +44,7 @@ packages: [
     explanation: `Finaly, we can import the stylesheets we just created into our application. For now we'll put them in document.tsx, but if you'd like to only style a part of your app with tailwind you could import the styles lower down in your component tree.`,
     singleFileSearch: paths.app(),
     transform(program: Collection<j.Program>) {
-      const stylesImport = j.importDeclaration([], j.literal("tailwindcss/tailwind.css"))
+      const stylesImport = j.importDeclaration([], j.literal("app/styles/index.css"))
       return addImport(program, stylesImport)
     },
   })
--- recipes/tailwind/templates/config/tailwind.config.js
+++ recipes/tailwind/templates/config/tailwind.config.js
@@ -1,11 +1,10 @@
 module.exports = {
+  future: "all",
   purge: ["{app,pages}/**/*.{js,jsx,ts,tsx}"],
-  darkMode: false,  // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
+  darkMode: false,  // or 'media' or 'class'
+  variants: {},
   plugins: [],
 }
