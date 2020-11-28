f82d7ba530be4e9422bb3048893f8764c226527c
Khaled Garbaya <khaledgarbaya@gmail.com>
2020-11-28 19:33:33 +0100

    fix(generator): change file path to kebab-case

--- packages/generator/src/generators/mutation-generator.ts
+++ packages/generator/src/generators/mutation-generator.ts
@@ -1,5 +1,6 @@
 import {join} from "path"
 import {Generator, GeneratorOptions} from "../generator"
+import {camelCaseToKebabCase} from "../utils/kebab-case"
 
 export interface MutationGeneratorOptions extends GeneratorOptions {
   ModelName: string
@@ -45,7 +46,7 @@ export class MutationGenerator extends Generator<MutationGeneratorOptions> {
   }
 
   getTargetDirectory() {
-    const context = this.options.context ? `${this.options.context}/` : ""
+    const context = this.options.context ? `${camelCaseToKebabCase(this.options.context)}/` : ""
     return `app/${context}${this.options.modelNames}/mutations`
   }
 }
