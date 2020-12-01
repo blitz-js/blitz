6f15f52f5fa8bd9e8cc121e9fab08e8a9f1a0751
Mark Laing <m.p.laing@outlook.com>
2020-12-01 17:33:45 +0000

    reflects auth config change in auth example.

--- examples/auth/app/api/auth/[...auth].ts
+++ examples/auth/app/api/auth/[...auth].ts
@@ -23,109 +23,115 @@ assert(process.env.AUTH0_CLIENT_SECRET, "You must provide the AUTH0_CLIENT_SECRE
 
 export default passportAuth({
   successRedirectUrl: "/",
-  authenticateOptions: {scope: "openid email profile"}, //used for Auth0Strategy - without an empty profile is returned
   strategies: [
-    new TwitterStrategy(
-      {
-        consumerKey: process.env.TWITTER_CONSUMER_KEY,
-        consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-        callbackURL:
-          process.env.NODE_ENV === "production"
-            ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/twitter/callback"
-            : "http://localhost:3000/api/auth/twitter/callback",
-        includeEmail: true,
-      },
-      async function (_token, _tokenSecret, profile, done) {
-        const email = profile.emails && profile.emails[0]?.value
+    {
+      strategy: new TwitterStrategy(
+        {
+          consumerKey: process.env.TWITTER_CONSUMER_KEY,
+          consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
+          callbackURL:
+            process.env.NODE_ENV === "production"
+              ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/twitter/callback"
+              : "http://localhost:3000/api/auth/twitter/callback",
+          includeEmail: true,
+        },
+        async function (_token, _tokenSecret, profile, done) {
+          const email = profile.emails && profile.emails[0]?.value
 
-        if (!email) {
-          // This can happen if you haven't enabled email access in your twitter app permissions
-          return done(new Error("Twitter OAuth response doesn't have email."))
-        }
+          if (!email) {
+            // This can happen if you haven't enabled email access in your twitter app permissions
+            return done(new Error("Twitter OAuth response doesn't have email."))
+          }
 
-        const user = await db.user.upsert({
-          where: {email},
-          create: {
-            email,
-            name: profile.displayName,
-          },
-          update: {email},
-        })
+          const user = await db.user.upsert({
+            where: {email},
+            create: {
+              email,
+              name: profile.displayName,
+            },
+            update: {email},
+          })
 
-        const publicData = {userId: user.id, roles: [user.role], source: "twitter"}
-        done(null, {publicData})
-      },
-    ),
-    new GitHubStrategy(
-      {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL:
-          process.env.NODE_ENV === "production"
-            ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/github/callback"
-            : "http://localhost:3000/api/auth/github/callback",
-      },
-      async function (_token, _tokenSecret, profile, done) {
-        const email = profile.emails && profile.emails[0]?.value
+          const publicData = {userId: user.id, roles: [user.role], source: "twitter"}
+          done(null, {publicData})
+        },
+      ),
+    },
+    {
+      strategy: new GitHubStrategy(
+        {
+          clientID: process.env.GITHUB_CLIENT_ID,
+          clientSecret: process.env.GITHUB_CLIENT_SECRET,
+          callbackURL:
+            process.env.NODE_ENV === "production"
+              ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/github/callback"
+              : "http://localhost:3000/api/auth/github/callback",
+        },
+        async function (_token, _tokenSecret, profile, done) {
+          const email = profile.emails && profile.emails[0]?.value
 
-        if (!email) {
-          // This can happen if you haven't enabled email access in your twitter app permissions
-          return done(new Error("Twitter OAuth response doesn't have email."))
-        }
+          if (!email) {
+            // This can happen if you haven't enabled email access in your twitter app permissions
+            return done(new Error("Twitter OAuth response doesn't have email."))
+          }
 
-        const user = await db.user.upsert({
-          where: {email},
-          create: {
-            email,
-            name: profile.displayName,
-          },
-          update: {email},
-        })
+          const user = await db.user.upsert({
+            where: {email},
+            create: {
+              email,
+              name: profile.displayName,
+            },
+            update: {email},
+          })
 
-        const publicData = {
-          userId: user.id,
-          roles: [user.role],
-          source: "github",
-          githubUsername: profile.username,
-        }
-        done(null, {publicData})
-      },
-    ),
-    new Auth0Strategy(
-      {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL:
-          process.env.NODE_ENV === "production"
-            ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/auth0/callback"
-            : "http://localhost:3000/api/auth/auth0/callback",
-      },
-      async function (_token, _tokenSecret, extraParams, profile, done) {
-        const email = profile.emails && profile.emails[0]?.value
+          const publicData = {
+            userId: user.id,
+            roles: [user.role],
+            source: "github",
+            githubUsername: profile.username,
+          }
+          done(null, {publicData})
+        },
+      ),
+    },
+    {
+      authenticateOptions: {scope: "openid email profile"},
+      strategy: new Auth0Strategy(
+        {
+          domain: process.env.AUTH0_DOMAIN,
+          clientID: process.env.AUTH0_CLIENT_ID,
+          clientSecret: process.env.AUTH0_CLIENT_SECRET,
+          callbackURL:
+            process.env.NODE_ENV === "production"
+              ? "https://auth-example-flybayer.blitzjs.vercel.app/api/auth/auth0/callback"
+              : "http://localhost:3000/api/auth/auth0/callback",
+        },
+        async function (_token, _tokenSecret, extraParams, profile, done) {
+          const email = profile.emails && profile.emails[0]?.value
 
-        if (!email) {
-          // This can happen if you haven't enabled email access in your twitter app permissions
-          return done(new Error("GitHub OAuth response doesn't have email."))
-        }
+          if (!email) {
+            // This can happen if you haven't enabled email access in your twitter app permissions
+            return done(new Error("Auth0 response doesn't have email."))
+          }
 
-        const user = await db.user.upsert({
-          where: {email},
-          create: {
-            email,
-            name: profile.displayName,
-          },
-          update: {email},
-        })
+          const user = await db.user.upsert({
+            where: {email},
+            create: {
+              email,
+              name: profile.displayName,
+            },
+            update: {email},
+          })
 
-        const publicData = {
-          userId: user.id,
-          roles: [user.role],
-          source: "auth0",
-          githubUsername: profile.username,
-        }
-        done(undefined, {publicData})
-      },
-    ),
+          const publicData = {
+            userId: user.id,
+            roles: [user.role],
+            source: "auth0",
+            githubUsername: profile.username,
+          }
+          done(undefined, {publicData})
+        },
+      ),
+    },
   ],
 })
