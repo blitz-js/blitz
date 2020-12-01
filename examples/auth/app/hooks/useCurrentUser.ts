5580e5fb556dff4916f54c036e904229f4bb8d41
Brandon Bayer <b@bayer.ws>
2020-12-01 12:06:45 -0500

    fix

--- examples/auth/app/hooks/useCurrentUser.ts
+++ examples/auth/app/hooks/useCurrentUser.ts
@@ -1,7 +1,7 @@
-import {useQuery, useSession} from "blitz"
+import {useQuery} from "blitz"
 import getCurrentUser from "app/users/queries/getCurrentUser"
 
 export const useCurrentUser = () => {
-  const [user] = useQuery(getCurrentUser, null, {enabled: !!session.userId})
-  return session.userId ? user : null
+  const [user] = useQuery(getCurrentUser, null)
+  return user
 }
