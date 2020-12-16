46558a068429ca542f458a6f9523642d73ac8344
Kevin Østerkilde <kevin@oesterkilde.dk>
2020-12-16 13:33:57 +0100

    feat: Nextjs types export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -45,10 +45,12 @@ export {
 export {default as Head} from "next/head"
 
 export {default as Link} from "next/link"
+export {LinkProps} from "next/link"
 
 export {default as Router} from "next/router"
 
 export {default as Image} from "next/image"
+export {ImageProps} from "next/image"
 
 export {
   default as Document,
@@ -63,6 +65,7 @@ export {
 export {default as dynamic} from "next/dynamic"
 
 export {default as ErrorComponent} from "next/error"
+export {ErrorProps} from "next/error"
 
 export {default as getConfig} from "next/config"
 
