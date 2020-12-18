12510ce8c73234b54d05a646599d83ec77175c5b
KATT <alexander@n1s.se>
2020-12-18 11:44:51 +0100

    Remove auto-build

--- packages/server/src/prod.ts
+++ packages/server/src/prod.ts
@@ -1,12 +1,13 @@
-import {build} from "./build"
-import {alreadyBuilt} from "./build-hash"
+import {log} from "@blitzjs/display"
+import * as fs from "fs"
 import {normalize, ServerConfig} from "./config"
 import {nextStart} from "./next-utils"
 
 export async function prod(config: ServerConfig) {
   const {buildFolder, nextBin} = await normalize(config)
-  if (!(await alreadyBuilt(buildFolder))) {
-    await build(config)
+  if (!fs.existsSync(buildFolder)) {
+    log.error("Build folder not found, make sure to run `blitz build` before starting")
+    process.exit(1)
   }
   await nextStart(nextBin, buildFolder, config)
 }
