91cecce5f4e7c4d819733209a7de7b23e99c6e14
KATT <alexander@n1s.se>
2020-12-22 21:12:48 +0100

    only use node-fetch
    next.js bundles it

--- packages/core/src/middleware.test.ts
+++ packages/core/src/middleware.test.ts
@@ -1,6 +1,6 @@
 import http from "http"
-import fetch from "isomorphic-unfetch"
 import {apiResolver} from "next/dist/next-server/server/api-utils"
+import fetch from "node-fetch"
 import listen from "test-listen"
 import {BlitzApiRequest, BlitzApiResponse} from "."
 import {handleRequestWithMiddleware} from "./middleware"
