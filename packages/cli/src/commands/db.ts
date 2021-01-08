7145f1e2746e4c3dc12ba7c11c6bc03757ea5991
Brandon Bayer <b@bayer.ws>
2021-01-08 17:47:16 -0500

    add `blitz prisma` command

--- packages/cli/src/commands/db.ts
+++ packages/cli/src/commands/db.ts
@@ -1,35 +1,8 @@
 import {Command, flags} from "@oclif/command"
 import {log} from "@blitzjs/display"
+import {runPrisma, runPrismaExitOnError} from "./prisma"
 
-const getPrismaBin = () => require("@blitzjs/server").resolveBinAsync("@prisma/cli", "prisma")
-let prismaBin: string
 let schemaArg: string
-
-const runPrisma = async (args: string[], silent = false) => {
-  if (!prismaBin) {
-    try {
-      prismaBin = await getPrismaBin()
-    } catch (err) {
-      throw err
-    }
-  }
-
-  const cp = require("cross-spawn").spawn(prismaBin, args, {
-    stdio: silent ? "ignore" : "inherit",
-    env: process.env,
-  })
-  const code = await require("p-event")(cp, "exit", {rejectionEvents: []})
-
-  return code === 0
-}
-
-const runPrismaExitOnError = async (...args: Parameters<typeof runPrisma>) => {
-  const success = await runPrisma(...args)
-
-  if (!success) {
-    process.exit(1)
-  }
-}
 
 // Prisma client generation will fail if no model is defined in the schema.
 // So the silent option is here to ignore that failure
