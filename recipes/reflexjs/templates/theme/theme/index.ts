a0fbb025651c0218177715a8bd9f6f7dc29ed4c3
koolii <koolii0909@gmail.com>
2021-01-12 19:16:43 +0900

    Solve left eslint warnings

--- recipes/reflexjs/templates/theme/theme/index.ts
+++ recipes/reflexjs/templates/theme/theme/index.ts
@@ -1,4 +1,4 @@
-export default {
+const theme = {
   breakpoints: ["640px", "768px", "1024px", "1280px"],
   colors: {
     text: "#111",
@@ -492,3 +492,5 @@ export default {
     chart: `<svg fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>`,
   },
 }
+
+export default theme
