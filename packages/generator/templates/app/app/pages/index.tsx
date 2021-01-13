f0c37737b9ee0fca569ee338d3d55e3f63dee3c0
Brandon Bayer <b@bayer.ws>
2021-01-13 18:16:27 -0500

    updated app template

--- packages/generator/templates/app/app/pages/index.tsx
+++ packages/generator/templates/app/app/pages/index.tsx
@@ -1,8 +1,8 @@
-import { Link, BlitzPage, useMutation } from "blitz"
-import Layout from "app/layouts/Layout"
-import logout from "app/auth/mutations/logout"
-import { useCurrentUser } from "app/hooks/useCurrentUser"
 import { Suspense } from "react"
+import { Link, BlitzPage, useMutation } from "blitz"
+import Layout from "app/core/layouts/Layout"
+import { useCurrentUser } from "app/core/hooks/useCurrentUser"
+import logout from "app/auth/mutations/logout"
 
 /*
  * This file is just for a pleasant getting started page for your new app.
