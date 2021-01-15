48bc73505f15402e20bf929ec89bb7f4fc7a0883
Brandon Bayer <b@bayer.ws>
2021-01-15 16:58:23 -0500

    add blitz jest preset

---
+++ packages/blitz/jest-preset.js
@@ -0,0 +1,37 @@
+const path = require("path")
+const pkgDir = require("pkg-dir")
+const {pathsToModuleNameMapper} = require("ts-jest/utils")
+const projectRoot = pkgDir.sync() || process.cwd()
+const {compilerOptions} = require(path.join(projectRoot, "tsconfig"))
+
+module.exports = {
+  globalSetup: path.resolve(__dirname, "./jest-preset/global-setup.js"),
+  setupFilesAfterEnv: [
+    path.resolve(__dirname, "./jest-preset/setup-after-env.js"),
+    "<rootDir>/test/setup.ts",
+  ],
+  // Add type checking to Typescript test files
+  preset: "ts-jest",
+  testEnvironment: "jest-environment-jsdom-fourteen",
+  // Automatically clear mock calls and instances between every test
+  clearMocks: true,
+  testPathIgnorePatterns: ["/node_modules/", "/.blitz/", "/.next/", "<rootDir>/db/migrations"],
+  transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "babel-jest",
+  },
+  // This makes absolute imports work
+  moduleDirectories: ["node_modules", "<rootDir>"],
+  // Ignore the build directories
+  modulePathIgnorePatterns: ["<rootDir>/.blitz", "<rootDir>/.next"],
+  moduleNameMapper: {
+    // This ensures any path aliases in tsconfig also work in jest
+    ...pathsToModuleNameMapper(compilerOptions.paths || {}),
+    "\\.(css|less|sass|scss)$": "identity-obj-proxy",
+    "\\.(gif|ttf|eot|svg|png|jpg|jpeg)$": "<rootDir>/test/__mocks__/fileMock.js",
+  },
+  watchPlugins: ["jest-watch-typeahead/filename", "jest-watch-typeahead/testname"],
+  // Coverage output
+  coverageDirectory: ".coverage",
+  collectCoverageFrom: ["**/*.{js,jsx,ts,tsx}", "!**/*.d.ts", "!**/node_modules/**"],
+}
