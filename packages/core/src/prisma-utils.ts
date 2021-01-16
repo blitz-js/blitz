df80fa1d5eefb32d96ea2195e52a3784e3f485b8
Brandon Bayer <b@bayer.ws>
2021-01-15 19:07:53 -0500

    fix `makeServerOnlyPrisma()` not working in Jest tests (patch)

--- packages/core/src/prisma-utils.ts
+++ packages/core/src/prisma-utils.ts
@@ -1,7 +1,11 @@
 export const makeServerOnlyPrisma = <T>(PrismaClient: T): T => {
   return new Proxy(PrismaClient as any, {
     construct(target, args) {
-      if (typeof window !== "undefined") return {}
+      if (typeof window !== "undefined" && process.env.JEST_WORKER_ID === undefined) {
+        // Return empty object if in the browser
+        // Skip in Jest tests because window is defined in Jest tests
+        return {}
+      }
       ;(globalThis as any)._blitz_prismaClient =
         (globalThis as any)._blitz_prismaClient || new target(...args)
       return (globalThis as any)._blitz_prismaClient
