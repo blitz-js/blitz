96360172624345d26fc0a64716339e589782a485
Brandon Bayer <b@bayer.ws>
2021-01-18 18:45:35 -0500

    (newapp) Add `changePassword` mutation (#1741)

---
+++ packages/generator/templates/app/app/auth/mutations/changePassword.ts
@@ -0,0 +1,21 @@
+import { Ctx, NotFoundError, SecurePassword } from "blitz"
+import db from "db"
+import { authenticateUser } from "./login"
+import { ChangePasswordInput, ChangePasswordInputType } from "../validations"
+
+export default async function changePassword(input: ChangePasswordInputType, ctx: Ctx) {
+  ctx.session.authorize()
+  const { currentPassword, newPassword } = ChangePasswordInput.parse(input)
+
+  const user = await db.user.findFirst({ where: { id: ctx.session.userId } })
+  if (!user) throw new NotFoundError()
+  await authenticateUser(user.email, currentPassword)
+
+  const hashedPassword = await SecurePassword.hash(newPassword)
+  await db.user.update({
+    where: { id: user.id },
+    data: { hashedPassword },
+  })
+
+  return true
+}
