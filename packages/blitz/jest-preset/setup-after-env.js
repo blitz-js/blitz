654d9378adb985372cdf49729be3b8cf66f7d62a
Brandon Bayer <b@bayer.ws>
2021-01-19 18:05:06 -0500

    Update jest preset to runInBand and auto disconnect Prisma client after tests (#1753)

--- packages/blitz/jest-preset/setup-after-env.js
+++ packages/blitz/jest-preset/setup-after-env.js
@@ -1,1 +1,10 @@
 require("@testing-library/jest-dom")
+
+afterAll(async () => {
+  try {
+    // eslint-disable-next-line no-undef
+    await globalThis._blitz_prismaClient.$disconnect()
+  } catch (error) {
+    // ignore error
+  }
+})
