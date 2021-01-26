10a0bed302297a3f48644b7d791a30e029e4fb3a
Steffan Harris <contact@steffan.lol>
2021-01-26 23:48:34 +0000

    (newapp) Update `<Form>` to easily hide default submit button by making `submitText` optional (#1800)

--- packages/generator/templates/app/_forms/hookform/Form.tsx
+++ packages/generator/templates/app/_forms/hookform/Form.tsx
@@ -6,7 +6,7 @@ type FormProps<S extends z.ZodType<any, any>> = {
   /** All your form fields */
   children: ReactNode
   /** Text to display in the submit button */
-  submitText: string
+  submitText?: string
   schema?: S
   onSubmit: (values: z.infer<S>) => Promise<void | OnSubmitResult>
   initialValues?: UseFormOptions<z.infer<S>>["defaultValues"]
@@ -71,9 +71,11 @@ export function Form<S extends z.ZodType<any, any>>({
           </div>
         )}
 
-        <button type="submit" disabled={ctx.formState.isSubmitting}>
-          {submitText}
-        </button>
+        {submitText && (
+          <button type="submit" disabled={ctx.formState.isSubmitting}>
+            {submitText}
+          </button>
+        )}
 
         <style global jsx>{`
           .form > * + * {
