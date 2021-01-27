7f06994842187aada8369abf651f6aff3d5bf014
Brandon Bayer <b@bayer.ws>
2021-01-27 12:07:57 -0500

    fix

--- examples/custom-server/types.ts
+++ examples/custom-server/types.ts
@@ -1,6 +1,6 @@
 import {DefaultCtx, SessionContext} from "blitz"
 import {simpleRolesIsAuthorized} from "@blitzjs/server"
-import {User} from "db"
+import React from "react"
 
 declare module "blitz" {
   export interface Ctx extends DefaultCtx {
@@ -9,8 +9,16 @@ declare module "blitz" {
   export interface Session {
     isAuthorized: typeof simpleRolesIsAuthorized
     PublicData: {
-      userId: User["id"]
+      userId: number
       roles: string[]
     }
+  }
+}
+
+// This should not be needed. Usually it isn't but for some reason in this example it is
+declare module "react" {
+  interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> {
+    jsx?: boolean
+    global?: boolean
   }
 }
