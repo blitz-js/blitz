3bd030af7357b50871468405acd27b1a68320cd5
Brandon Bayer <b@bayer.ws>
2021-01-29 12:15:17 -0500

    Fix type of resolver.pipe when one of the pipe functions returns a promise (#1822)
    (patch)

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -75,14 +75,14 @@ export type Middleware = (
 export type FirstParam<F extends QueryFn> = Parameters<F>[0]
 
 /**
- * Get the type of the value, that the Promise holds.
+ * If type has a Promise, unwrap it. Otherwise return the original type
  */
-export type PromiseType<T extends PromiseLike<any>> = T extends PromiseLike<infer U> ? U : T
+export type Await<T> = T extends PromiseLike<infer U> ? U : T
 
 /**
  * Get the return type of a function which returns a Promise.
  */
-export type PromiseReturnType<T extends (...args: any) => Promise<any>> = PromiseType<ReturnType<T>>
+export type PromiseReturnType<T extends (...args: any) => Promise<any>> = Await<ReturnType<T>>
 
 export interface CancellablePromise<T> extends Promise<T> {
   cancel?: Function
