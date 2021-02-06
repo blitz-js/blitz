8a097622df0625539ed739ce36d90e28e76cde36
Brandon Bayer <b@bayer.ws>
2021-02-06 18:23:23 -0500

    add `SimpleRolesIsAuthorized<RoleType>` type so `session.$authorize()` can type check the roles AND update new app template accordingly

--- packages/generator/templates/app/app/auth/mutations/signup.ts
+++ packages/generator/templates/app/app/auth/mutations/signup.ts
@@ -5,7 +5,7 @@ import { Signup } from "app/auth/validations"
 export default resolver.pipe(resolver.zod(Signup), async ({ email, password }, ctx) => {
   const hashedPassword = await SecurePassword.hash(password)
   const user = await db.user.create({
-    data: { email: email.toLowerCase(), hashedPassword, role: "user" },
+    data: { email: email.toLowerCase(), hashedPassword, role: "USER" },
     select: { id: true, name: true, email: true, role: true },
   })
 
