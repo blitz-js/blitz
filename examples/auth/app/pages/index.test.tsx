35b318aea0ff60634fd07625ceadd7f19dc1415c
Brandon Bayer <b@bayer.ws>
2021-02-09 17:19:00 -0500

    fix example test

--- examples/auth/app/pages/index.test.tsx
+++ examples/auth/app/pages/index.test.tsx
@@ -1,10 +1,17 @@
 import {render} from "test/utils"
-
 import Home from "./index"
-import {useCurrentUser} from "app/core/hooks/useCurrentUser"
 
-jest.mock("app/core/hooks/useCurrentUser")
-const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurrentUser>
+jest.mock("blitz", () => ({
+  ...jest.requireActual("blitz")!,
+  useQuery: () => [
+    {
+      id: 1,
+      name: "User",
+      email: "user@email.com",
+      role: "user",
+    },
+  ],
+}))
 
 test("renders blitz documentation link", () => {
   // This is an example of how to ensure a specific item is in the document
@@ -12,12 +19,6 @@ test("renders blitz documentation link", () => {
   // when you remove the the default content from the page
 
   // This is an example on how to mock api hooks when testing
-  mockUseCurrentUser.mockReturnValue({
-    id: 1,
-    name: "User",
-    email: "user@email.com",
-    role: "user",
-  })
 
   const {getByText} = render(<Home />)
   const element = getByText(/powered by blitz/i)
