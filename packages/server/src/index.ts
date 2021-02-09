4b23a38d30f191790ff562ac27a01ee50ca59b70
Brandon Bayer <b@bayer.ws>
2021-02-09 17:25:23 -0500

    wip

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,3 +1,4 @@
+import path from "path"
 export {withBlitz} from "./with-blitz"
 export {build} from "./build"
 export {dev} from "./dev"
@@ -20,3 +21,9 @@ exports = module.exports
 
 // eslint-disable-next-line import/no-default-export
 export default next
+
+// Ensure these files are not eliminated by trace-based tree-shaking (like Vercel)
+path.resolve("next.config.js")
+path.resolve("blitz.config.js")
+path.resolve(".next/blitz/db.js")
+// End anti-tree-shaking
