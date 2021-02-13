bba3a10463a2d39d664236c2431e896cab674e88
Brandon Bayer <b@bayer.ws>
2021-02-13 15:30:00 -0500

    (newapp) add `types.d.ts` with type declarations for CSS/SCSS modules

---
+++ packages/generator/templates/app/types.d.ts
@@ -0,0 +1,8 @@
+declare module "*.module.scss" {
+  const content: { [className: string]: string }
+  export default content
+}
+declare module "*.module.css" {
+  const content: { [className: string]: string }
+  export default content
+}
