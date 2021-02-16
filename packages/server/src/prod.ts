ffcd0b42434838220eb349dcdee647547705e698
Brandon Bayer <b@bayer.ws>
2021-02-16 11:35:53 -0500

    fix error on `blitz start` for new project (patch) (#1952)

--- packages/server/src/prod.ts
+++ packages/server/src/prod.ts
@@ -1,9 +1,14 @@
 import {log} from "@blitzjs/display"
+import * as fs from "fs"
 import {normalize, ServerConfig} from "./config"
 import {customServerExists, nextStart, startCustomServer} from "./next-utils"
 
 export async function prod(config: ServerConfig) {
   const {buildFolder, nextBin} = await normalize(config)
+  if (!fs.existsSync(buildFolder)) {
+    log.error("Could not find a production build, you must run `blitz build` before starting")
+    process.exit(1)
+  }
 
   if (customServerExists(buildFolder)) {
     log.success("Using your custom server")
