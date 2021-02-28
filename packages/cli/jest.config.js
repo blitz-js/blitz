c14de2c4d714ad30b6e78f02e430dd69e91307f4
Brandon Bayer <b@bayer.ws>
2021-02-27 19:11:36 -0500

    remove lodash from core

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   preset: "../../jest.config.js",
   // collectCoverage: !!`Boolean(process.env.CI)`,
-  modulePathIgnorePatterns: ["<rootDir>/tmp", "<rootDir>/lib", "<rootDir>/.test"],
+  modulePathIgnorePatterns: ["<rootDir>/tmp", "<rootDir>/lib", "<rootDir>/commands/.test"],
   testPathIgnorePatterns: ["src/commands/test.ts"],
   testTimeout: 30000,
 }
