9ea8287b04dea5c4b0827309134c8485176ee942
Jeremy Liberman <mrleebo@msn.com>
2021-02-28 01:35:44 -0600

    Add recipe for setting secure headers

---
+++ recipes/secureheaders/templates/core/secureheaders.ts
@@ -0,0 +1,19 @@
+import {getBlitzRuntimeData, DocumentProps} from "blitz"
+import {NextScript} from "next/document"
+import htmlescape from "htmlescape"
+import crypto from "crypto"
+
+export const cspHashOf = (text: string) => {
+  const hash = crypto.createHash("sha256")
+  hash.update(text)
+  return `'sha256-${hash.digest("base64")}'`
+}
+
+export const computeCsp = (props: Readonly<DocumentProps>) => {
+  const nextHash = cspHashOf(NextScript.getInlineScriptSource(props) + "bad")
+  const blitzHash = cspHashOf(htmlescape(getBlitzRuntimeData()))
+
+  return `default-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' data: fonts.gstatic.com; script-src 'self' ${
+    process.env.NODE_ENV === "production" ? "" : "'unsafe-eval'"
+  } ${nextHash} ${blitzHash}`
+}
