be91b8e3ede55d13ae2fbdedee619993e798df79
Brandon Bayer <b@bayer.ws>
2021-02-27 19:45:47 -0500

    cleanup

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -16,8 +16,8 @@ export * from "./supertokens"
 import next from "next"
 
 // Support commonjs `require('blitz')`
-// module.exports = next
-// exports = module.exports
+module.exports = next
+exports = module.exports
 
 // eslint-disable-next-line import/no-default-export
 export default next
