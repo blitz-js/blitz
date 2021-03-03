0dcf30a932f5de29c520e512733d8aa40b8ced70
Simon Knott <info@simonknott.de>
2021-03-03 10:51:43 +0100

    Add "static" example

---
+++ examples/static/blitz.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  middleware: [],
+  /* Uncomment this to customize the webpack config
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    // Note: we provide webpack above so you should not `require` it
+    // Perform customizations to webpack config
+    // Important: return the modified config
+    return config
+  },
+  */
+}
