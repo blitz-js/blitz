78b2afcffc3f37ba138a0ce4e33e43b235c95956
Kevin Langley <me@kevinlangleyjr.com>
2021-03-06 20:46:04 -0500

    Adding changes to _document.tsx

--- recipes/styled-components/index.ts
+++ recipes/styled-components/index.ts
@@ -2,7 +2,7 @@ import {addImport, paths, RecipeBuilder} from "@blitzjs/installer"
 import {NodePath} from "ast-types/lib/node-path"
 import j from "jscodeshift"
 import {Collection} from "jscodeshift/src/Collection"
-import {join} from "path"
+import { join } from "path"
 
 
 function wrapComponentWithStyledComponentsThemeProvider(program: Collection<j.Program>) {
@@ -66,6 +66,145 @@ export default RecipeBuilder()
     templateValues: {},
   })
   .addTransformFilesStep({
+    stepId: "modifyGetInitialPropsInCustomDocumentApp",
+    stepName: "Add custom getInitialProps logic in Custom Document",
+    explanation: `We will add custom getInitialProps logic in _document. We need to do this so that styles are correctly rendered on the server side.`,
+    singleFileSearch: paths.document(),
+    transform(program: Collection<j.Program>) {
+      // import ServerStyleSheets
+      const serverStyleSheetsImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier("ServerStyleSheet"))],
+        j.literal("styled-components"),
+      )
+
+      program.find(j.ImportDeclaration, { source: { value: "blitz" } }).forEach((blitzImportPath) => {
+        if (
+          !blitzImportPath.value.specifiers
+            .filter((spec) => j.ImportSpecifier.check(spec))
+            .some((node) => (node as j.ImportSpecifier)?.imported?.name === "DocumentContext")
+        ) {
+          blitzImportPath.value.specifiers.splice(
+            0,
+            0,
+            j.importSpecifier(j.identifier("DocumentContext")),
+          )
+        }
+      })
+      program.find(j.ClassBody).forEach((path) => {
+        const { node } = path
+
+        const ctxParam = j.identifier("ctx")
+        ctxParam.typeAnnotation = j.tsTypeAnnotation(
+          j.tsTypeReference(j.identifier("DocumentContext")),
+        )
+
+        const getInitialPropsBody = j.blockStatement([
+          j.variableDeclaration("const", [
+            j.variableDeclarator(
+              j.identifier("sheet"),
+              j.newExpression(j.identifier("ServerStyleSheet"), []),
+            ),
+          ]),
+          j.variableDeclaration("const", [
+            j.variableDeclarator(
+              j.identifier("originalRenderPage"),
+              j.memberExpression(j.identifier("ctx"), j.identifier("renderPage")),
+            ),
+          ]),
+          j.expressionStatement(
+            j.assignmentExpression(
+              "=",
+              j.memberExpression(j.identifier("ctx"), j.identifier("renderPage")),
+              j.arrowFunctionExpression(
+                [],
+                j.callExpression(j.identifier("originalRenderPage"), [
+                  j.objectExpression([
+                    j.objectProperty(
+                      j.identifier("enhanceApp"),
+                      j.arrowFunctionExpression(
+                        [j.identifier("App")],
+                        j.arrowFunctionExpression(
+                          [j.identifier("props")],
+                          j.callExpression(
+                            j.memberExpression(j.identifier("sheets"), j.identifier("collectStyles")),
+                            [
+                              j.jsxElement(
+                                j.jsxOpeningElement(
+                                  j.jsxIdentifier("App"),
+                                  [j.jsxSpreadAttribute(j.identifier("props"))],
+                                  true,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ]),
+                ]),
+              ),
+            ),
+          ),
+          j.variableDeclaration("const", [
+            j.variableDeclarator(
+              j.identifier("initialProps"),
+              j.awaitExpression(
+                j.callExpression(
+                  j.memberExpression(j.identifier("Document"), j.identifier("getInitialProps")),
+                  [j.identifier("ctx")],
+                ),
+              ),
+            ),
+          ]),
+          j.returnStatement(
+            j.objectExpression([
+              j.spreadElement(j.identifier("initialProps")),
+              j.objectProperty(
+                j.identifier("styles"),
+                j.jsxFragment(
+                  j.jsxOpeningFragment(),
+                  j.jsxClosingFragment(),
+                  [
+                    j.jsxText("\n"),
+                    j.jsxExpressionContainer(
+                      j.memberExpression(j.identifier("initialProps"), j.identifier("styles")),
+                    ),
+                    j.jsxText("\n"),
+                    j.jsxExpressionContainer(
+                      j.callExpression(
+                        j.memberExpression(j.identifier("sheet"), j.identifier("getStyleElement")),
+                        [],
+                      ),
+                    ),
+                    j.jsxText("\n"),
+                  ]
+                ),
+              ),
+            ]),
+          ),
+        ])
+
+        const getInitialPropsMethod = j.classMethod(
+          "method",
+          j.identifier("getInitialProps"),
+          [ctxParam],
+          getInitialPropsBody,
+          false,
+          true,
+        )
+        getInitialPropsMethod.async = true
+
+        // TODO: better way will be to check if the method already exists and modify it or else add it
+        // currently it gets added assuming it did not exist before
+        node.body.splice(0, 0, getInitialPropsMethod)
+      })
+
+      addImport(program, serverStyleSheetsImport)
+
+      return program
+    },
+  })
+  .addTransformFilesStep({
     stepId: "addThemeProviderToApp",
     stepName: "Import required provider and wrap the root of the app with it",
     explanation: `Additionally we supply ThemeProvider with a basic theme property and base global styles.`,
@@ -87,8 +226,6 @@ export default RecipeBuilder()
 
       addImport(program, styledComponentsProviderImport)
       addImport(program, themeImport)
-
-      // Still need to apply changes to _document.tsx
 
       return wrapComponentWithStyledComponentsThemeProvider(program)
     },
