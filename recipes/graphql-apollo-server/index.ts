814c00f9107688fe013621cfe6ad63a49edab90b
Jeremy Liberman <mrleebo@msn.com>
2021-03-18 01:41:47 -0500

    Rename recipe to graphql-apollo-server and replace type-graphql with nexus-prisma

---
+++ recipes/graphql-apollo-server/index.ts
@@ -0,0 +1,41 @@
+import {addPrismaGenerator, paths, RecipeBuilder} from "@blitzjs/installer"
+import {join} from "path"
+
+export default RecipeBuilder()
+  .setName("GraphQL Apollo Server")
+  .setDescription(
+    `Implements a GraphQL API using Apollo Server that you can extend with your prisma models.`,
+  )
+  .setOwner("jeremy@jeremyliberman.com")
+  .setRepoLink("https://github.com/blitz-js/blitz")
+  .addAddDependenciesStep({
+    stepId: "addDeps",
+    stepName: "Add dependencies",
+    explanation: `Add dependencies for defining and handling a GraphQL endpoint.`,
+    packages: [
+      {name: "apollo-server-micro", version: "2"},
+      {name: "graphql", version: "15"},
+      {name: "prisma", version: "2.19"},
+      {name: "@prisma/client", version: "2.19"},
+      {name: "nexus", version: "1"},
+      {name: "nexus-prisma", version: "0.24"},
+    ],
+  })
+  .addNewFilesStep({
+    stepId: "addGraphqlApi",
+    stepName: "Add GraphQL module",
+    explanation: `Add some GraphQL types and a route to serve the GraphQL API from.`,
+    targetDirectory: "./app/graphql",
+    templatePath: join(__dirname, "templates", "graphql"),
+    templateValues: {},
+  })
+  .addTransformFilesStep({
+    stepId: "updatePrismaSchema",
+    stepName: "Add generator to schema.prisma",
+    explanation: "Adds the nexus-prisma generator to your schema.prisma file",
+    singleFileSearch: paths.prismaSchema(),
+    transformPlain(program: string) {
+      return addPrismaGenerator(program, "nexusPrisma", `provider = "nexus-prisma"`)
+    },
+  })
+  .build()
