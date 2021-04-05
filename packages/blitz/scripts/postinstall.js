a702f776f6bf2a758217a1b921e90f9a5ef1cc68
Brandon Bayer <b@bayer.ws>
2021-04-05 17:56:04 -0400

    fix postinstall script

--- packages/blitz/scripts/postinstall.js
+++ packages/blitz/scripts/postinstall.js
@@ -1,9 +1,14 @@
 const symlinkDir = require("symlink-dir")
 const path = require("path")
+const fs = require("fs")
 
-const nodeModulesPath = path.join(__dirname, "../../")
-symlinkDir(path.join(nodeModulesPath, "@blitzjs/next"), path.join(nodeModulesPath, "next"))
-  .then((result) => {
-    // console.log(result)
-  })
-  .catch((err) => console.error(err))
+const isInBlitzMonorepo = fs.existsSync(path.join(__dirname, "../test"))
+
+if (!isInBlitzMonorepo) {
+  const nodeModulesPath = path.join(__dirname, "../../")
+  symlinkDir(path.join(nodeModulesPath, "@blitzjs/next"), path.join(nodeModulesPath, "next"))
+    .then((result) => {
+      // console.log(result)
+    })
+    .catch((err) => console.error(err))
+}
