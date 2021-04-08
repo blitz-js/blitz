869f22e6851ef53d3e31d7a7265386d5a5aa851f
Brandon Bayer <b@bayer.ws>
2021-04-08 18:17:15 -0400

    link to couple nextjs test files instead of duplicate

--- test/jest-global-teardown.js
+++ test/jest-global-teardown.js
@@ -1,9 +1,1 @@
-let globalTeardown = () => {}
-
-if (process.env.BROWSERSTACK) {
-  globalTeardown = () => global.browserStackLocal.killAllProcesses(() => {})
-}
-
-module.exports = async () => {
-  await globalTeardown()
-}
+../nextjs/test/jest-global-teardown.js
