625089a0bac672af7c38e5d08c550688d8f58968
Brandon Bayer <b@bayer.ws>
2021-04-10 18:01:34 -0400

    a diff try

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -2,4 +2,6 @@
 
 yarn cpx README.md packages/blitz/
 
+sed -i '' 's/name\": \"next\"/name\": \"@blitzjs\/next\"/' nextjs/packages/next/package.json
+
 sed -i '' 's/next\": \"/next\": \"npm:@blitzjs\/next@/' packages/core/package.json
--- scripts/prepublish.sh
+++ scripts/prepublish.sh
@@ -2,5 +2,6 @@
 
 yarn uncommitted
 
-sed -i '' 's/name\": \"next\"/name\": \"@blitzjs\/next\"/' nextjs/packages/next/package.json
-git update-index --assume-unchanged nextjs/packages/next/package.json
+# sed -i '' 's/name\": \"next\"/name\": \"@blitzjs\/next\"/' nextjs/packages/next/package.json
+# This step is reverted in package.json#scripts#postpublish
+# git update-index --assume-unchanged nextjs/packages/next/package.json
