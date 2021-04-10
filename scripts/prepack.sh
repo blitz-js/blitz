b531a5f9807e5fe7fb990ddc9ea98b11c857872c
Brandon Bayer <b@bayer.ws>
2021-04-10 18:08:51 -0400

    cleanup

--- scripts/prepack.sh
+++ scripts/prepack.sh
@@ -2,6 +2,8 @@
 
 yarn cpx README.md packages/blitz/
 
+# Rename nextjs package so we can publish as @blitzjs/next
 sed -i '' 's/name\": \"next\"/name\": \"@blitzjs\/next\"/' nextjs/packages/next/package.json
 
+# Also change next dependency name in blitz core
 sed -i '' 's/next\": \"/next\": \"npm:@blitzjs\/next@/' packages/core/package.json
--- scripts/prepublish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-yarn uncommitted
-
-# sed -i '' 's/name\": \"next\"/name\": \"@blitzjs\/next\"/' nextjs/packages/next/package.json
-# This step is reverted in package.json#scripts#postpublish
-# git update-index --assume-unchanged nextjs/packages/next/package.json
