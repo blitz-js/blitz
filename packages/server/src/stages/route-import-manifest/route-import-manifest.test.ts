9954a6c87ccd7fd05b189fa3af5485c54303901d
Brandon Bayer <b@bayer.ws>
2021-04-19 15:36:23 -0400

    fix type of Routes manifest to not include null | undefined for pathname

--- packages/server/src/stages/route-import-manifest/route-import-manifest.test.ts
+++ packages/server/src/stages/route-import-manifest/route-import-manifest.test.ts
@@ -36,9 +36,13 @@ exports.Routes = {
 import type { UrlObject } from "url"
 import type { ParsedUrlQueryInput } from "querystring"
 
+interface RouteUrlObject extends UrlObject {
+  pathname: string
+}
+
 export const Routes: {
-  Home(query?: ParsedUrlQueryInput): UrlObject;
-  CommentView(query: { postId: string | number; openedCommentPath: (string | number)[] } & ParsedUrlQueryInput): UrlObject;
+  Home(query?: ParsedUrlQueryInput): RouteUrlObject;
+  CommentView(query: { postId: string | number; openedCommentPath: (string | number)[] } & ParsedUrlQueryInput): RouteUrlObject;
 }
       `.trim(),
   })
