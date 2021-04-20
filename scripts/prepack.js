20e6eff5d0d0fbfc9d4501acd0c46f875698d6ad
Brandon Bayer <b@bayer.ws>
2021-04-20 12:00:12 -0400

    cleanup

--- scripts/prepack.js
+++ scripts/prepack.js
@@ -3,12 +3,6 @@ const cpx = require("@juanm04/cpx")
 
 cpx.copy("README.md", "packages/blitz/")
 
-// # Rename nextjs package so we can publish as @blitzjs/next
-// sed -i '' 's/name\": \"next\"/name\": \"@blitzjs\/next\"/' nextjs/packages/next/package.json
-//
-// # Also change next dependency name in blitz core
-// sed -i '' 's/next\": \"/next\": \"npm:@blitzjs\/next@/' packages/core/package.json
-
 const nextJsonPath = "nextjs/packages/next/package.json"
 const nextJson = fs.readJSONSync(nextJsonPath)
 nextJson.name = "@blitzjs/next"
