3a0910098c5af9bdd7b12dc4432c52910f41047e
Roshan Manuel <Roshan,manuel@angelic-group.com>
2021-04-26 17:20:05 -0400

    add Suspense boundary

--- packages/generator/templates/app/app/pages/_app.tsx
+++ packages/generator/templates/app/app/pages/_app.tsx
@@ -9,19 +9,24 @@ import {
 } from "blitz"
 import { ErrorBoundary } from "react-error-boundary"
 import LoginForm from "app/auth/components/LoginForm"
+import { Suspense } from "react"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
   const router = useRouter()
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      resetKeys={[router.asPath]}
-      onReset={useQueryErrorResetBoundary().reset}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
+    // NOTE: This suspense tag provides a convenient global fallback
+    // Make sure to add suspense lower in the tree to optimize loading experience where possible
+    <Suspense fallback="Loading...">
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        resetKeys={[router.asPath]}
+        onReset={useQueryErrorResetBoundary().reset}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </ErrorBoundary>
+    </Suspense>
   )
 }
 
