1d8f69df7e01ca40be1dfe8554c9bf62e9424e63
Roshan Manuel <31125563+Roesh@users.noreply.github.com>
2021-04-27 17:24:06 -0400

    (newapp) Add a `<Suspense>` boundary to `_app.tsx` (#2277)
    Co-authored-by: Roshan Manuel <Roshan,manuel@angelic-group.com>
    Co-authored-by: Brandon Bayer <b@bayer.ws>

--- packages/generator/templates/app/app/pages/_app.tsx
+++ packages/generator/templates/app/app/pages/_app.tsx
@@ -9,19 +9,22 @@ import {
 } from "blitz"
 import { ErrorBoundary } from "react-error-boundary"
 import LoginForm from "app/auth/components/LoginForm"
+import { Suspense } from "react"
 
 export default function App({ Component, pageProps }: AppProps) {
   const getLayout = Component.getLayout || ((page) => page)
   const router = useRouter()
 
   return (
-    <ErrorBoundary
-      FallbackComponent={RootErrorFallback}
-      resetKeys={[router.asPath]}
-      onReset={useQueryErrorResetBoundary().reset}
-    >
-      {getLayout(<Component {...pageProps} />)}
-    </ErrorBoundary>
+    <Suspense fallback="Loading...">
+      <ErrorBoundary
+        FallbackComponent={RootErrorFallback}
+        resetKeys={[router.asPath]}
+        onReset={useQueryErrorResetBoundary().reset}
+      >
+        {getLayout(<Component {...pageProps} />)}
+      </ErrorBoundary>
+    </Suspense>
   )
 }
 
