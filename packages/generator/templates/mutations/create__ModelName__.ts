c354d11c085521ea4f40d4117d98f29e3c534a14
Mina Abadir <mina@abadir.email>
2021-05-23 13:21:49 -0400

    Remove nonstrict from create__ModelName__.ts

--- packages/generator/templates/mutations/create__ModelName__.ts
+++ packages/generator/templates/mutations/create__ModelName__.ts
@@ -6,7 +6,7 @@ if (process.env.parentModel) {
   const Create__ModelName__ = z.object({
     name: z.string(),
     __parentModelId__: z.number()
-  }).nonstrict()
+  })
 } else {
   const Create__ModelName__ = z.object({
     name: z.string(),
