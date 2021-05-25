37be31d41b268cae7b48c44330ca3b5a7f71b468
Brandon Bayer <b@bayer.ws>
2021-05-25 11:40:13 -0500

    Only build blitz config when needed and Fix `blitz.config.js` not found error (patch)

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -8,12 +8,25 @@ import {run as oclifRun} from "@oclif/command"
 // Load the .env environment variable so it's available for all commands
 require("dotenv-expand")(require("dotenv-flow").config({silent: true}))
 
-export function run() {
-  // eslint-disable-next-line @typescript-eslint/no-floating-promises
-  buildConfig().then(() => {
+function buildConfigIfNeeded() {
+  console.log(process.argv)
+  if (["help", "-h", "autocomplete", "new", "s", "start"].includes(process.argv[2])) {
+    return Promise.resolve()
+  }
+
+  return buildConfig()
+}
+
+function runOclif() {
+  return (
     oclifRun()
       .then(require("@oclif/command/flush"))
       // @ts-ignore (TS complains about using `catch`)
       .catch(require("@oclif/errors/handle"))
-  })
+  )
+}
+
+export function run() {
+  // eslint-disable-next-line @typescript-eslint/no-floating-promises
+  buildConfigIfNeeded().then(runOclif)
 }
