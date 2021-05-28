bfeb39982cd48600a4156c17278109b89f7f04f7
Brandon Bayer <b@bayer.ws>
2021-05-28 11:16:39 -0400

    simplify integration test

--- test/integration/trailing-slash/test/index.test.ts
+++ test/integration/trailing-slash/test/index.test.ts
@@ -1,5 +1,5 @@
 /* eslint-env jest */
-import {findPort, killApp, launchApp, renderViaHTTP, waitFor} from "lib/blitz-test-utils"
+import {findPort, killApp, launchApp, renderViaHTTP} from "lib/blitz-test-utils"
 import webdriver from "lib/next-webdriver"
 import {join} from "path"
 
@@ -13,7 +13,7 @@ describe("Queries", () => {
       env: {__NEXT_TEST_WITH_DEVTOOL: 1},
     })
 
-    const prerender = ["/use-query", "/invalidate"]
+    const prerender = ["/use-query"]
     await Promise.all(prerender.map((route) => renderViaHTTP(context.appPort, route)))
   })
   afterAll(() => killApp(context.server))
@@ -26,49 +26,6 @@ describe("Queries", () => {
       await browser.waitForElementByCss("#content")
       text = await browser.elementByCss("#content").text()
       expect(text).toMatch(/basic-result/)
-      if (browser) await browser.close()
-    })
-  })
-
-  describe("invalidateQuery", () => {
-    it("should invalidate the query", async () => {
-      const browser = await webdriver(context.appPort, "/invalidate")
-      await browser.waitForElementByCss("#content")
-      let text = await browser.elementByCss("#content").text()
-      expect(text).toMatch(/0/)
-      await browser.elementByCss("button").click()
-      waitFor(500)
-      text = await browser.elementByCss("#content").text()
-      expect(text).toMatch(/1/)
-
-      if (browser) await browser.close()
-    })
-  })
-
-  describe("useInfiniteQuery", () => {
-    it("should render new query result after param change", async () => {
-      const browser = await webdriver(context.appPort, "/use-infinite-query")
-      await browser.waitForElementByCss("#content")
-      let text = await browser.elementByCss("#content").text()
-      expect(JSON.parse(text)).toEqual([
-        {
-          items: [10, 11, 12, 13, 14],
-          hasMore: true,
-          nextPage: {take: 5, skip: 5},
-          count: 100,
-        },
-      ])
-      await browser.elementByCss("button").click()
-      waitFor(500)
-      text = await browser.elementByCss("#content").text()
-      expect(JSON.parse(text)).toEqual([
-        {
-          items: [11, 12, 13, 14, 15],
-          hasMore: true,
-          nextPage: {take: 5, skip: 5},
-          count: 100,
-        },
-      ])
       if (browser) await browser.close()
     })
   })
