5e4d21d0a160bd34a0b4a4ba1061ab3a447e1509
Brandon Bayer <b@bayer.ws>
2021-06-07 20:16:47 -0400

    more fix

--- examples/store/app/admin/api/users.ts
+++ examples/store/app/admin/api/users.ts
@@ -1,13 +1,5 @@
-import db, {Product} from "db"
+import db from "db"
 import {BlitzApiRequest, BlitzApiResponse} from "blitz"
-import {mean} from "lodash"
-
-// this is here mainly as an integration test for
-// importing from api/
-export function meanPrice(products: Product[]) {
-  const prices = products.map((p) => p.price).filter((p) => !!p) as number[]
-  return mean(prices)
-}
 
 export default async function users(_req: BlitzApiRequest, res: BlitzApiResponse) {
   const products = await db.product.findMany()
