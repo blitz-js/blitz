4d46717c10ed6e582d347af4c2744772e03818c9
Brandon Bayer <b@bayer.ws>
2021-06-16 10:56:51 -0400

    switch our eslint config to use the new next config

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,16 +1,8 @@
 module.exports = {
-  extends: ["react-app", "plugin:jsx-a11y/recommended"],
-  plugins: ["jsx-a11y", "cypress"],
+  extends: ["next"],
   rules: {
     "import/no-anonymous-default-export": "error",
     "import/no-webpack-loader-syntax": "off",
-    "react/react-in-jsx-scope": "off", // React is always in scope with Blitz
-    "jsx-a11y/anchor-is-valid": "off", //Doesn't play well with Blitz/Next <Link> usage
-    "jsx-a11y/label-has-associated-control": "off", //Doesn't play well with form libraries
-  },
-  env: {
-    es2020: true,
-    "cypress/globals": true,
   },
   ignorePatterns: [".next/", ".blitz/"],
 }
