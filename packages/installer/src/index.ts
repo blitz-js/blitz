b09c18811374008d71935470322de260777d3d5e
Nicolas Torres <nicolas.torres.gussoni@gmail.com>
2021-06-19 20:01:22 +0200

    Add support for custom messages and icons in recipes (#2510)
    Adds ability to customize the success message icon in recipes (patch)
    
    Co-authored-by: Nicolas <ntorres.dev@gmail.com>

--- packages/installer/src/index.ts
+++ packages/installer/src/index.ts
@@ -4,6 +4,8 @@ export * from "./executors/executor"
 export {type as AddDependencyType} from "./executors/add-dependency-executor"
 export {type as FileTransformType} from "./executors/file-transform-executor"
 export {type as NewFileType} from "./executors/new-file-executor"
+export {type as PrintMessageType} from "./executors/print-message-executor"
+
 export * from "./utils/paths"
 export * from "./transforms"
 export {customTsParser} from "./utils/transform"
