c9dba04f562d6a7328867a878a5df4271184a97e
Daniel Bannert <prisis@users.noreply.github.com>
2021-06-19 20:19:29 +0200

    Fix so that npm scripts don't run on `blitz install` recipes (#2498)
    Co-authored-by: danielb <daniel.bannert@limango.de> (patch)

--- packages/cli/src/commands/install.ts
+++ packages/cli/src/commands/install.ts
@@ -244,7 +244,7 @@ export class Install extends Command {
 
         const isYarn = require("fs-extra").existsSync(require("path").resolve("yarn.lock"))
         const pkgManager = isYarn ? "yarn" : "npm"
-        const installArgs = isYarn ? ["install"] : ["install", "--legacy-peer-deps"]
+        const installArgs = isYarn ? ["install", "--ignore-scripts"] : ["install", "--legacy-peer-deps", "--ignore-scripts"]
         await new Promise((resolve) => {
           const installProcess = require("cross-spawn")(pkgManager, installArgs)
           installProcess.on("exit", resolve)
