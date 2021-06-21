d16ed02a509cadac82ba335758fd620650f2f2c6
Simon Knott <info@simonknott.de>
2021-06-21 19:23:26 +0200

    Fix prefetched dehydrated state to support Dates, Maps, etc (patch) (#2512)
    Co-authored-by: Brandon Bayer <b@bayer.ws>

--- nextjs/test/integration/config/test/index.test.js
+++ nextjs/test/integration/config/test/index.test.js
@@ -54,7 +54,7 @@ describe('Configuration', () => {
 
   it('should log webpack version correctly', async () => {
     expect(context.output).toContain(
-      `Using webpack 4. Reason: webpack5 flag is set to false in next.config.js`
+      `Using webpack 4. Reason: webpack5 flag is set to false in blitz.config.js`
     )
   })
 
