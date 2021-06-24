1e4a148e6a7a9f2b20445ebb2a4522447dce0c0b
Brandon Bayer <b@bayer.ws>
2021-06-24 10:25:26 -0600

    cleanup

--- nextjs/packages/next/build/index.ts
+++ nextjs/packages/next/build/index.ts
@@ -243,7 +243,6 @@ export default async function build(
     const pagePaths: string[] = await nextBuildSpan
       .traceChild('collect-pages')
       .traceAsyncFn(() => collectPages(pagesDir, config.pageExtensions))
-    console.log('pagePaths', pagePaths)
 
     // needed for static exporting since we want to replace with HTML
     // files
