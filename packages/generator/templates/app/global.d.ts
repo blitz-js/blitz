aeb2a389eace166aa7437670a2112408981e93b7
Sam Holmes <samholmes1337@gmail.com>
2021-07-29 16:21:17 +0100

    Fix duplicate image types (requires adding line to global.d.ts) (#2597)
    (patch)

--- packages/generator/templates/app/global.d.ts
+++ packages/generator/templates/app/global.d.ts
@@ -1,3 +1,4 @@
 // These reference imports provide type definitions for things like styled-jsx and css modules
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+/// <reference types="next/image-types/global" />
