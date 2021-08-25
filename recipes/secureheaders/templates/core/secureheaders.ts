8d98843573fbb98773e2845579a0de12d65ca9b7
Brandon Bayer <b@bayer.ws>
2021-08-25 11:38:46 -0400

    (recipe) fix secureheaders recipe

--- recipes/secureheaders/templates/core/secureheaders.ts
+++ recipes/secureheaders/templates/core/secureheaders.ts
@@ -1,5 +1,4 @@
-import {getBlitzRuntimeData, DocumentProps} from "blitz"
-import {NextScript} from "next/document"
+import {BlitzScript, DocumentProps} from "blitz"
 import htmlescape from "htmlescape"
 import crypto from "crypto"
 
@@ -10,10 +9,9 @@ export const cspHashOf = (text: string) => {
 }
 
 export const computeCsp = (props: Readonly<DocumentProps>) => {
-  const nextHash = cspHashOf(NextScript.getInlineScriptSource(props))
-  const blitzHash = cspHashOf(htmlescape(getBlitzRuntimeData()))
+  const scriptHash = cspHashOf(BlitzScript.getInlineScriptSource(props))
 
   return `default-src 'self'; style-src 'self' 'unsafe-inline' fonts.googleapis.com; font-src 'self' data: fonts.gstatic.com; script-src 'self' ${
     process.env.NODE_ENV === "production" ? "" : "'unsafe-eval'"
-  } ${nextHash} ${blitzHash}`
+  } ${scriptHash}`
 }
