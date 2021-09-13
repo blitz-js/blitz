7a3f57c8e371eb8440f4f7098fdb1e27c10e1459
Roshan Manuel <Roshan,manuel@angelic-group.com>
2021-09-13 15:26:55 -0400

    update abstract builder with common functions

--- packages/generator/src/generators/template-builders/builder.ts
+++ packages/generator/src/generators/template-builders/builder.ts
@@ -1,4 +1,5 @@
-import {GeneratorOptions} from "@babel/generator"
+import {GeneratorOptions} from "../../generator"
+import {camelCaseToKebabCase} from "../../utils/inflector"
 
 export interface IBuilder<T> {
   getTemplateValues(Options: T): Promise<any>
@@ -27,5 +28,19 @@ export abstract class Builder<T> implements IBuilder<T> {
   public getParam(input: string = "") {
     if (!input) return input
     return `[${input}]`
+  }
+
+  public getModelNamesPath(context: string | undefined, modelNames: string) {
+    const kebabCaseContext = context ? `${camelCaseToKebabCase(context)}/` : ""
+    const kebabCaseModelNames = camelCaseToKebabCase(modelNames)
+    return kebabCaseContext + kebabCaseModelNames
+  }
+
+  public getZodTypeName(type: string = "") {
+    if (["string", "null", "undefined", "unknown", "void", "boolean"].includes(type)) {
+      return type
+    } else {
+      return type === "int" ? "number" : "any"
+    }
   }
 }
