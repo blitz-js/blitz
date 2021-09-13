850190454fcd669e6447f3ddd0d1777c2564cad2
Roshan Manuel <Roshan,manuel@angelic-group.com>
2021-09-13 18:11:07 -0400

    fix lint again 2

--- packages/generator/src/generators/template-builders/null-builder.ts
+++ packages/generator/src/generators/template-builders/null-builder.ts
@@ -1,6 +1,7 @@
 import {IBuilder} from "./builder"
 
 export const NullBuilder: IBuilder<any> = {
+  // eslint-disable-next-line require-await
   getTemplateValues: async () => {
     return {}
   },
