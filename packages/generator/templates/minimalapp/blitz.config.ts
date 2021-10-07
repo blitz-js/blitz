df846929cb6e21cd889c3f555c8af00caf6d48ae
Aleksandra Sikora <alexsandra.sikora@gmail.com>
2021-10-07 13:12:03 +0200

    Update packages/generator/templates/minimalapp/blitz.config.ts
    Co-authored-by: Brandon Bayer <b@bayer.ws>

--- packages/generator/templates/minimalapp/blitz.config.ts
+++ packages/generator/templates/minimalapp/blitz.config.ts
@@ -1,12 +1,6 @@
-import { BlitzConfig, sessionMiddleware, simpleRolesIsAuthorized } from "blitz"
+import { BlitzConfig } from "blitz"
 
 const config: BlitzConfig = {
-  middleware: [
-    sessionMiddleware({
-      cookiePrefix: "__safeNameSlug__",
-      isAuthorized: simpleRolesIsAuthorized,
-    }),
-  ],
   /* Uncomment this to customize the webpack config
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Note: we provide webpack above so you should not `require` it
