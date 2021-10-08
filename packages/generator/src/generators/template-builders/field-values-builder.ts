dfe3d0255eeb3b0d7d8b878632d31c7565d74a8e
Roshan Manuel <Roshan,manuel@angelic-group.com>
2021-10-08 07:42:32 -0400

    field values infers parent model id type

--- packages/generator/src/generators/template-builders/field-values-builder.ts
+++ packages/generator/src/generators/template-builders/field-values-builder.ts
@@ -1,8 +1,21 @@
+import {getPrismaSchema} from "../../utils/get-prisma-schema"
 import {FieldGeneratorOptions, Builder, CommonTemplateValues} from "./builder"
+import {create as createStore} from "mem-fs"
+import {create as createEditor, Editor} from "mem-fs-editor"
+import * as ast from "@mrleebo/prisma-ast"
 
 export class FieldValuesBuilder extends Builder<FieldGeneratorOptions, CommonTemplateValues> {
+  private getEditor = (): Editor => {
+    if (this.fs !== undefined) {
+      return this.fs
+    }
+    const store = createStore()
+    this.fs = createEditor(store)
+    return this.fs
+  }
+
   // eslint-disable-next-line require-await
-  public async getTemplateValues(options: FieldGeneratorOptions):Promise<CommonTemplateValues> {
+  public async getTemplateValues(options: FieldGeneratorOptions): Promise<CommonTemplateValues> {
     const values = {
       parentModelId: this.getId(options.parentModel),
       parentModelIdZodType: "number", // TODO: Need to parse prisma schema and get parent ID somehow
@@ -25,7 +38,7 @@ export class FieldValuesBuilder extends Builder<FieldGeneratorOptions, CommonTem
 
       const checks = options.extraArgs.map(async (arg) => {
         const [valueName, typeName] = arg.split(":")
-        if(valueName === "id"){          
+        if (valueName === "id") {
           values.modelIdZodType = await this.getZodTypeName(typeName)
           argValue = arg
         }
@@ -34,6 +47,46 @@ export class FieldValuesBuilder extends Builder<FieldGeneratorOptions, CommonTem
 
       // remove id argument before processing
       const nonIdArgs = options.extraArgs.filter((arg) => arg !== argValue)
+
+      if (options.parentModel !== undefined && options.parentModel.length > 0) {
+        const {schema} = getPrismaSchema(this.getEditor())
+
+        const model = schema.list.find(function (component): component is ast.Model {
+          return component.type === "model" && component.name === "Project"
+          //TODO: Check case sensitivity, is  component.name === options.parentModel || component.name === options.ParentModel necessary
+          // Case sensitvity is important. Schema.prisma allows both project and Project to exist in the same file
+          // We need to inform users to pass in the exact name (in the docs), and we also need to access the raw argument passed in by the
+          // user here.
+        })
+
+        if (model !== undefined) {
+          // O(N * M), N - Number of prisma models, M - average # properties per model
+          const idField = model.properties.find(function (property): property is ast.Field {
+            return (
+              property.type === "field" &&
+              property.attributes?.findIndex((attr) => attr.name === "id") !== -1
+            )
+          })
+
+          // TODO: Do we want a map between prisma types and "user types", we can then use that map instead of these conditionals
+          if (idField?.fieldType === "Int") {
+            values.parentModelIdZodType = "number"
+          } else if (idField?.fieldType === "String") {            
+            if (
+              idField.attributes?.find(
+                (attr) => attr.name === "default" && attr.args?.findIndex((arg) => arg.value === "uuid") !== -1,
+              )
+            ) {
+              values.parentModelIdZodType = "string().uuid"
+            } else {
+              values.parentModelIdZodType = "string"
+            }
+          }
+        } else {
+          // handle scenario where parent wasnt found in existing schema. Should we throw an error, or a warning asking the user to verify that the parent model exists?
+        }
+      }
+
       const ftv = await this.getFieldTemplateValues(nonIdArgs)
       return {...values, fieldTemplateValues: ftv}
     }
