0b0a983f995eab5e1b940910470727e435135011
Aleksandra <alexsandra.sikora@gmail.com>
2021-10-08 15:21:17 +0200

    add more tests

--- packages/cli/test/commands/new.test.ts
+++ packages/cli/test/commands/new.test.ts
@@ -82,12 +82,12 @@ describe("`new` command", () => {
     }
 
     async function withNewApp(
-      options: {flags?: string[]},
+      flags: string[],
       test: (dirName: string, packageJson: any) => Promise<void> | void,
     ) {
       const tempDir = makeTempDir()
 
-      await whileStayingInCWD(() => New.run([tempDir, "--skip-install", ...options.flags]))
+      await whileStayingInCWD(() => New.run([tempDir, "--skip-install", ...flags]))
 
       const packageJsonFile = fs.readFileSync(path.join(tempDir, "package.json"), {
         encoding: "utf8",
@@ -103,7 +103,7 @@ describe("`new` command", () => {
     testIfNotWindows(
       "pins Blitz to the current version",
       async () =>
-        await withNewApp({}, async (_dirName, packageJson) => {
+        await withNewApp([], async (_dirName, packageJson) => {
           const {
             dependencies: {blitz: blitzVersion},
           } = packageJson
@@ -165,7 +165,7 @@ describe("`new` command", () => {
     })
 
     testIfNotWindows("generates minimal app with --template=minimal flag", async () => {
-      await withNewApp({flags: ["--template=minimal"]}, (dirName, packageJson) => {
+      await withNewApp(["--template=minimal"], (dirName, packageJson) => {
         const {
           dependencies: {prisma, zod},
         } = packageJson
@@ -179,6 +179,25 @@ describe("`new` command", () => {
         })
         expect(
           readme.includes("This is a minimal [Blitz.js](https://github.com/blitz-js/blitz) app."),
+        ).toBe(true)
+      })
+    })
+
+    testIfNotWindows("generates full app with --template=full flag", async () => {
+      await withNewApp(["--template=full"], (dirName, packageJson) => {
+        const {
+          dependencies: {"@prisma/client": prismaClient, zod},
+        } = packageJson
+
+        expect(prismaClient).not.toBeUndefined()
+        expect(zod).not.toBeUndefined()
+
+        const readme = fs.readFileSync(path.join(dirName, "README.md"), {
+          encoding: "utf8",
+          flag: "r",
+        })
+        expect(
+          readme.includes("This is a [Blitz.js](https://github.com/blitz-js/blitz) app."),
         ).toBe(true)
       })
     })
@@ -212,7 +231,7 @@ describe("`new` command", () => {
       testIfNotWindows("uses template versions", async () => {
         nock("https://registry.npmjs.org").get(/.*/).reply(500).persist()
 
-        await withNewApp({}, (_, packageJson) => {
+        await withNewApp([], (_, packageJson) => {
           const {dependencies} = packageJson
           expect(dependencies.blitz).toBe("latest")
         })
