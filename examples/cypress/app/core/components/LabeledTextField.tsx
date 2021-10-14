3d17eae8b8d86240f84ffb17cb65fd00380a2bce
Aleksandra Sikora <alexsandra.sikora@gmail.com>
2021-10-15 00:17:39 +0200

    Add Cypress example (#2836)

---
+++ examples/cypress/app/core/components/LabeledTextField.tsx
@@ -0,0 +1,49 @@
+import { forwardRef, ComponentPropsWithoutRef, PropsWithoutRef } from "react"
+import { useField, UseFieldConfig } from "react-final-form"
+
+export interface LabeledTextFieldProps extends PropsWithoutRef<JSX.IntrinsicElements["input"]> {
+  /** Field name. */
+  name: string
+  /** Field label. */
+  label: string
+  /** Field type. Doesn't include radio buttons and checkboxes */
+  type?: "text" | "password" | "email" | "number"
+  outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
+  labelProps?: ComponentPropsWithoutRef<"label">
+  fieldProps?: UseFieldConfig<string>
+}
+
+export const LabeledTextField = forwardRef<HTMLInputElement, LabeledTextFieldProps>(
+  ({ name, label, outerProps, fieldProps, labelProps, ...props }, ref) => {
+    const {
+      input,
+      meta: { touched, error, submitError, submitting },
+    } = useField(name, {
+      parse:
+        props.type === "number"
+          ? (Number as any)
+          : // Converting `""` to `null` ensures empty values will be set to null in the DB
+            (v) => (v === "" ? null : v),
+      ...fieldProps,
+    })
+
+    const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError
+
+    return (
+      <div {...outerProps}>
+        <label {...labelProps}>
+          {label}
+          <input {...input} disabled={submitting} {...props} ref={ref} />
+        </label>
+
+        {touched && normalizedError && (
+          <div role="alert" style={{ color: "red" }}>
+            {normalizedError}
+          </div>
+        )}
+      </div>
+    )
+  }
+)
+
+export default LabeledTextField
