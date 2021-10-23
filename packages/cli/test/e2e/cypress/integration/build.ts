b0ffd09be258f75c7d998d159db3e14650f2016e
Reda <loukil.red@gmail.com>
2021-10-24 01:22:53 +0200

    setup application and build it using cypres exec

---
+++ packages/cli/test/e2e/cypress/integration/build.ts
@@ -0,0 +1,17 @@
+describe("It should build the project", () => {
+  it("App build", () => {
+    cy.exec("rimraf -rf app")
+    cy.exec(`../blitz/bin/blitz new app --template=full --yarn --form=react-final-form`, {
+      log: true,
+      timeout: 120000,
+    })
+    cy.exec(`../blitz/bin/blitz -v`, {log: true, timeout: 120000})
+    cy.exec(
+      `echo "declare module 'react' { interface StyleHTMLAttributes<T> extends React.HTMLAttributes<T> { jsx?: boolean global?: boolean }}" >> app/types.ts`,
+    )
+    cy.exec("export NODE_TLS_REJECT_UNAUTHORIZED=1;cd app;yarn;yarn run build", {
+      log: true,
+      timeout: 120000,
+    })
+  })
+})
