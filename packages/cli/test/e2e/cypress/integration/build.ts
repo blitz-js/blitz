af0dce66a21d845140b2638cab66c24e3205aa7c
Reda <loukil.red@gmail.com>
2021-10-25 00:00:00 +0200

    get blitz version and log it on the stdoutput, force package manager to install the same version on the generated repo

--- packages/cli/test/e2e/cypress/integration/build.ts
+++ packages/cli/test/e2e/cypress/integration/build.ts
@@ -1,18 +1,33 @@
 describe("It should build the project", () => {
   it("App build", () => {
+    const packageManager = "yarn"
     cy.exec("rimraf -rf app")
     cy.exec(
-      `../blitz/bin/blitz new app --template=full --yarn --form=react-final-form --language=typescript`,
+      `../blitz/bin/blitz new app --template=full --${packageManager} --form=react-final-form --language=typescript`,
       {
         log: true,
         timeout: 120000,
       },
     )
     cy.exec(`../blitz/bin/blitz -v`, {log: true, timeout: 120000})
-    cy.exec("node ./test/e2e/cypress/integration/helpers/append-file.js")
-    cy.exec("export NODE_TLS_REJECT_UNAUTHORIZED=1;cd app;yarn;yarn run build", {
-      log: true,
-      timeout: 120000,
+
+    cy.exec(`node ./test/e2e/cypress/integration/helpers/blitz-cli-version.js`).then((version) => {
+      cy.exec(`cd app && ${packageManager} add blitz@${version.stdout} --save`)
+      cy.exec(`node ./test/e2e/cypress/integration/helpers/blitz-cli-version.js`).then(
+        (versionTest) => {
+          expect(version.stdout).eqls(versionTest.stdout)
+        },
+      )
     })
+
+    cy.exec("node ./test/e2e/cypress/integration/helpers/append-file.js")
+    cy.exec(
+      `export NODE_TLS_REJECT_UNAUTHORIZED=1;cd app;${packageManager};${packageManager} run build`,
+      {
+        log: true,
+        timeout: 120000,
+      },
+    )
+    cy.exec("rimraf -rf app")
   })
 })
--- packages/cli/test/e2e/cypress/integration/helpers/blitz-cli-version.js
+++ packages/cli/test/e2e/cypress/integration/helpers/blitz-cli-version.js
@@ -1,3 +1,6 @@
 const path = require("path")
 
 const {dependencies} = require(`${path.join(process.cwd(), "../blitz")}/package.json`)
+
+//log the dependency version to get in cypress.exec() function output
+console.log(dependencies["@blitzjs/cli"])
