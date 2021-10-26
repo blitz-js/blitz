d711d744a31970af2e6e5b59701e2a39a8c45e20
Reda <loukil.red@gmail.com>
2021-10-26 23:09:56 +0200

    delete helpers scripts

--- packages/cli/test/e2e/cypress/integration/helpers/blitz-cli-version.js
+++
@@ -1,6 +0,0 @@
-const path = require("path")
-
-const {dependencies} = require(`${path.join(process.cwd(), "../blitz")}/package.json`)
-
-//log the dependency version to get in cypress.exec() function output
-console.log(dependencies["@blitzjs/cli"])
