2fab47c25d51bc8d7ec95131bc122494dbeb6577
JuanM04 <me@juanm04.com>
2021-11-14 14:38:50 -0300

    Updated official recipes test

--- packages/cli/test/commands/install.test.ts
+++ packages/cli/test/commands/install.test.ts
@@ -29,39 +29,32 @@ describe("`install` command", () => {
       location: RecipeLocation.Remote,
     })
   })
-  describe("run official recipes list flag ", () => {
-    it("list of official recipes ", () => {
-      const getOfficialRecipeList = Install.prototype.getOfficialRecipeList
-      return getOfficialRecipeList().then((recipeList) => {
-        expect(recipeList).toEqual(
-          expect.arrayContaining([
-            "base-web",
-            "bumbag-ui",
-            "chakra-ui",
-            "emotion",
-            "gh-action-yarn-mariadb",
-            "gh-action-yarn-postgres",
-            "ghost",
-            "graphql-apollo-server",
-            "logrocket",
-            "material-ui",
-            "quirrel",
-            "reflexjs",
-            "render",
-            "secureheaders",
-            "stitches",
-            "styled-components",
-            "tailwind",
-            "theme-ui",
-          ]),
-        )
-      })
-    })
-    it("not to be included tsconfig.json of official recipes", () => {
-      const getOfficialRecipeList = Install.prototype.getOfficialRecipeList
-      return getOfficialRecipeList().then((recipeList) => {
-        expect(recipeList).toEqual(expect.not.arrayContaining(["tsconfig.json"]))
-      })
-    })
+
+  it("list of official recipes", async () => {
+    const recipeList = await Install.prototype.getOfficialRecipeList()
+
+    expect(recipeList).toEqual(
+      expect.arrayContaining([
+        "base-web",
+        "bumbag-ui",
+        "chakra-ui",
+        "emotion",
+        "gh-action-yarn-mariadb",
+        "gh-action-yarn-postgres",
+        "ghost",
+        "graphql-apollo-server",
+        "logrocket",
+        "material-ui",
+        "quirrel",
+        "reflexjs",
+        "render",
+        "secureheaders",
+        "stitches",
+        "styled-components",
+        "tailwind",
+        "theme-ui",
+      ]),
+    )
+    expect(recipeList).toEqual(expect.not.arrayContaining(["tsconfig.json"]))
   })
 })
