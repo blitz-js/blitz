d65d9ec6ae4c0127aed6723b16a336890f2801da
Aleksandra <alexsandra.sikora@gmail.com>
2021-11-30 18:40:10 +0100

    update cache key and fix next config

--- nextjs/test/integration/env-config-app-env/app/next.config.js
+++ nextjs/test/integration/env-config-app-env/app/next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   cleanDistDir: false,
-  target: 'experimental-serverless-trace',
+  // update me
   env: {
     nextConfigEnv: process.env.ENV_KEY_IN_NEXT_CONFIG,
     nextConfigPublicEnv: process.env.NEXT_PUBLIC_ENV_KEY_IN_NEXT_CONFIG,
