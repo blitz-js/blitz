b1faff2052029ba1a7c649f1ea00c297af8202a3
Aleksandra <alexsandra.sikora@gmail.com>
2021-12-02 11:08:38 +0100

    revert the change

--- packages/blitz/jest-preset/global-setup.js
+++ packages/blitz/jest-preset/global-setup.js
@@ -1,5 +1,6 @@
 // eslint-disable-next-line import/no-default-export
 import {loadEnvConfig} from "@blitzjs/env"
 module.exports = function globalSetup(globalConfig) {
-  loadEnvConfig()
+  const projectDir = process.cwd()
+  loadEnvConfig(projectDir)
 }
