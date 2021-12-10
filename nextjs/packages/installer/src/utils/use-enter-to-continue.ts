5f1c6a4571a4c7a87f6308e7580954ebde2a61e0
Aleksandra Sikora <alexsandra.sikora@gmail.com>
2021-12-10 13:26:54 +0100

    Move installer package to nextjs/packages (#3044)
    (patch)

---
+++ nextjs/packages/installer/src/utils/use-enter-to-continue.ts
@@ -0,0 +1,12 @@
+import { useInput } from 'ink'
+
+export function useEnterToContinue(
+  cb: Function,
+  additionalCondition: boolean = true
+) {
+  useInput((_input, key) => {
+    if (additionalCondition && key.return) {
+      cb()
+    }
+  })
+}
