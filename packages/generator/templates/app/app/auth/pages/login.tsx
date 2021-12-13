9a00412049303e2ac6c50b33a1a23a028181da4a
Francesco Sardo <francesco.deploy@gmail.com>
2021-12-13 15:48:04 +0000

    Fix inconsistent formatting in `LoginForm` component and `Login` page (#3063)
    (newapp)

--- packages/generator/templates/app/app/auth/pages/login.tsx
+++ packages/generator/templates/app/app/auth/pages/login.tsx
@@ -1,6 +1,6 @@
-import {useRouter, BlitzPage} from "blitz"
+import { useRouter, BlitzPage } from "blitz"
 import Layout from "app/core/layouts/Layout"
-import {LoginForm} from "app/auth/components/LoginForm"
+import { LoginForm } from "app/auth/components/LoginForm"
 
 const LoginPage: BlitzPage = () => {
   const router = useRouter()
