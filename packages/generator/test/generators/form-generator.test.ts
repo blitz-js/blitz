9dba3767106c5083940390e965d71e37810f016b
Aleksandra <alexsandra.sikora@gmail.com>
2021-12-14 15:08:09 +0100

    bring back removed test

--- packages/generator/test/generators/form-generator.test.ts
+++ packages/generator/test/generators/form-generator.test.ts
@@ -26,5 +26,21 @@ describe("Form Generator", () => {
       `<__component__ name="__fieldName__" label="__Field_Name__" placeholder="__Field_Name__" />`,
     )
     expect(curlyBraceComment1.match(regex)?.[0].replace(regex, "$2$3")).not.toBe(`something Random`)
+
+    const normalComment1 = `// template: __fieldName__: z.__zodType__(),`
+    expect(normalComment1.match(regex)?.[0].replace(regex, "$2$3")).toBe(
+      `__fieldName__: z.__zodType__(),`,
+    )
+    expect(normalComment1.match(regex)?.[0].replace(regex, "$2$3")).not.toBe(`something Random`)
+
+    const commentWithSpacing = `//     template: __fieldName__: z.__zodType__(),`
+    const commentWithNoSpacing = `//template: __fieldName__: z.__zodType__(),`
+
+    expect(commentWithSpacing.match(regex)?.[0].replace(regex, "$2$3")).toBe(
+      `__fieldName__: z.__zodType__(),`,
+    )
+    expect(commentWithNoSpacing.match(regex)?.[0].replace(regex, "$2$3")).toBe(
+      `__fieldName__: z.__zodType__(),`,
+    )
   })
 })
