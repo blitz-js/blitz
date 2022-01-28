27837e405bbe865d167c2d97fcd37668a1926202
mochi-sann <44772513+mochi-sann@users.noreply.github.com>
2022-01-28 14:36:56 +0900

    update index.ts

---
+++ recipes/next-ui/index.ts
@@ -0,0 +1,210 @@
+import {addImport, paths, Program, RecipeBuilder} from "@blitzjs/installer"
+import type {NodePath} from "ast-types/lib/node-path"
+import j, {JSXIdentifier} from "jscodeshift"
+
+// Copied from https://github.com/blitz-js/blitz/pull/805, let's add this to the @blitzjs/installer
+function wrapComponentWithChakraProvider(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter(
+      (path) =>
+        path.parent?.parent?.parent?.value?.id?.name === "App" &&
+        path.parent?.value.type === j.ReturnStatement.toString(),
+    )
+    .forEach((path: NodePath) => {
+      const {node} = path
+      path.replace(
+        j.jsxElement(
+          j.jsxOpeningElement(j.jsxIdentifier("NextUIProvider")),
+          j.jsxClosingElement(j.jsxIdentifier("NextUIProvider")),
+          [j.jsxText("\n"), node, j.jsxText("\n")],
+        ),
+      )
+    })
+  return program
+}
+
+function updateLabeledTextFieldWithInputComponent(program: Program) {
+  program
+    .find(j.TSInterfaceDeclaration)
+    .find(j.TSExpressionWithTypeArguments)
+    .forEach((path: j.ASTPath<j.TSExpressionWithTypeArguments>) => {
+      path.replace(
+        j.tsExpressionWithTypeArguments(
+          j.identifier("ComponentPropsWithoutRef"),
+          j.tsTypeParameterInstantiation([j.tsTypeQuery(j.identifier("Input"))]),
+        ),
+      )
+    })
+
+  return program
+}
+
+function replaceOuterDivWithFormControl(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter((path) => {
+      const {node} = path
+      const openingElementNameNode = node?.openingElement?.name as JSXIdentifier
+
+      // This will not include JSX elements within curly braces
+      const countOfChildrenJSXElements =
+        path.node.children?.filter((childNode) => childNode.type === "JSXElement").length || 0
+
+      return (
+        openingElementNameNode?.name === "div" &&
+        node?.openingElement?.selfClosing === false &&
+        countOfChildrenJSXElements === 1
+      )
+    })
+    .forEach((path) => {
+      path.node.openingElement = j.jsxOpeningElement(
+        j.jsxIdentifier("FormControl"),
+        path.node.openingElement.attributes,
+      )
+      path.node.closingElement = j.jsxClosingElement(j.jsxIdentifier("FormControl"))
+    })
+
+  return program
+}
+
+function replaceInputWithChakraInput(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter((path) => {
+      const {node} = path
+      const openingElementNameNode = node?.openingElement?.name as JSXIdentifier
+
+      return openingElementNameNode?.name === "input" && node?.openingElement?.selfClosing === true
+    })
+    .forEach((path) => {
+      const {node} = path
+      node.openingElement = j.jsxOpeningElement(
+        j.jsxIdentifier("Input"),
+        node.openingElement.attributes,
+        node?.openingElement?.selfClosing,
+      )
+    })
+
+  return program
+}
+
+function replaceLabelWithChakraLabel(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter((path) => {
+      const {node} = path
+      const openingElementNameNode = node?.openingElement?.name as JSXIdentifier
+
+      return openingElementNameNode?.name === "label" && node?.openingElement?.selfClosing === false
+    })
+    .forEach((path) => {
+      path.node.openingElement = j.jsxOpeningElement(
+        j.jsxIdentifier("FormLabel"),
+        path.node.openingElement.attributes,
+      )
+      path.node.closingElement = j.jsxClosingElement(j.jsxIdentifier("FormLabel"))
+    })
+
+  return program
+}
+
+function removeDefaultStyleElement(program: Program) {
+  program
+    .find(j.JSXElement)
+    .filter((path) => {
+      const {node} = path
+      const openingElementNameNode = node?.openingElement?.name as JSXIdentifier
+
+      // Assumes there's one style element at the point the user runs the recipe.
+      return openingElementNameNode?.name === "style" && node?.openingElement?.selfClosing === false
+    })
+    .forEach((path) => {
+      // Removes the node.
+      path.replace()
+    })
+
+  return program
+}
+
+export default RecipeBuilder()
+  .setName("Next UI")
+  .setDescription(`This will install all necessary dependencies and configure Next UI for use.`)
+  .setOwner("moyurusuto.mochi@gmail.com")
+  .setRepoLink("https://github.com/blitz-js/blitz")
+  .addAddDependenciesStep({
+    stepId: "Install",
+    stepName: "Insstall Next UI",
+    explanation: `Import the chakra-ui provider into _app, so it is accessible in the whole app`,
+    packages: [{name: "@nextui-org/react", version: "1.x"}],
+  })
+  .addTransformFilesStep({
+    stepId: "importProviderAndReset",
+    stepName: "Import ChakraProvider component",
+    explanation: `Import the chakra-ui provider into _app, so it is accessible in the whole app`,
+    singleFileSearch: paths.app(),
+    transform(program) {
+      const stylesImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier("NextUIProvider"))],
+        j.literal("@nextui-org/react"),
+      )
+
+      addImport(program, stylesImport)
+      return wrapComponentWithChakraProvider(program)
+    },
+  })
+  .addTransformFilesStep({
+    stepId: "updateLabeledTextField",
+    stepName: "Update the `LabeledTextField` with Chakra UI's `Input` component",
+    explanation: `The LabeledTextField component uses Chakra UI's input component`,
+    singleFileSearch: "app/core/components/LabeledTextField.tsx",
+    transform(program) {
+      // Add ComponentPropsWithoutRef import
+      program.find(j.ImportDeclaration, {source: {value: "react"}}).forEach((path) => {
+        let specifiers = path.value.specifiers || []
+        if (
+          !specifiers.some(
+            (node) => (node as j.ImportSpecifier)?.imported?.name === "ComponentPropsWithoutRef",
+          )
+        ) {
+          specifiers.push(j.importSpecifier(j.identifier("ComponentPropsWithoutRef")))
+        }
+      })
+
+      const chakraInputImport = j.importDeclaration(
+        [j.importSpecifier(j.identifier("Input"))],
+        j.literal("@chakra-ui/input"),
+      )
+
+      const chakraFormControlImport = j.importDeclaration(
+        [
+          j.importSpecifier(j.identifier("FormControl")),
+          j.importSpecifier(j.identifier("FormLabel")),
+        ],
+        j.literal("@chakra-ui/form-control"),
+      )
+
+      addImport(program, chakraInputImport)
+      addImport(program, chakraFormControlImport)
+
+      // Imperative steps to describe transformations
+
+      // 1. Update the type of `LabeledTextField`
+      updateLabeledTextFieldWithInputComponent(program)
+
+      // 2. Remove the default <style jsx> styling
+      removeDefaultStyleElement(program)
+
+      // 3. Replace outer div with `FormControl`
+      replaceOuterDivWithFormControl(program)
+
+      // 4. Replace `input` with `ChakraInput`
+      replaceInputWithChakraInput(program)
+
+      // 5. Replace `label` with `ChakraLabel`
+      replaceLabelWithChakraLabel(program)
+
+      return program
+    },
+  })
+  .build()
