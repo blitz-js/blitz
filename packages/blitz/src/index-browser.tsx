6ff7e99d15b7bc755579279a7e3424ba8c86caad
Aleksandra <alexsandra.sikora@gmail.com>
2022-03-11 14:35:11 -0500

    Include client error patch in blitz core (#3232)

--- packages/blitz/src/index-browser.tsx
+++ packages/blitz/src/index-browser.tsx
@@ -1,6 +1,7 @@
 import "./global"
 import React, {ComponentType} from "react"
 import {IncomingMessage, ServerResponse} from "http"
+import {AuthenticationError, AuthorizationError, NotFoundError, RedirectError} from "./errors"
 
 export type BlitzProvider = <TProps = any>(
   component: ComponentType<TProps>,
@@ -35,6 +36,43 @@ export function createClientPlugin<TPluginOptions, TPluginExports extends object
   pluginConstructor: (options: TPluginOptions) => ClientPlugin<TPluginExports>,
 ) {
   return pluginConstructor
+}
+
+if (process.env.NODE_ENV === "development") {
+  function onUnhandledError(ev: ErrorEvent) {
+    if (
+      ev.error instanceof RedirectError ||
+      ev.error instanceof AuthenticationError ||
+      ev.error instanceof AuthorizationError ||
+      ev.error instanceof NotFoundError
+    ) {
+      // This prevents 'Uncaught error' logs in the console.
+      // This doesn't change how React or error boundaries handle errors
+      ev.preventDefault()
+    }
+  }
+  window.addEventListener("error", onUnhandledError)
+
+  if ("Cypress" in window) {
+    // Hide some errors from Cypress, so they don't fail Cypress tests
+    // https://github.com/cypress-io/cypress/issues/7196
+
+    const cypressOnErrorFun = window.onerror
+
+    window.onerror = (message, source, lineno, colno, err) => {
+      if (
+        cypressOnErrorFun &&
+        !(
+          err instanceof RedirectError ||
+          err instanceof AuthenticationError ||
+          err instanceof AuthorizationError ||
+          err instanceof NotFoundError
+        )
+      ) {
+        cypressOnErrorFun(message, source, lineno, colno, err)
+      }
+    }
+  }
 }
 
 export * from "./utils"
