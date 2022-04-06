034531dbbb7c4bbcc6acab799cece123b615552b
Dillon Raphael <dillon@creatorsneverdie.com>
2022-04-06 10:52:07 -0400

    set dynamic path for windows os

--- integration-tests/auth/test/next-test-utils.ts
+++ integration-tests/auth/test/next-test-utils.ts
@@ -114,7 +114,6 @@ export function runNextCommand(argv: any[], options: RunNextCommandOptions = {})
       cwd,
       env,
       stdio: ["ignore", "pipe", "pipe"],
-      windowsVerbatimArguments: true,
     })
 
     if (typeof options.instance === "function") {
