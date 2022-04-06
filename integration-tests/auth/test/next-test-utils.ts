d7c8c830ca4b8d074c3586c5688ca07e472b8362
Dillon Raphael <dillon@creatorsneverdie.com>
2022-04-06 10:45:11 -0400

    set path for windows test command

--- integration-tests/auth/test/next-test-utils.ts
+++ integration-tests/auth/test/next-test-utils.ts
@@ -114,6 +114,7 @@ export function runNextCommand(argv: any[], options: RunNextCommandOptions = {})
       cwd,
       env,
       stdio: ["ignore", "pipe", "pipe"],
+      windowsVerbatimArguments: true,
     })
 
     if (typeof options.instance === "function") {
