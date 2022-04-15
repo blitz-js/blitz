78b639b8209d211851abc82fa268acd0d2fb7107
Dillon Raphael <dillon@creatorsneverdie.com>
2022-04-15 08:30:18 -0400

    add type assert to template

--- packages/generator/templates/app/app/users/queries/getCurrentUser.ts
+++ packages/generator/templates/app/app/users/queries/getCurrentUser.ts
@@ -5,7 +5,7 @@ export default async function getCurrentUser(_ = null, { session }: Ctx) {
   if (!session.userId) return null
 
   const user = await prisma.user.findFirst({
-    where: { id: session.userId },
+    where: { id: session.userId as number },
     select: { id: true, name: true, email: true, role: true },
   })
 
