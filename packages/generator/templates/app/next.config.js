bb9eaed52021ef97f218bd42d6cd292ecc92b8b5
Dillon Raphael <dillon@creatorsneverdie.com>
2022-04-26 12:15:10 -0400

    remove withBundleAnalyzer

--- packages/generator/templates/app/next.config.js
+++ packages/generator/templates/app/next.config.js
@@ -1,10 +1,3 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-})
 const {withBlitz} = require("@blitzjs/next")
 
-module.exports = withBlitz(
-  withBundleAnalyzer({
-    reactStrictMode: true,
-  }),
-)
+module.exports = withBlitz()
