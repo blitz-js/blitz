392f1ae9ce12243f019a3f33ec33df5242e725f2
the-bayer <2bayerb0@gmail.com>
2022-04-27 12:06:41 -0400

    Updated changePassword mutation with try/catch

--- packages/generator/templates/app/app/auth/mutations/changePassword.ts
+++ packages/generator/templates/app/app/auth/mutations/changePassword.ts
@@ -10,7 +10,11 @@ export default resolver.pipe(
     const user = await db.user.findFirst({ where: { id: ctx.session.userId! } })
     if (!user) throw new NotFoundError()
 
-    await authenticateUser(user.email, currentPassword)
+     try {
+      await authenticateUser(user.email, currentPassword)
+    } catch (error: any) {
+      throw new Error("Invalid Password")
+    }
 
     const hashedPassword = await SecurePassword.hash(newPassword.trim())
     await db.user.update({
