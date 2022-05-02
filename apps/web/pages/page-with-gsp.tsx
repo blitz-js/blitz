7faf75cc25d0be65dce382ee18f9e26e52ef59e4
Brandon Bayer <b@bayer.ws>
2022-05-02 10:00:05 -0700

    cleanup

--- apps/web/pages/page-with-gsp.tsx
+++ apps/web/pages/page-with-gsp.tsx
@@ -1,12 +1,18 @@
 import {gSP} from "app/blitz-server"
 
-export const getStaticProps = async ({ctx}) => {
+export const getStaticProps = gSP(async ({ctx}) => {
   return {
     props: {
-      data: Array(1000).fill({something: "hey yo"}),
+      data: {
+        // userId: ctx?.session.userId,
+        // session: {
+        //   id: session.userId,
+        //   publicData: session.$publicData,
+        // },
+      },
     },
   }
-}
+})
 
 function PageWithGsp({data}) {
   return <div>{JSON.stringify(data, null, 2)}</div>
