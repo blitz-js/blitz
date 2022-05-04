26fdc0450f40df4406c849ddc2b5af71521a6450
beerose <alexsandra.sikora@gmail.com>
2022-05-04 14:54:02 +0200

    Update toolkit app

--- apps/toolkit-app/pages/auth/signup.tsx
+++ apps/toolkit-app/pages/auth/signup.tsx
@@ -1,13 +1,14 @@
 import { useRouter } from "next/router"
 import Layout from "app/core/layouts/Layout"
 import { SignupForm } from "app/auth/components/SignupForm"
+import { Routes } from "@blitzjs/next"
 
 const SignupPage = () => {
   const router = useRouter()
 
   return (
     <Layout title="Sign Up">
-      <SignupForm onSuccess={() => router.push("/")} />
+      <SignupForm onSuccess={() => router.push(Routes.Home())} />
     </Layout>
   )
 }
