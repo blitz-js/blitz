f39f6b8681e6ac6f9860e45991b9c2862845f164
beerose <alexsandra.sikora@gmail.com>
2022-05-05 14:36:56 +0200

    Setup session

--- integration-tests/trailing-slash/app/blitz-client.ts
+++ integration-tests/trailing-slash/app/blitz-client.ts
@@ -1,7 +1,12 @@
+import {AuthClientPlugin} from "@blitzjs/auth"
 import {setupBlitzClient} from "@blitzjs/next"
 
 const {withBlitz} = setupBlitzClient({
-  plugins: [],
+  plugins: [
+    AuthClientPlugin({
+      cookiePrefix: "auth-tests-cookie-prefix",
+    }),
+  ],
 })
 
 export {withBlitz}
--- integration-tests/trailing-slash/app/blitz-server.ts
+++ integration-tests/trailing-slash/app/blitz-server.ts
@@ -1,7 +1,16 @@
 import {setupBlitzServer} from "@blitzjs/next"
+import {AuthServerPlugin, PrismaStorage, simpleRolesIsAuthorized} from "@blitzjs/auth"
+import {prisma} from "../prisma"
 
 const {gSSP, gSP, api} = setupBlitzServer({
-  plugins: [],
+  plugins: [
+    AuthServerPlugin({
+      cookiePrefix: "web-cookie-prefix",
+      // TODO fix type
+      storage: PrismaStorage(prisma as any),
+      isAuthorized: simpleRolesIsAuthorized,
+    }),
+  ],
 })
 
 export {gSSP, gSP, api}
