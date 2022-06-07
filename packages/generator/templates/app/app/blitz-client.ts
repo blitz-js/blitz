10ce646786d77f22f7a65662308736379cc452f6
beerose <alexsandra.sikora@gmail.com>
2022-06-07 11:05:36 -0700

    Update parameter

--- packages/generator/templates/app/app/blitz-client.ts
+++ packages/generator/templates/app/app/blitz-client.ts
@@ -7,6 +7,6 @@ export const { withBlitz } = setupBlitzClient({
     AuthClientPlugin({
       cookiePrefix: "__safeNameSlug__-cookie-prefix",
     }),
-    BlitzRpcPlugin(),
+    BlitzRpcPlugin({}),
   ],
 })
