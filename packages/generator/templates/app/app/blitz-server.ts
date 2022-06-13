f1003faf94d62f5330b7fb8ca13ab35f0af7ab95
Dillon Raphael <dillon@creatorsneverdie.com>
2022-06-13 15:35:38 -0400

    update generator template (#3439)

--- packages/generator/templates/app/app/blitz-server.ts
+++ packages/generator/templates/app/app/blitz-server.ts
@@ -2,11 +2,12 @@ import { setupBlitzServer } from "@blitzjs/next"
 import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
 import db from "db"
 import { simpleRolesIsAuthorized } from "@blitzjs/auth"
+import { authConfig } from "./blitz-client"
 
 export const { gSSP, gSP, api } = setupBlitzServer({
   plugins: [
     AuthServerPlugin({
-      cookiePrefix: "__safeNameSlug__-cookie-prefix",
+      ...authConfig,
       storage: PrismaStorage(db as any),
       isAuthorized: simpleRolesIsAuthorized,
     }),
