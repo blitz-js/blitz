b405c1e876ff3ee07a88bdae512143c636304bad
Aleksandra <alexsandra.sikora@gmail.com>
2022-07-01 12:44:37 +0200

    Add missing _document and 404 pages to the templates (#3480)

---
+++ packages/generator/templates/app/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Main, NextScript, Head } from 'next/document';
+
+class MyDocument extends Document {
+	// Only uncomment if you need to customize this behaviour
+	// static async getInitialProps(ctx: DocumentContext) {
+	//   const initialProps = await Document.getInitialProps(ctx)
+	//   return {...initialProps}
+	// }
+	render() {
+		return (
+			<Html lang="en">
+				<Head />
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
+}
+
+export default MyDocument;
