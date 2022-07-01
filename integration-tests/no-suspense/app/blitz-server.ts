995b6ba4f51e9005e47291b44a502bab14dbbe80
beerose <alexsandra.sikora@gmail.com>
2022-07-01 13:28:43 +0200

    More types fixes

--- integration-tests/no-suspense/app/blitz-server.ts
+++ integration-tests/no-suspense/app/blitz-server.ts
@@ -1,7 +1,7 @@
 import {setupBlitzServer} from "@blitzjs/next"
 import {AuthServerPlugin, PrismaStorage} from "@blitzjs/auth"
 import {simpleRolesIsAuthorized} from "@blitzjs/auth"
-import {prisma as db} from "../prisma/index"
+import db from "../prisma/index"
 
 const {gSSP, gSP, api} = setupBlitzServer({
   plugins: [
