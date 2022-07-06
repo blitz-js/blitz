2372f521d6de89c81ad6e40422059259c0c64e8e
beerose <alexsandra.sikora@gmail.com>
2022-07-06 12:58:45 +0200

    Update global middleware location

--- integration-tests/middleware/next.config.js
+++ integration-tests/middleware/next.config.js
@@ -1,9 +1,2 @@
 const {withBlitz} = require("@blitzjs/next")
-module.exports = withBlitz({
-  middleware: [
-    (req, res, next) => {
-      res.setHeader("global-middleware", "true")
-      return next()
-    },
-  ],
-})
+module.exports = withBlitz({})
