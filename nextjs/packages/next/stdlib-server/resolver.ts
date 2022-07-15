3775da72c9dc6d880c086ac28a7a40664eab2f30
Yuval Datner <contact@datner.me>
2022-07-15 03:44:07 +0300

    fix(stdlib-server): change type from ctx to resolved value of pipe

--- nextjs/packages/next/stdlib-server/resolver.ts
+++ nextjs/packages/next/stdlib-server/resolver.ts
@@ -93,7 +93,7 @@ function pipe<
   de: PipeFn<D, E, CD, CE>,
   ef: PipeFn<E, F, CE, CF>,
   fg: PipeFn<F, G, CF, CG>
-): (input: A, ctx: CA) => EnsurePromise<CG>
+): (input: A, ctx: CA) => EnsurePromise<G>
 function pipe<
   A,
   B,
