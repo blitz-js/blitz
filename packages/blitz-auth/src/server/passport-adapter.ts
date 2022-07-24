73331d51a1be1a3e01d9e38da5d50b0561978d75
Saad Altabari <saadtabari@gmail.com>
2022-07-24 16:51:13 -0700

    Truncate error strings in passportAuth redirectUrl (#2831)

--- packages/blitz-auth/src/server/passport-adapter.ts
+++ packages/blitz-auth/src/server/passport-adapter.ts
@@ -11,6 +11,7 @@ import {
   RequestMiddleware,
   MiddlewareResponse,
   secureProxyMiddleware,
+  truncateString,
 } from "blitz"
 import {IncomingMessage, ServerResponse} from "http"
 import {PublicData, SessionContext} from "../shared"
@@ -161,7 +162,8 @@ export function passportAuth(config: BlitzPassportConfig): ApiHandler {
                 "/"
 
               if (error) {
-                redirectUrl += "?authError=" + encodeURIComponent(error.toString())
+                redirectUrl +=
+                  "?authError=" + encodeURIComponent(truncateString(error.toString(), 100))
                 res.setHeader("Location", redirectUrl)
                 res.statusCode = 302
                 res.end()
