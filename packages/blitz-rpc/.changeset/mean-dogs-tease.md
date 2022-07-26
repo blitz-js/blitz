aefbb335c887ebdb90e1ff2c11060c9d5116d5f5
Yuval Datner <contact@datner.me>
2022-07-26 15:50:30 +0300

    Fix enchancePrisma leaking to browser on certain cases

---
+++ packages/blitz-rpc/.changeset/mean-dogs-tease.md
@@ -0,0 +1,6 @@
+---
+"blitz": patch
+"@blitzjs/rpc": patch
+---
+
+Fix enhancePrisma is undefined when importing types in the browser
--- packages/blitz-rpc/src/index-server.ts
+++ packages/blitz-rpc/src/index-server.ts
@@ -68,6 +68,11 @@ interface WebpackRule {
 
 export interface InstallWebpackConfigOptions {
   webpackConfig: {
+    resolve: {
+      alias: {
+        [key: string]: boolean
+      }
+    }
     module: {
       rules: WebpackRule[]
     }
@@ -79,6 +84,8 @@ export function installWebpackConfig({
   webpackConfig,
   webpackRuleOptions,
 }: InstallWebpackConfigOptions) {
+  webpackConfig.resolve.alias["npm-which"] = false
+  webpackConfig.resolve.alias["cross-spawn"] = false
   webpackConfig.module.rules.push({
     test: /[\\/]\[\[\.\.\.blitz]]\.[jt]sx?$/,
     use: [
