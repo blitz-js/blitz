fc3cd36ca3ba8c812df6551298c83a78189a1db7
Himanshu Singh <bt20cse062@sce.org.in>
2022-07-29 08:26:52 +0530

    Update index-server.ts
    Console log is converted to standard logger

--- packages/blitz-rpc/src/index-server.ts
+++ packages/blitz-rpc/src/index-server.ts
@@ -2,6 +2,7 @@ import {assert, Ctx, prettyMs} from "blitz"
 import {NextApiRequest, NextApiResponse} from "next"
 import {deserialize, serialize as superjsonSerialize} from "superjson"
 import {resolve} from "path"
+import { baseLogger, newline } from './lib/logging'
 
 // TODO - optimize end user server bundles by not exporting all client stuff here
 export * from "./index-browser"
@@ -158,6 +159,14 @@ export function rpcHandler(config: RpcConfig) {
     if (!resolver) {
       throw new Error("No default export for resolver path: " + routePath)
     }
+    
+     const log = baseLogger().getChildLogger({
+      prefix: [route.replace('/api/rpc/', '') + '()'],
+    })
+     
+    const customChalk = new chalk.constructor({
+      level: log.settings.type === 'json' ? 0 : chalk.level,
+    })
 
     if (req.method === "HEAD") {
       // We used to initiate database connection here
@@ -168,7 +177,7 @@ export function rpcHandler(config: RpcConfig) {
 
       if (typeof req.body.params === "undefined") {
         const error = {message: "Request body is missing the `params` key"}
-        console.error(error.message)
+        log.error(error.message)
         res.status(400).json({
           result: null,
           error,
@@ -182,17 +191,23 @@ export function rpcHandler(config: RpcConfig) {
           meta: req.body.meta?.params,
         })
 
-        console.info("Starting with input:", data ? data : JSON.stringify(data))
+        log.info(
+        customChalk.dim('Starting with input:'),
+          data ? data : JSON.stringify(data)
+        )
         const startTime = Date.now()
         const result = await resolver(data, (res as any).blitzCtx)
         const resolverDuration = Date.now() - startTime
-        console.debug("Result:", result ? result : JSON.stringify(result))
+        log.debug(
+          customChalk.dim('Result:'),
+          result ? result : JSON.stringify(result)
+        )
 
         const serializerStartTime = Date.now()
         const serializedResult = superjsonSerialize(result)
 
-        const nextSerializerStartTime = Date.now()
-        ;(res as any).blitzResult = result
+        const nextSerializerStartTime = Date.now();
+        (res as any).blitzResult = result
         res.json({
           result: serializedResult.json,
           error: null,
@@ -200,16 +215,27 @@ export function rpcHandler(config: RpcConfig) {
             result: serializedResult.meta,
           },
         })
-        console.debug(`Next.js serialization:${prettyMs(Date.now() - nextSerializerStartTime)}`)
+        
+        log.debug(
+          customChalk.dim(
+            `Next.js serialization:${prettyMs(
+              Date.now() - nextSerializerStartTime
+            )}`
+          )
+        )
         const serializerDuration = Date.now() - serializerStartTime
         const duration = Date.now() - startTime
 
         console.info(
-          `Finished: resolver:${prettyMs(resolverDuration)} serializer:${prettyMs(
-            serializerDuration,
-          )} total:${prettyMs(duration)}`,
+          customChalk.dim(
+            `Finished: resolver:${prettyMs(
+              resolverDuration
+            )} serializer:${prettyMs(serializerDuration)} total:${prettyMs(
+              duration
+            )}`
+          )
         )
-        console.log("\n")
+       newline()
 
         return
       } catch (error: any) {
@@ -219,8 +245,8 @@ export function rpcHandler(config: RpcConfig) {
 
         config.onError?.(error)
 
-        console.error(error)
-        console.log("\n")
+        log.error(error)
+        newline()
 
         if (!error.statusCode) {
           error.statusCode = 500
@@ -239,7 +265,7 @@ export function rpcHandler(config: RpcConfig) {
       }
     } else {
       // Everything else is error
-      console.warn(`${req.method} method not supported`)
+      log.warn(`${req.method} method not supported`)
       res.status(404).end()
       return
     }
