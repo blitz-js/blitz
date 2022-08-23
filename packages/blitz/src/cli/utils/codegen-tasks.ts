5f99a8728c0d75c52bf9da234410cb93922f201b
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-08-23 19:54:16 +0530

    Update codegen-tasks.ts

--- packages/blitz/src/cli/utils/codegen-tasks.ts
+++ packages/blitz/src/cli/utils/codegen-tasks.ts
@@ -5,6 +5,7 @@ import {join} from "path"
 import fs from "fs-extra"
 import {getPackageJson} from "./get-package-json"
 import {runPrisma} from "../../utils/run-prisma"
+import os from "os"
 
 export const codegenTasks = async () => {
   try {
@@ -37,15 +38,36 @@ export const codegenTasks = async () => {
       (name) => name === "prisma",
     )
 
+    const tempDir = os.tmpdir() 
+    const creationDate = await fs.stat(process.cwd())
+    if (!fs.existsSync(join(tempDir, `blitz-${creationDate.birthtimeMs}prisma-status.json`))) {
+      const lastModified = await fs.stat(join(tempDir, "db/schema.prisma"))
+      await fs.writeFile(
+        join(tempDir, `blitz-${creationDate.birthtimeMs}prisma-status.json`),
+        JSON.stringify({lastModified: lastModified.mtimeMs}),
+      )
+    }
     if (hasPrisma) {
-      let prismaSpinner = log.spinner(`Generating Prisma client`).start()
-      const result = await runPrisma(["generate"], true)
-      if (result.success) {
-        prismaSpinner.succeed(log.greenText("Generated Prisma client"))
+      //check if the prisma schema has been modified since the last time we ran the codegen
+      const prismaStatus = await fs.readJson(join(tempDir, `blitz-${creationDate.birthtimeMs}prisma-status.json`))
+      const lastModified = await fs.stat(join(tempDir, "db/schema.prisma"))
+      if (prismaStatus.lastModified !== lastModified.mtimeMs) {
+        //if it has been modified, run the codegen again
+        let prismaSpinner = log.spinner(`Generating Prisma client`).start()
+        const result = await runPrisma(["generate"], true)
+        if (result.success) {
+          prismaSpinner.succeed(log.greenText("Generated Prisma client"))
+        } else {
+          prismaSpinner.fail()
+          console.log("\n" + result.stderr)
+          process.exit(1)
+        }
+        //update the prisma-status file with the last modified date of the prisma schema
+        await fs.writeJson(join(process.cwd(), `blitz-${creationDate.birthtimeMs}prisma-status.json`), {
+          lastModified: lastModified.mtimeMs,
+        })
       } else {
-        prismaSpinner.fail()
-        console.log("\n" + result.stderr)
-        process.exit(1)
+        log.success("Prisma client was up to date")
       }
     }
   } catch (err) {
