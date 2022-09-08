4e0fc4d7c4b8ab56032cf0f9865ff5c0fe43a98b
Dillon Raphael <dillon@creatorsneverdie.com>
2022-09-08 10:04:38 -0400

    revert cchanges for auth integration test

--- integration-tests/auth/test/index.test.ts
+++ integration-tests/auth/test/index.test.ts
@@ -128,6 +128,7 @@ describe("Auth Tests", () => {
       try {
         appPort = await findPort()
         app = await launchApp(appDir, appPort, {cwd: process.cwd()})
+        await seed()
       } catch (error) {
         console.log(error)
       }
