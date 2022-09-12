9216fcf49090122de7109f5c5bc8769f826aa5a3
Dillon Raphael <dillon@creatorsneverdie.com>
2022-09-12 15:46:25 -0400

    try calling seed function in seed file

--- packages/blitz/src/utils/prisma.ts
+++ packages/blitz/src/utils/prisma.ts
@@ -39,9 +39,8 @@ export const enhancePrisma = <TPrismaClientCtor extends Constructor>(
           const prismaBin = which(process.cwd()).sync("prisma")
 
           await new Promise((res, rej) => {
-            const process = spawn(prismaBin, ["migrate", "reset", "--force", "--skip-generate"], {
-              stdio: "ignore",
-            })
+            const process = spawn(prismaBin, ["migrate", "reset", "--force", "--skip-generate"], {})
+            console.log(process)
             process.on("exit", (code) => (code === 0 ? res(0) : rej(code)))
           })
           globalThis._blitz_prismaClient.$disconnect()
