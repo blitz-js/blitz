7021c3951f9681c0b38b57c4d8d90bb025abcf86
Dillon Raphael <dillon@creatorsneverdie.com>
2022-09-12 14:03:51 -0400

    try require resolve

--- packages/blitz/src/utils/prisma.ts
+++ packages/blitz/src/utils/prisma.ts
@@ -36,9 +36,9 @@ export const enhancePrisma = <TPrismaClientCtor extends Constructor>(
               "You are calling db.$reset() in a production environment. We think you probably didn't mean to do that, so we are throwing this error instead of destroying your life's work.",
             )
           }
-          console.log(path.resolve("."))
-          const prismaBin = which(path.resolve(".")).sync("prisma")
-
+          // console.log(path.resolve("."))
+          // const prismaBin = which(path.resolve(".")).sync("prisma")
+          const prismaBin = require.resolve("prisma")
           await new Promise((res, rej) => {
             const process = spawn(prismaBin, ["migrate", "reset", "--force", "--skip-generate"], {
               stdio: "ignore",
