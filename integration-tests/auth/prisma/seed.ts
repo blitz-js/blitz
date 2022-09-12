aea690007b67a820831c70aebed4cfcc2b8ebd1b
Dillon Raphael <dillon@creatorsneverdie.com>
2022-09-12 14:36:44 -0400

    test adding proces.exit(0) in seed file

--- integration-tests/auth/prisma/seed.ts
+++ integration-tests/auth/prisma/seed.ts
@@ -1,20 +1,21 @@
 import prisma from "./index"
-import { SecurePassword } from "@blitzjs/auth"
+import {SecurePassword} from "@blitzjs/auth"
 
 const seed = async () => {
   await prisma.$reset()
 
   const hashedPassword = await SecurePassword.hash("abcd1234")
 
-  await prisma.user.create({
-    data: {
-      email: "test@test.com",
-      hashedPassword,
-      role: "user",
-    },
-  }).catch(console.error)
-
+  await prisma.user
+    .create({
+      data: {
+        email: "test@test.com",
+        hashedPassword,
+        role: "user",
+      },
+    })
+    .catch(console.error)
+  process.exit(0)
 }
-
 
 export default seed
