74d9b2cd37743aa9e61f8a75632a00c071728f5b
Dillon Raphael <dillon@creatorsneverdie.com>
2022-09-12 13:14:27 -0400

    remove process.cwd from which

--- packages/blitz/src/utils/prisma.ts
+++ packages/blitz/src/utils/prisma.ts
@@ -35,7 +35,9 @@ export const enhancePrisma = <TPrismaClientCtor extends Constructor>(
               "You are calling db.$reset() in a production environment. We think you probably didn't mean to do that, so we are throwing this error instead of destroying your life's work.",
             )
           }
-          const prismaBin = which(process.cwd()).sync("prisma")
+
+          const prismaBin = which().sync("prisma")
+
           await new Promise((res, rej) => {
             const process = spawn(prismaBin, ["migrate", "reset", "--force", "--skip-generate"], {
               stdio: "ignore",
