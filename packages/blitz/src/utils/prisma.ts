da1f9ccc663fff8f72f438b4f79dcafa0b187ff1
Dillon Raphael <dillon@creatorsneverdie.com>
2022-09-12 15:40:29 -0400

    reset database outside of

--- packages/blitz/src/utils/prisma.ts
+++ packages/blitz/src/utils/prisma.ts
@@ -39,7 +39,7 @@ export const enhancePrisma = <TPrismaClientCtor extends Constructor>(
           const prismaBin = which(process.cwd()).sync("prisma")
 
           await new Promise((res, rej) => {
-            const process = spawn(prismaBin, ["db", "push", "--force-reset"], {
+            const process = spawn(prismaBin, ["migrate", "reset", "--force", "--skip-generate"], {
               stdio: "ignore",
             })
             process.on("exit", (code) => (code === 0 ? res(0) : rej(code)))
