35cfc70712f4dff9391a4bd7f36b03dfe87cc40b
beerose <alexsandra.sikora@gmail.com>
2022-09-19 10:24:13 +0200

    Lint & changeset

--- packages/blitz-next/src/index-server.ts
+++ packages/blitz-next/src/index-server.ts
@@ -187,7 +187,10 @@ export const setupBlitzServer = ({plugins, onError}: SetupBlitzOptions) => {
     ): NextApiHandler<TResult | void> =>
     async (req, res) => {
       try {
-     return await handleRequestWithMiddleware(req, res, [...middlewares, (req, res) => handler(req, res, res.blitzCtx)]);
+        return await handleRequestWithMiddleware(req, res, [
+          ...middlewares,
+          (req, res) => handler(req, res, res.blitzCtx),
+        ])
       } catch (error: any) {
         onError?.(error)
         return res.status(400).send(error)
