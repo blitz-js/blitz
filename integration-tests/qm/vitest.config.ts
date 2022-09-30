5142c04fc1ef6a9411bf5df7adf325b20d954abf
Fran Zekan <zekan.fran369@gmail.com>
2022-09-30 17:21:00 +0200

    Format

--- integration-tests/qm/vitest.config.ts
+++ integration-tests/qm/vitest.config.ts
@@ -1,12 +1,12 @@
 /// <reference types="vitest" />
 
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import {defineConfig} from "vitest/config"
+import react from "@vitejs/plugin-react"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: 'jsdom',
+    environment: "jsdom",
   },
 })
