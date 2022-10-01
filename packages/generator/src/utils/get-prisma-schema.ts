4afa6b5b1b0a501f1801ce2f7c2a4dd38de8d8ae
Siddharth Suresh <siddh.suresh@gmail.com>
2022-10-01 13:54:58 +0530

    make db import generic

--- packages/generator/src/utils/get-prisma-schema.ts
+++ packages/generator/src/utils/get-prisma-schema.ts
@@ -1,13 +1,38 @@
 import * as ast from "@mrleebo/prisma-ast"
 import {Editor} from "mem-fs-editor"
 import {log} from "../utils/log"
+import fs from "fs"
 import path from "path"
 
-export const getPrismaSchema = (memFsEditor: Editor): {schema: ast.Schema; schemaPath: string} => {
-  const schemaPath = path.resolve("db/schema.prisma")
-  if (!memFsEditor.exists(schemaPath)) {
-    throw new Error("Prisma schema file was not found")
+function getDbFolder() {
+  if (fs.existsSync(path.join(process.cwd(), "db"))) {
+    return "db"
   }
+  try {
+    const packageJsonPath = path.join(process.cwd(), "package.json")
+    const packageJson = fs.readFileSync(packageJsonPath, "utf8")
+    const packageJsonObj = JSON.parse(packageJson)
+    if (!packageJsonObj.prisma || !packageJsonObj.prisma.schema) {
+      throw new Error(
+        "db folder does not exist and Prisma schema not found in package.json. Please either create the db folder or add the prisma schema path to the package.json",
+      )
+    }
+    const prismaSchemaPath = path.join(process.cwd(), packageJsonObj.prisma.schema)
+    if (!fs.existsSync(prismaSchemaPath)) {
+      throw new Error(
+        "prisma.schema file not found. Please either create the db folder or add the prisma schema path to the package.json",
+      )
+    }
+    const folder = packageJsonObj.prisma.schema.split("/")[0] as string
+    return folder
+  } catch (e) {
+    throw e
+  }
+}
+
+export const getPrismaSchema = (memFsEditor: Editor): {schema: ast.Schema; schemaPath: string} => {
+  const dbFolder = getDbFolder()
+  const schemaPath = path.join(process.cwd(), dbFolder, "schema.prisma")
 
   let schema: ast.Schema
   try {
