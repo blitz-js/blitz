f6f54681518dfa401b4e3021e3c566dd60eb3a81
Siddharth Suresh <siddharth.suresh2020@vitstudent.ac.in>
2022-10-12 23:36:51 +0530

    Update blitz-server.ts

--- apps/toolkit-app/app/blitz-server.ts
+++ apps/toolkit-app/app/blitz-server.ts
@@ -3,7 +3,6 @@ import { setupBlitzServer } from "@blitzjs/next"
 import { AuthServerPlugin, PrismaStorage } from "@blitzjs/auth"
 import db from "db"
 import { simpleRolesIsAuthorized } from "@blitzjs/auth"
-import type { BlitzCliConfig } from "blitz"
 
 const { gSSP, gSP, api } = setupBlitzServer({
   plugins: [
