c36cfb7d09cb7acaa4b33fe90d085340cd77ab43
Siddharth Suresh <siddh.suresh@gmail.com>
2022-10-14 23:22:06 +0530

    fix lock file and add react-final-form select component

--- packages/generator/templates/app/_forms/finalform/LabelSelectField.tsx
+++ packages/generator/templates/app/_forms/finalform/LabelSelectField.tsx
@@ -1,55 +1,60 @@
-import React, { PropsWithoutRef } from "react"
-import { useField, UseFieldConfig } from "react-final-form"
-import { useId } from "@reach/auto-id"
+import { forwardRef, PropsWithoutRef } from "react"
+import { useField } from "react-final-form"
 
 export interface LabeledSelectFieldProps extends PropsWithoutRef<JSX.IntrinsicElements["select"]> {
   /** Field name. */
   name: string
   /** Field label. */
   label: string
-  /** Field type. Doesn't include radio buttons and checkboxes */
-  type?: "text" | "number"
+  type?: "number" | "string"
+  options: any
   outerProps?: PropsWithoutRef<JSX.IntrinsicElements["div"]>
-  fieldProps?: UseFieldConfig<string>
 }
 
-export const LabeledSelectField = React.forwardRef<HTMLSelectElement, LabeledSelectFieldProps>(
-  ({ name, label, fieldProps, outerProps, ...props }, ref) => {
+export const LabeledSelectField = forwardRef<HTMLSelectElement, LabeledSelectFieldProps>(
+  ({ name, label, outerProps, options, type="number", ...props }, ref) => {
     const {
       input,
       meta: { touched, error, submitError, submitting },
     } = useField(name, {
-      ...((props.type === "number"
-        ? { parse: (v: string) => Number(v) }
-        : {
-            parse: (v: string) => (v === "" ? null : v),
-          }) as any),
-      ...fieldProps,
+      parse: type === "number" ? Number : undefined,
     })
-    const id = useId() + "name"
 
     const normalizedError = Array.isArray(error) ? error.join(", ") : error || submitError
-    const showError = touched && normalizedError
 
     return (
       <div {...outerProps}>
-        <div>
-          <label htmlFor={id} >
-            {label}
-          </label>
-          <div role="alert">
-            {showError && normalizedError}
+        <label>
+          {label}
+          <select {...input} disabled={submitting} {...props} ref={ref}>
+          {options && options.map((value) => <option value={value.id}>{value[name]}</option>)}
+          </select>
+        </label>
+
+        {touched && normalizedError && (
+          <div role="alert" style={{ color: "red" }}>
+            {normalizedError}
           </div>
-        </div>
-        <select
-          id={id}
-          key={id}
-          {...input}
-          disabled={submitting}
-          {...props}
-          ref={ref}
-        />
+        )}
+
+        <style jsx>{`
+          label {
+            display: flex;
+            flex-direction: column;
+            align-items: start;
+            font-size: 1rem;
+          }
+          select {
+            font-size: 1rem;
+            padding: 0.25rem 0.5rem;
+            border-radius: 3px;
+            appearance: none;
+            margin-top: 0.5rem;
+          }
+        `}</style>
       </div>
     )
   }
 )
+
+export default LabeledSelectField
