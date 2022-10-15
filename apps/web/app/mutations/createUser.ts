2327099877ccea16fd4a388cdd0fc0b4c1784440
Siddharth Suresh <siddh.suresh@gmail.com>
2022-10-15 18:48:56 +0530

    fix

--- apps/web/app/mutations/createUser.ts
+++ apps/web/app/mutations/createUser.ts
@@ -1,7 +1,10 @@
 import {Ctx} from "blitz"
 import db, {User} from "db"
 
-export default async function createUser(input: {name: string; email: string}, ctx): Promise<User> {
+export default async function createUser(
+  input: {name: string; email: string},
+  ctx: Ctx,
+): Promise<User> {
   ctx.session.$authorize()
   const user = await db.user.create({data: {name: input.name, email: input.email}})
 
