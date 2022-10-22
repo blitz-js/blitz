6dba9517e7a13a7ce33b4f385ca08ea500d5c6e6
beerose <alexsandra.sikora@gmail.com>
2022-10-22 11:15:56 +0800

    Fix reset token being undefined when passed to the resetPassword mutation

--- apps/toolkit-app/pages/auth/reset-password.tsx
+++ apps/toolkit-app/pages/auth/reset-password.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Layout from "app/core/layouts/Layout"
 import { LabeledTextField } from "app/core/components/LabeledTextField"
 import { Form, FORM_ERROR } from "app/core/components/Form"
@@ -9,8 +10,13 @@ import { useMutation } from "@blitzjs/rpc"
 import Link from "next/link"
 
 const ResetPasswordPage: BlitzPage = () => {
+  const [token, setToken] = useState("")
   const router = useRouter()
   const [resetPasswordMutation, { isSuccess }] = useMutation(resetPassword)
+
+  useEffect(() => {
+    setToken(router.query.token as string)
+  }, [router.isReady])
 
   return (
     <div>
@@ -30,11 +36,11 @@ const ResetPasswordPage: BlitzPage = () => {
           initialValues={{
             password: "",
             passwordConfirmation: "",
-            token: router.query.token as string,
+            token,
           }}
           onSubmit={async (values) => {
             try {
-              await resetPasswordMutation(values)
+              await resetPasswordMutation({ ...values, token })
             } catch (error: any) {
               if (error.name === "ResetPasswordError") {
                 return {
