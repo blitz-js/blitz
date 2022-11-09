f13d7741c2d476fdf5eb20a38ba9cd4e338723a8
Brandon Bayer <b@bayer.ws>
2022-11-09 13:04:39 -0600

    change integration test to include useQuery

--- integration-tests/rpc/next.config.js
+++ integration-tests/rpc/next.config.js
@@ -1,7 +1,4 @@
 const {withBlitz} = require("@blitzjs/next")
 module.exports = withBlitz({
   target: "experimental-serverless-trace",
-  blitz: {
-    includeRPCFolders: ["../no-suspense/app"],
-  },
 })
