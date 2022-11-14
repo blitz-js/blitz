72a4e594ae6c80f8bde1321cafd13993f47de820
Brandon Bayer <b@bayer.ws>
2022-11-14 18:00:20 -0500

    internal: remove unneeded useSession from useQuery hooks (#3968)

--- packages/blitz-next/build.config.ts
+++ packages/blitz-next/build.config.ts
@@ -2,7 +2,17 @@ import {BuildConfig} from "unbuild"
 
 const config: BuildConfig = {
   entries: ["./src/index-browser", "./src/index-server"],
-  externals: ["index-browser.cjs", "index-browser.mjs", "blitz", ".blitz", "next", "react"],
+  externals: [
+    "index-browser.cjs",
+    "index-browser.mjs",
+    "blitz",
+    ".blitz",
+    "react",
+    "next",
+    "next/head",
+    "next/router",
+    "next/dist/shared/lib/router/router",
+  ],
   declaration: true,
   rollup: {
     emitCJS: true,
