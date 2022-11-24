933a11fcab23faa731859a9bf0c0bfeb0024e4d3
beerose <alexsandra.sikora@gmail.com>
2022-11-24 12:20:01 +0800

    Add proper vitest config to js templates

--- packages/generator/templates/minimalapp/vitest.config.ts
+++ packages/generator/templates/minimalapp/vitest.config.ts
@@ -1,11 +1,22 @@
-const nextJest = require("@blitzjs/next/jest")
+import { loadEnvConfig } from "@next/env";
+import { defineConfig } from "vitest/config";
 
-const createJestConfig = nextJest({
-  dir: "./",
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    dir: "./",
+    globals: true,
+    setupFiles: './test/setup.ts',
+    coverage: {
+       reporter: ['text', 'json', 'html'],
+    },
+  }
 })
-
-const customJestConfig = {
-  testEnvironment: "jest-environment-jsdom",
-}
-
-module.exports = createJestConfig(customJestConfig)
