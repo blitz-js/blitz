c4c2a3af66ea30c345cddeee55c31c3c130f83af
Siddharth Suresh <siddh.suresh@gmail.com>
2022-11-30 01:07:04 +0530

    convert from lowdb to prisma

--- integration-tests/auth-with-rpc/src/queries/getPublicDataForUser.ts
+++ integration-tests/auth-with-rpc/src/queries/getPublicDataForUser.ts
@@ -2,12 +2,12 @@ import {Ctx} from "blitz"
 import db from "../../db"
 
 export default async function getPublicDataForUser({userId}: any, ctx: Ctx) {
-  return (
-    db
-      //@ts-ignore
-      .get("sessions")
-      .filter({userId})
-      .value()
-      .map((session: any) => JSON.parse(session.publicData))
-  )
+  return await db.session.findMany({
+    where: {
+      userId: userId,
+    },
+    select: {
+      publicData: true,
+    },
+  })
 }
