27036188683be493b7754d34cbda0eaa3fa06a56
Brandon Bayer <b@bayer.ws>
2023-01-10 19:13:11 -0500

    change rpc route basepath includeRPC in monorepos to be one folder up (#4045)
    * change rpc route basepath includeRPC in monorepos to be one folder up
    
    * changeset
    
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- integration-tests/rpc-path-root/next.config.js
+++ integration-tests/rpc-path-root/next.config.js
@@ -2,6 +2,6 @@ const {withBlitz} = require("@blitzjs/next")
 module.exports = withBlitz({
   blitz: {
     resolverPath: "root",
-    includeRPCFolders: ["../no-suspense/app"],
+    includeRPCFolders: ["../no-suspense"],
   },
 })
