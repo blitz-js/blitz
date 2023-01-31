c1cf69e21de7c19af83ee671b3c18aa8640503ea
sweetliquid <master@sweetliquid.me>
2023-01-31 13:48:42 +0800

    docs: delete duplicates of `the` in comments

--- apps/toolkit-app/test/index.test.tsx
+++ apps/toolkit-app/test/index.test.tsx
@@ -10,7 +10,7 @@ describe("renders blitz documentation link", () => {
   it("test", () => {
     // This is an example of how to ensure a specific item is in the document
     // But it's disabled by default (by test.skip) so the test doesn't fail
-    // when you remove the the default content from the page
+    // when you remove the default content from the page
 
     // This is an example on how to mock api hooks when testing
     mockUseCurrentUser.mockReturnValue({
--- packages/generator/templates/app/test/index.test.tsx
+++ packages/generator/templates/app/test/index.test.tsx
@@ -9,7 +9,7 @@ const mockUseCurrentUser = useCurrentUser as jest.MockedFunction<typeof useCurre
 test.skip("renders blitz documentation link", () => {
   // This is an example of how to ensure a specific item is in the document
   // But it's disabled by default (by test.skip) so the test doesn't fail
-  // when you remove the the default content from the page
+  // when you remove the default content from the page
 
   // This is an example on how to mock api hooks when testing
   mockUseCurrentUser.mockReturnValue({
