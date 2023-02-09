14968d2c3cd2abaeffce29f5eab5a3a476cc069c
Siddharth Suresh <siddh.suresh@gmail.com>
2023-02-10 00:16:03 +0530

    fix unit test

--- packages/blitz-rpc/test/unit/react-query-utils.unit.test.ts
+++ packages/blitz-rpc/test/unit/react-query-utils.unit.test.ts
@@ -3,9 +3,9 @@
  */
 import {expect, beforeEach, describe, it, vi} from "vitest"
 
-import {getQueryClient, invalidateQuery, setQueryData} from "../../src/client"
+import {getQueryClient, invalidateQuery, setQueryData} from "../../src/query/react-query"
 
-import {getQueryCacheFunctions, initializeQueryClient} from "../../src/client/react-query-utils"
+import {getQueryCacheFunctions, initializeQueryClient} from "../../src/query/utils"
 import {buildQueryRpc} from "../blitz-test-utils"
 globalThis.queryClient = initializeQueryClient()
 
