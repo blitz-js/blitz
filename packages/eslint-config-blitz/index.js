6bc1472edf1371c04fdc35ead4f762b6c2235c32
Siddharth Suresh <siddh.suresh@gmail.com>
2023-03-06 20:50:18 +0530

    add new package for blitz eslint

---
+++ packages/eslint-config-blitz/index.js
@@ -0,0 +1,29 @@
+module.exports = {
+  extends: ["eslint-config-next", "prettier"],
+  ignorePatterns: ["*.d.ts"],
+  settings: {
+    next: {
+      rootDir: ["./apps/*/", "./packages/*/"],
+    },
+  },
+  rules: {
+    "@next/next/no-html-link-for-pages": "off",
+  },
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      plugins: ["@typescript-eslint"],
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      rules: {
+        "@typescript-eslint/no-floating-promises": "error",
+        "no-use-before-define": "off",
+        "@typescript-eslint/no-use-before-define": ["off"],
+        "no-redeclare": "off",
+        "@typescript-eslint/no-redeclare": ["error"],
+        "react/display-name": "off",
+      },
+    },
+  ],
+}
