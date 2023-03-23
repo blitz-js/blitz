eb04f32cd5a2894b2b746807df317164f56f145d
Siddharth Suresh <siddh.suresh@gmail.com>
2023-03-23 14:48:17 +0530

    fix validations

--- packages/generator/templates/validations/schemas.ts
+++ packages/generator/templates/validations/schemas.ts
@@ -1,20 +1,29 @@
-import {z} from "zod"
+import { z } from "zod"
 
 if (process.env.parentModel) {
-    export const Create__ModelName__Schema = z.object({
-      __parentModelId__: z.__parentModelIdZodType__(),
-      // template: __fieldName__: z.__zodType__(),
-    })
+  export const Create__ModelName__Schema = z.object({
+    __parentModelId__: z.__parentModelIdZodType__(),
+    // template: __fieldName__: z.__zodType__(),
+  })
 } else {
-    export const Create__ModelName__Schema = z.object({
-       // template: __fieldName__: z.__zodType__(),
-   })
+  export const Create__ModelName__Schema = z.object({
+    // template: __fieldName__: z.__zodType__(),
+  })
 }
 
-export const Update__ModelName__Schema = Create__ModelName__Schema.merge(z.object({
-  id: z.number(),
-}))
+if (process.env.parentModel) {
+  export const Update__ModelName__Schema = z.object({
+    id: z.__modelIdZodType__(),
+    __parentModelId__: z.__parentModelIdZodType__(),
+    // template: __fieldName__: z.__zodType__(),
+  })
+} else {
+  export const Update__ModelName__Schema = z.object({
+    id: z.__modelIdZodType__(),
+    // template: __fieldName__: z.__zodType__(),
+  })
+}
 
 export const Delete__ModelName__Schema = z.object({
-  id: z.number(),
+  id: z.__modelIdZodType__(),
 })
