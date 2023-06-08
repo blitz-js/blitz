1fcf06f3c8c15dd7c44f5aff3cd2d861462e82c9
Siddharth Suresh <siddh.suresh@gmail.com>
2023-06-08 20:46:58 +0530

    Update apps/toolkit-app/next.config.js

--- apps/toolkit-app/next.config.js
+++ apps/toolkit-app/next.config.js
@@ -6,17 +6,6 @@ const { withBlitz } = require("@blitzjs/next")
  **/
 const config = {
   reactStrictMode: true,
-  webpack: (config, { isServer }) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "next-auth/core/lib/oauth/callback":
-        process.cwd() + "/node_modules/next-auth/core/lib/oauth/callback.js",
-      "next-auth/core/lib/oauth/authorization-url":
-        process.cwd() + "/node_modules/next-auth/core/lib/oauth/authorization-url.js",
-      "next-auth/core/init": process.cwd() + "/node_modules/next-auth/core/init.js",
-    }
-    return config
-  },
 }
 
 module.exports = withBlitz(withNextAuthAdapter(config))
