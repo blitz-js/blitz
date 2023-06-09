b57f5aa229a92d0b443b0b4f2714a57f34091a61
Siddharth Suresh <siddh.suresh@gmail.com>
2023-06-09 19:21:23 +0530

    Remove unnecessary exports from @blitzjs/auth

--- packages/blitz-auth/src/server/index.ts
+++ packages/blitz-auth/src/server/index.ts
@@ -1,4 +1,15 @@
-export * from "./auth-sessions"
 export * from "./auth-utils"
 export * from "./auth-plugin"
 export * from "./adapters"
+
+export {
+  SessionContextClass,
+  getAllSessionHandlesForUser,
+  getCookieParser,
+  getSession,
+  isLocalhost,
+  setPublicDataForUser,
+  setCookie,
+  simpleRolesIsAuthorized,
+} from "./auth-sessions"
+export type {AnonymousSessionPayload, SimpleRolesIsAuthorized} from "./auth-sessions"
