d284400014936a3fbe84016a6303ed13a41dcd02
Siddharth Suresh <siddh.suresh@gmail.com>
2023-06-26 18:36:52 +0530

    wrap with try catch

--- packages/blitz-auth/src/server/adapters/next-auth/webpack.ts
+++ packages/blitz-auth/src/server/adapters/next-auth/webpack.ts
@@ -4,24 +4,28 @@ import path from "path"
 export function withNextAuthAdapter(nextConfig) {
   const config = Object.assign({}, nextConfig)
   const nextAuthPackageName = "next-auth"
-  const nextAuthPath = path.dirname(require.resolve(`${nextAuthPackageName}`))
-  const webpack = (config) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      "next-auth/core/lib/oauth/callback": path.join(nextAuthPath, "core/lib/oauth/callback.js"),
-      "next-auth/core/lib/oauth/authorization-url": path.join(
-        nextAuthPath,
-        "core/lib/oauth/authorization-url.js",
-      ),
-      "next-auth/core/init": path.join(nextAuthPath, "core/init.js"),
+  try {
+    const nextAuthPath = path.dirname(require.resolve(`${nextAuthPackageName}`))
+    const webpack = (config) => {
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        "next-auth/core/lib/oauth/callback": path.join(nextAuthPath, "core/lib/oauth/callback.js"),
+        "next-auth/core/lib/oauth/authorization-url": path.join(
+          nextAuthPath,
+          "core/lib/oauth/authorization-url.js",
+        ),
+        "next-auth/core/init": path.join(nextAuthPath, "core/init.js"),
+      }
+      return config
     }
+    if (typeof nextConfig.webpack === "function") {
+      config.webpack = (config, options) => {
+        return nextConfig.webpack(webpack(config), options)
+      }
+    }
+    config.webpack = webpack
+    return config
+  } catch (e) {
     return config
   }
-  if (typeof nextConfig.webpack === "function") {
-    config.webpack = (config, options) => {
-      return nextConfig.webpack(webpack(config), options)
-    }
-  }
-  config.webpack = webpack
-  return config
 }
