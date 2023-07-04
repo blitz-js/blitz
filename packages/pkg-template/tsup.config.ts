fa3ba9aea4b57cc1a42c5315e4fe67ae169982bf
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-04 23:46:15 +0300

    build(pkg-template)!: change build system to tsup

---
+++ packages/pkg-template/tsup.config.ts
@@ -0,0 +1,9 @@
+import {defineConfig} from "tsup"
+
+export default defineConfig({
+  dts: true,
+  clean: true,
+  format: ["esm", "cjs"],
+  target: "es2015",
+  entry: ["./src/index-browser.tsx", "./src/index-server.ts"],
+})
