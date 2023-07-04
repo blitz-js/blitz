bf251281d6557134b4e971e934f7912fe7e8d9ec
Yuval Datner <22598347+datner@users.noreply.github.com>
2023-07-04 23:47:50 +0300

    build(codemod)!: change build system to tsup

--- packages/codemod/src/index.test.ts
+++ packages/codemod/src/index.test.ts
@@ -3,7 +3,7 @@ import spawn from "cross-spawn"
 
 describe("codemod cli", () => {
   it("errors without codemod name", async () => {
-    const run = spawn.sync("node", ["dist/index.cjs"], {encoding: "utf8"})
+    const run = spawn.sync("node", ["dist/index.js"], {encoding: "utf8"})
     expect(run.stdout).toContain("Codemod not found")
   })
 })
