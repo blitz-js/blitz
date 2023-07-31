70415981172b78100903e78f56771655e898b2fc
Siddharth Suresh <siddh.suresh@gmail.com>
2023-07-31 17:33:27 +0530

    fix: update `seed.ts`

--- integration-tests/auth-with-rpc/db/seed.ts
+++ integration-tests/auth-with-rpc/db/seed.ts
@@ -3,8 +3,15 @@ import {SecurePassword} from "@blitzjs/auth/secure-password"
 
 const seed = async () => {
   const hashedPassword = await SecurePassword.hash("abcd1234")
-  await prisma.user.create({
-    data: {
+  await prisma.user.upsert({
+    where: {
+      email: "test@test.com",
+    },
+    update: {
+      hashedPassword,
+      role: "user",
+    },
+    create: {
       email: "test@test.com",
       hashedPassword,
       role: "user",
