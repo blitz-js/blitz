87b2789015abd79f01eb8610df531129f4aee0bf
Siddharth Suresh <siddh.suresh@gmail.com>
2023-07-31 17:30:16 +0530

    fix: seed script update to use `upsert`

--- integration-tests/auth/db/seed.ts
+++ integration-tests/auth/db/seed.ts
@@ -3,8 +3,15 @@ import {SecurePassword} from "@blitzjs/auth/secure-password"
 
 const seed = async () => {
   const hashedPassword = await SecurePassword.hash("abcd1234")
-  await prisma.user.create({
-    data: {
+  await prisma.user.upsert({
+    where: {
+      email: "test@test.com",
+    },
+    update: {
+      hashedPassword,
+      role: "user",
+    },
+    create: {
       email: "test@test.com",
       hashedPassword,
       role: "user",
@@ -13,6 +20,6 @@ const seed = async () => {
   process.exit(0)
 }
 
-seed()
+seed().catch
 
 export default seed
