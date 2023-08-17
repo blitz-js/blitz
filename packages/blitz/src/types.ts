8b01175b477f9ba1316915f0f1c8d96f6b6e7c4b
Siddharth Suresh <siddh.suresh@gmail.com>
2023-08-17 13:29:31 +0530

    blitz-auth: `useAuthenticatedBlitzContext` returns `AuthenticatedCtx` (#4198)
    Co-authored-by: Brandon Bayer <b@bayer.ws>

--- packages/blitz/src/types.ts
+++ packages/blitz/src/types.ts
@@ -2,7 +2,7 @@ import {UrlObject} from "url"
 // Context for plugins to declaration merge stuff into
 export interface Ctx {}
 
-export interface AuthenticatedMiddlewareCtx {}
+export interface AuthenticatedCtx {}
 
 export type CodegenField = {
   component: string
@@ -17,7 +17,7 @@ export interface RouteUrlObject extends Pick<UrlObject, "pathname" | "query" | "
   href: string
 }
 
-export interface AuthenticatedMiddlewareCtx {}
+export interface AuthenticatedCtx {}
 
 export type EventHooks = {
   onSessionCreated?: OnSessionCreated
