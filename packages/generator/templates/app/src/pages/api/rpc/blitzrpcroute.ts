c39df0ba3fc622b7688303f400cb526f68ac4154
Siddharth Suresh <siddh.suresh@gmail.com>
2023-10-15 14:59:28 +0530

    review 1 fixes

--- packages/generator/templates/app/src/pages/api/rpc/blitzrpcroute.ts
+++ packages/generator/templates/app/src/pages/api/rpc/blitzrpcroute.ts
@@ -1,4 +1,4 @@
 import { rpcHandler } from "@blitzjs/rpc"
-import { api } from "app/blitz-server"
+import { api } from "src/app/blitz-server"
 
 export default api(rpcHandler({ onError: console.log }))
