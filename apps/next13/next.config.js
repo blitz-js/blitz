fc15b3dd40879c61511a720a6016df7e17ee31aa
Siddharth Suresh <siddh.suresh@gmail.com>
2023-12-29 14:39:18 +0530

    upgrade to next 14

--- apps/next13/next.config.js
+++ apps/next13/next.config.js
@@ -3,7 +3,6 @@ const {withBlitz} = require("@blitzjs/next")
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    appDir: true,
     serverComponentsExternalPackages: ["@prisma/client", "prisma"],
   },
 }
