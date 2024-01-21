737630b906d4498b402a2356ddf6ba8c765699b7
doc0x1 <19937034+Doc0x1@users.noreply.github.com>
2024-01-21 11:07:20 -0600

    Initial commit working towards making the Blitz installer compatible with NextJS's App Router filesystem.

--- recipes/tailwind/index.ts
+++ recipes/tailwind/index.ts
@@ -1,6 +1,25 @@
 import {addImport, paths, RecipeBuilder} from "blitz/installer"
 import j from "jscodeshift"
 import {join} from "path"
+import fs from "fs"
+import path from "path"
+
+function ext(jsx = false) {
+  return fs.existsSync(path.resolve("tsconfig.json")) ? (jsx ? ".tsx" : ".ts") : ".js"
+}
+
+function isUsingAppRouter() {
+  // Check if using the NextJS app router
+  // The root layout file is always present in the app directory
+  const appRouterLayoutFile = `${paths.appSrcDirectory()}/layout${ext(true)}`
+  const appRouterLayoutFileExists = fs.existsSync(path.resolve(appRouterLayoutFile))
+
+  if (appRouterLayoutFileExists) {
+    return true
+  } else {
+    return false
+  }
+}
 
 export default RecipeBuilder()
   .setName("Tailwind CSS")
@@ -29,8 +48,12 @@ export default RecipeBuilder()
     stepId: "addStyles",
     stepName: "Stylesheet",
     explanation: `Adds a root CSS stylesheet where Tailwind is imported and where you can add global styles`,
-    targetDirectory: `./${paths.appSrcDirectory()}/core`,
-    templatePath: join(__dirname, "templates", "styles"),
+    targetDirectory: isUsingAppRouter()
+      ? `./${paths.appSrcDirectory()}`
+      : `./${paths.appSrcDirectory()}/core`,
+    templatePath: isUsingAppRouter()
+      ? join(__dirname, "next")
+      : join(__dirname, "templates", "styles"),
     templateValues: {},
   })
   .addTransformFilesStep({
@@ -39,6 +62,14 @@ export default RecipeBuilder()
     explanation: `Imports the stylesheet we just added into your app`,
     singleFileSearch: paths.app(),
     transform(program) {
+      if (isUsingAppRouter()) {
+        const stylesImport = j.importDeclaration(
+          [],
+          j.literal(`${paths.appSrcDirectory()}globals.css`),
+        )
+        addImport(program, stylesImport)
+      }
+
       const stylesImport = j.importDeclaration(
         [],
         j.literal(`${paths.appSrcDirectory()}/core/styles/index.css`),
