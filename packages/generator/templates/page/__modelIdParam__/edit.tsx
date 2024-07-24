267dafa7b331319ab0ff07cbe42f58618af9fcc2
Angelo Broggian <122638135+angeldevildev@users.noreply.github.com>
2024-07-24 17:52:47 +0200

    Update edit.tsx for issue N.4362

--- packages/generator/templates/page/__modelIdParam__/edit.tsx
+++ packages/generator/templates/page/__modelIdParam__/edit.tsx
@@ -2,7 +2,7 @@ import {Suspense} from "react"
 import {Routes} from '@blitzjs/next'
 import Head from "next/head"
 import Link from 'next/link'
-import { useRouter } from "next/router"
+import { useRouter } from "next/navigation"
 import {useQuery, useMutation} from '@blitzjs/rpc'
 import {useParam} from '@blitzjs/next'
 
